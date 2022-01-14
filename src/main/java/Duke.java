bc241417ce09eb5b1ef0a17ec4e25372e54d7acf
Toh Yan Jie <yanjie20009@gmail.com>
2022-01-15 00:52:00 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Ui ui = new Ui();
+
+        System.out.println(logo);
+        System.out.println(ui.drawLine());
+        System.out.println(ui.greet());
+        System.out.println(ui.drawLine());
+        System.out.println(ui.goodbye());
+        System.out.println(ui.drawLine());
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,13 @@
+public class Ui {
+    protected String drawLine() {
+        return "____________________________________________________________";
+    }
+
+    protected String greet() {
+        return "Hello! I'm Duke\n" + "What can I do for you?";
+    }
+
+    protected String goodbye() {
+        return "Bye. Hope to see you again soon!";
+    }
+}
