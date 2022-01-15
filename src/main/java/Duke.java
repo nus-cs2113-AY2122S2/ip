4684df2ee7b383c7a44b27432120f17a58f08df9
SeanHo <seanhowenbin@gmail.com>
2022-01-15 23:35:30 +0800

    Level -0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String greet = " Hello! I'm Duke\n" +
+                " What can I do for you?";
+
+        String bye = " Bye. Hope to see you again soon!";
+
+        String line = "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(greet);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+
+
     }
 }
