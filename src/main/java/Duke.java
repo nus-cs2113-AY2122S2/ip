5cd41358293c3b8276c4fdcae63e6001c5218b39
OzairHasan <ozairhasan98@gmail.com>
2022-01-16 19:58:01 +0800

    Duke Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
 public class Duke {
+
+    //method prints a horizontal line
+    public static void dispLine(){
+        System.out.println("____________________________________________________________");
+    }
+
+    //method prints Duke greeting
+    public static void greeting(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        dispLine();
+    }
+
+    //method prints Duke termination
+    public static void goodbye(){
+        System.out.println("Bye. Hope to see you again soon!");
+        dispLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        dispLine();
+
+        greeting();
+        goodbye();
+
     }
 }
