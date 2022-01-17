f989c1deee2fd7eb801e9cc471ca28af215f4f26
teanweijun <teanweijun@LAPTOP-SRL4FS5V.localdomain>
2022-01-17 15:23:29 +0800

    Add Level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+    public static void partitionPrint(String input) {
+        String partition = "____________________________________________________________";
+        System.out.println(input);
+        System.out.println(partition);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String greet = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        String partition = "____________________________________________________________";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(partition);
         System.out.println("Hello from\n" + logo);
+        partitionPrint(greet);
+        partitionPrint(goodbye);
     }
 }
