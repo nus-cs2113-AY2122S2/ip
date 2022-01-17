f75145029c8a52edd39202746258b167306c0bbc
unknown <brendanawjang@gmail.com>
2022-01-17 16:37:25 +0800

    Implement Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,14 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")){
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+        sc.close();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
