0d44f3f87d68f3d1fb05d618742d7a4d1b393d07
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-01-17 18:24:49 +0800

    Update .gitignore and Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+    private static void printGreetings() {
+        String logo = "     _______.  ______   .______          ___      \n"
+                + "    /       | /  __  \\  |   _  \\        /   \\     \n"
+                + "   |   (----`|  |  |  | |  |_)  |      /  ^  \\    \n"
+                + "    \\   \\    |  |  |  | |      /      /  /_\\  \\   \n"
+                + ".----)   |   |  `--'  | |  |\\  \\----./  _____  \\  \n"
+                + "|_______/     \\______/  | _| `._____/__/     \\__\\ \n";
+
+        String line = "____________________________________________________________";
+
+        System.out.println(logo);
+        System.out.println(line);
+        System.out.println("Hello! I'm Sora");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printGreetings();
+
+
     }
 }
