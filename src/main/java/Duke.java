25725e6b5d480a3c243256a2bd54541d21e9c667
LDerpy <e0540038@u.nus.edu>
2022-01-17 15:03:17 +0800

    Add Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String startDuke =
+                      " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n"
+                    + "___________________________________\n"
+                    + "Hello! Duke here!\n"
+                    + "How can I help?\n"
+                    + "___________________________________\n";
+
+        String endDuke =
+                  "Goodbye. See you next time!\n"
+                + "___________________________________\n";
+        System.out.println(startDuke);
+        System.out.println(endDuke);
     }
 }
