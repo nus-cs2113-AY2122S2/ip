b69c12e408b3df2b8fe2f2530cfd91aae31291a1
Nnythingy <ngjongrayedward8@gmail.com>
2022-01-19 17:43:36 +0800

    Add increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "____________________________________________________________\n" +
+                " Hello! I'm Nnythingy\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        String goodBye = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(welcome);
+        System.out.println(goodBye);
     }
 }
