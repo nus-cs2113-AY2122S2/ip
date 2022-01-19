2d2e82e2213cf0acbe5a080511a622e29443df89
BotBw <wang1570@e.ntu.edu.sg>
2022-01-19 16:02:14 +0800

    Add user greet and firewell message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
+        userGreet();
+
+        userFarewell();
+    }
+
+    public static void userGreet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("---------------------------------");
+        System.out.println("Hello, I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------------------");
+
+    }
+
+    public static void userFarewell() {
+        System.out.println("Bye. Hope to see you soon!");
     }
 }
