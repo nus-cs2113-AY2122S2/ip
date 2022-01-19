68d0bb0267c9aab9d97fde74f1aa7b093ba83461
allyfern72 <79740655+allyfern72@users.noreply.github.com>
2022-01-20 00:32:01 +0800

    Add Increment - Level 0: Greet and farewell

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+
+    public static void greet(){
+        System.out.println("__________________________________________________");
+        System.out.println("Hello there! Bao here!");
+        System.out.println("How can I help?");
+        System.out.println("__________________________________________________");
+    }
+
+    public static void farewell(){
+        System.out.println("Alright, goodbye. See you later alligator!");
+        System.out.println("__________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____       _       ___  \n" +
+                      " | __ )     / \\     / _ \\ \n" +
+                      " |  _ \\    / _ \\   | | | | \n" +
+                      " | |_) |  / ___ \\  | |_| | \n" +
+                      " |____/  /_/   \\_\\  \\___/";
+
+                System.out.println("Hello from\n" + logo);
+        greet();
+        farewell();
     }
 }
