5b111e89bf6c1270517f319ba42d3647fcdda1ff
tuan0369 <tuannguyen111202@gmail.com>
2022-01-20 18:44:12 +0800

    Add methods to greet, exit and draw a line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
 public class Duke {
+    public static void greeting() {
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+
+    }
+
+    public static void linePrinter() {
+        System.out.println("______________________________________________");
+    }
+
+    public static void exitLine() {
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +20,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        linePrinter();
+        greeting();
+        linePrinter();
+        exitLine();
+        linePrinter();
+
     }
 }
