1c218332e0c56d791362f13a41c18c100ae26085
z0723-julie <ZHAO0348@e.ntu.edu.sg>
2022-01-20 13:46:20 +0800

    Add greetings and exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String greetings = " Hello! I'm Duke\n" + " What can I do for you?";
+        System.out.println("____________________________________________________________");
+        String exits = " Bye. Hope to see you again soon!";
+        System.out.println(greetings);
+        System.out.println("____________________________________________________________");
+        System.out.println(exits);
+        System.out.println("____________________________________________________________");
     }
 }
