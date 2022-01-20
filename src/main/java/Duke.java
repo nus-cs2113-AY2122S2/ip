3822c1431b86bb14c3d7830cf40cd57e676af0d6
jieruilim <jieruilim@u.nus.edu>
2022-01-20 17:28:21 +0800

    Duke Level 0 - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    public static void greetings() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+    public static void exits() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        greetings();
+        exits();
+
     }
 }
