5abc45156842d5b2013d54649f2c136b38b0e987
hjunleon <randell.crapy@gmail.com>
2022-01-22 06:10:01 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,15 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello, I'm \n" + logo);
         System.out.println("What can I do for you?\n");
+
+        Scanner in = new Scanner(System.in);
+        while(true){
+            String cmd = in.nextLine();
+            if (cmd.equals("bye")){
+                break;
+            }
+            System.out.println(cmd);
+        }
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
