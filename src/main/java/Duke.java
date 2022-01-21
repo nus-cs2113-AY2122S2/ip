d74fbd328589bbca97c45688eecda2e05abcd35a
unknown <brendanawjang@gmail.com>
2022-01-21 21:11:41 +0800

    Add support for tracking Todos, Events and Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,23 +14,42 @@ public class Duke {
         String userInput = sc.nextLine();
         ArrayList<Task> store = new ArrayList<Task>();
 
-        while (!userInput.equals("bye")){
-            if (userInput.equals("list")){
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < store.size(); i++){
+                for (int i = 0; i < store.size(); i++) {
                     System.out.println(i + 1 + ". " + store.get(i).getTask());
                 }
-            } else if (userInput.startsWith("mark")){
+            } else if (userInput.startsWith("mark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
                 Task currentTask = store.get(taskIndex - 1);
                 currentTask.setDone(true);
                 System.out.println(currentTask.getTask());
-            } else if (userInput.startsWith("unmark")){
+            } else if (userInput.startsWith("unmark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
                 Task currentTask = store.get(taskIndex - 1);
                 currentTask.setDone(false);
                 System.out.println(currentTask.getTask());
-            } else{
+            } else if (userInput.startsWith("todo")) {
+                String task = userInput.split("todo")[1].trim();
+                ToDo newTask = new ToDo(task);
+                store.add(newTask);
+                printTask(newTask, store.size());
+            } else if (userInput.startsWith("deadline")) {
+                String[] processedString = userInput.split("/by");
+                String by = processedString[1].trim();
+                String task = processedString[0].split("deadline")[1].trim();
+                Deadline newTask = new Deadline(task, by);
+                store.add(newTask);
+                printTask(newTask, store.size());
+            } else if (userInput.startsWith("event")) {
+                String[] processedString = userInput.split("/at");
+                String at = processedString[1].trim();
+                String task = processedString[0].split("event")[1].trim();
+                Event newTask = new Event(task, at);
+                store.add(newTask);
+                printTask(newTask, store.size());
+            } else {
                 Task newTask = new Task(userInput);
                 store.add(newTask);
                 System.out.println("added: " + userInput);
@@ -39,5 +58,11 @@ public class Duke {
         }
         sc.close();
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printTask(Task newTask, int length) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(newTask.getTask());
+        System.out.println("Now you have " + length + " tasks in the list");
     }
 }
