52439f55548f56de6e64fe9140f84c66ced740f3
Daryl <aka.hjd@gmail.com>
2022-01-21 21:13:47 +0800

    task: Level-3 - mark as done

--- src/main/java/bot/Sailfish.java
+++ src/main/java/bot/Sailfish.java
@@ -49,6 +49,12 @@ public final class Sailfish {
             case "list": // List all tasks.
                 this.list();
                 break;
+            case "mark":
+                this.mark(command);
+                break;
+            case "unmark":
+                this.unMark(command);
+                break;
             case "bye": // Exit the app.
                 System.out.println("Farewell, sailor!");
                 return;
@@ -77,6 +83,11 @@ public final class Sailfish {
      * List all stored tasks.
      */
     private void list() {
+        if (this.tasks.size() == 0 ) {
+            System.out.println("No tasks!");
+            return;
+        }
+
         StringBuilder builder = new StringBuilder();
         builder.append("Here are the tasks in your list:\n");
 
@@ -84,11 +95,71 @@ public final class Sailfish {
         for (int i = 0; i < this.tasks.size(); i++) {
             builder.append(String.format("%d. %s\n", i + 1, this.tasks.get(i)));
         }
+        builder.deleteCharAt(builder.length() - 1);
 
         // Print the tasks.
         System.out.println(builder);
     }
 
+    /**
+     * Mark a task as done.
+     *
+     * @param command Command object containing parsed information.
+     */
+    private void mark(Command command) {
+        // Get the index of the task to mark.
+        Task task;
+        try {
+            task = this.getTask(command);
+        } catch (NumberFormatException e) {
+            System.out.println("Please specify a integer for the index and range!");
+            return;
+        }
+
+        // Set the required task as marked.
+        task.setMarked(true);
+        System.out.printf("Nice! I have marked this task as done:\n%s\n", task);
+    }
+
+    /**
+     * Mark a task as undone.
+     *
+     * @param command Command object containing parsed information.
+     */
+    private void unMark(Command command) {
+        Task task;
+        try {
+            task = this.getTask(command);
+        } catch (NumberFormatException e) {
+            System.out.println("Please specify a integer for the index and range!");
+            return;
+        }
+
+        // Set the required task as undone.
+        task.setMarked(false);
+        System.out.printf("Ok, I've marked this task as not done yet:\n%s\n", task);
+    }
+
+    /**
+     * Helper method to get a task at a particular index.
+     *
+     * @param command Command object containing parsed information.
+     * @return Task object.
+     * @throws NumberFormatException For invalid index.
+     */
+    private Task getTask(Command command) throws NumberFormatException {
+        int index = Integer.parseInt(command.getDesc()) - 1;
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new NumberFormatException();
+        }
+        return this.tasks.get(index);
+    }
+
+    /**
+     * Add a new task.
+     *
+     * @param command Command object containing parsed information.
+     */
     private void add(Command command) {
         this.tasks.add(new Task(String.format("%s %s", command.getCommand(), command.getDesc())));
         System.out.printf("Added task: %s %s\n", command.getCommand(), command.getDesc());
--- src/main/java/bot/Task.java
+++ src/main/java/bot/Task.java
@@ -7,16 +7,30 @@ public class Task {
     public final String task;
 
     /**
+     * Checks whether the item has been marked.
+     */
+    public boolean marked;
+
+    /**
      * Creates a Task object.
      *
-     * @param task      Task description.
+     * @param task Task description.
      */
     public Task(String task) {
         this.task = task;
     }
 
+    /**
+     * Sets whether the task is marked as done or not.
+     *
+     * @param marked New marked status.
+     */
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
     @Override
     public String toString() {
-        return this.task;
+        return String.format("[%s] %s ", this.marked ? "X" : " ", this.task);
     }
 }
