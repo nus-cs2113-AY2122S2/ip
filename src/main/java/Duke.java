e8e901973b751cf67fc8b96b5d201aa8b49095c0
cczhouqi <>
2022-01-21 11:30:23 +0800

    Implemented Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
+        String boundary = "____________________________________________________________\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(boundary+ logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        System.out.print(boundary + "Bye. Hope to see you again soon!\n" + boundary);
     }
 }
