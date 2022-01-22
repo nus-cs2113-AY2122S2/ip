d48396bbab00f2620aaee07699c7b92726264dad
SeanHo <seanhowenbin@gmail.com>
2022-01-23 00:05:21 +0800

    Add increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
     private static String line = "____________________________________________________________\n";
+    private static String[] list = new String[100];
+    private static int listIndex = 0;
 
     public static void greeting() {
         String logo = "███████╗██╗  ██╗██████╗ ███████╗██╗  ██╗\n" +
@@ -31,21 +32,49 @@ public class Duke {
 
         String greet = " Oh! Hello there! I'm Shrek\n" +
                 " What can I do for you?";
-        System.out.println(line);
+        System.out.print(line);
         System.out.println(logo + "\n" + shrekLogo + "\n" + line + greet);
     }
+
     public static void goodbye(){
         String bye = " Bye bye! See you later!";
-        System.out.println(line + bye + "\n" + line);
+        System.out.print(line + bye + "\n" + line);
     }
+
+    public static void printList(){
+        System.out.println("Go finish these task, NOW:");
+        for(int i = 1; i <= listIndex; i++){
+            System.out.println(i + ". " + list[i]);
+        }
+    }
+
+    public static void addToList(String input){
+        listIndex++;
+        list[listIndex] = input;
+        System.out.println("Added: " + input);
+    }
+
     public static void takeInput(String userInput){
-        System.out.println(line + userInput + "\n" + line );
+        System.out.println(line);
+        String[] words = userInput.split(" ");
+        switch (words[0]){
+        case "list":
+            printList();
+            break;
+        case "":
+            System.out.println("Type something ya prick");
+            break;
+        default:
+            addToList(userInput);
+        }
+        System.out.print(line);
     }
+
     public static void main(String[] args) {
         greeting();
         Scanner in = new Scanner(System.in);
-        String userInput = in.nextLine();
-        while (!userInput.toLowerCase().equals("bye")){
+        String userInput = in.nextLine().toLowerCase();
+        while (!userInput.equals("bye")){
             takeInput(userInput);
             userInput = in.nextLine();
         }
