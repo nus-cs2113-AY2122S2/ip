9c24bb0056f9f0604754cb0d4429095a0519f53f
shxr3f <e0540641@u.nus.edu>
2022-01-22 16:37:04 +0800

    Created a Greet Class that holds methods pertaining to greetings.

---
+++ src/main/java/Greet.java
@@ -0,0 +1,14 @@
+public class Greet {
+    private static void printDecoration(){
+        System.out.println("-----------------------------------------------------");
+    }
+    public static void sayHi(){
+        printDecoration();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+    }
+    public static void sayBye(){
+        printDecoration();
+        System.out.println("Bye! Hope to see you again soon!\n");
+        printDecoration();
+    }
+}
