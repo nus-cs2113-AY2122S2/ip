27839951b3a55d9af2e34d816daec1edf9d6781c
chooyikai <chooyikai@gmail.com>
2022-01-22 11:51:56 +0800

    Add marking and unmarking tasks, revamp command format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // We can assume <= 100 tasks
-        String[] savedTasks = new String[100];
+        Task[] savedTasks = new Task[100];
         int numSavedTasks = 0;
 
         System.out.println("Hi, I'm Robit!");
@@ -15,21 +15,80 @@ public class Duke {
             System.out.print("> ");
             String userInput = in.nextLine();
             System.out.println("------------------------------------------------------------");
-            switch (userInput) {
+            String[] userInputTokenized = userInput.split(" ", 2);
+            switch (userInputTokenized[0]) {
             case "bye":
-                System.out.println("Goodbye!");
-                System.out.println("------------------------------------------------------------");
-                return;
+                if (userInputTokenized.length == 1) {
+                    System.out.println("Goodbye!");
+                    System.out.println("------------------------------------------------------------");
+                    return;
+                } else {
+                    System.out.println("Oops, \"bye\" doesn't take any arguments!");
+                }
+                break;
             case "list":
-                for (int i = 0; i < numSavedTasks; i++) {
-                    System.out.println((i+1) + ") " + savedTasks[i]);
+                if (userInputTokenized.length == 1) {
+                    System.out.println("Here's your to-do list:");
+                    for (int i = 0; i < numSavedTasks; i++) {
+                        if (savedTasks[i].getIsDone()) {
+                            System.out.println((i + 1) + ") [✓] " + savedTasks[i].getTaskDescription());
+                        } else {
+                            System.out.println((i + 1) + ") [ ] " + savedTasks[i].getTaskDescription());
+                        }
+                    }
+                } else {
+                    System.out.println("Oops, \"list\" doesn't take any arguments!");
+                }
+                break;
+            case "add":
+                if (userInputTokenized.length == 2) {
+                    savedTasks[numSavedTasks] = new Task(userInputTokenized[1]);
+                    numSavedTasks++;
+                    System.out.println("Task added: " + userInputTokenized[1]);
+                } else {
+                    System.out.println("Oops, \"add\" expected 1 argument. Try \"add (your task)\" instead.");
+                }
+                break;
+            case "mark":
+                try {
+                    int index = Integer.parseInt(userInputTokenized[1]);
+                    if (index > 0 && index <= numSavedTasks) {
+                        if (savedTasks[index - 1].getIsDone()) {
+                            System.out.println("That task is already marked!");
+                        } else {
+                            savedTasks[index - 1].setIsDone(true);
+                            System.out.println("I've marked this task as done. Yay!");
+                            System.out.println(index + ") [✓] " + savedTasks[index - 1].getTaskDescription());
+                        }
+                    } else {
+                        System.out.println("Oops, there's no task with that index!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("That's not a number!");
+                }
+                break;
+            case "unmark":
+                try {
+                    int index = Integer.parseInt(userInputTokenized[1]);
+                    if (index > 0 && index <= numSavedTasks) {
+                        if (!savedTasks[index - 1].getIsDone()) {
+                            System.out.println("That task is already unmarked!");
+                        } else {
+                            savedTasks[index - 1].setIsDone(false);
+                            System.out.println("I've marked this task as unfinished.");
+                            System.out.println(index + ") [ ] " + savedTasks[index - 1].getTaskDescription());
+                        }
+                    } else {
+                        System.out.println("Oops, there's no task with that index!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("That's not a number!");
                 }
                 break;
             default:
-                savedTasks[numSavedTasks] = userInput;
-                numSavedTasks++;
-                System.out.println("Task added: " + userInput);
+                System.out.println("I didn't quite understand that...");
             }
+
             System.out.println("------------------------------------------------------------");
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String taskDescription;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.taskDescription = description;
+        this.isDone = false;
+    }
+
+    public String getTaskDescription() {
+        return this.taskDescription;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
