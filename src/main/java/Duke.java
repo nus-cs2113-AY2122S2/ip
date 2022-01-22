01a3ebfda242ed544c098d9604d7e5c0532b1571
chooyikai <chooyikai@gmail.com>
2022-01-22 11:14:51 +0800

    Add saving and listing tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        // We can assume <= 100 tasks
+        String[] savedTasks = new String[100];
+        int numSavedTasks = 0;
 
         System.out.println("Hi, I'm Robit!");
         System.out.println("What would you like me to do?");
@@ -12,14 +15,22 @@ public class Duke {
             System.out.print("> ");
             String userInput = in.nextLine();
             System.out.println("------------------------------------------------------------");
-            if (userInput.equals("bye")) {
+            switch (userInput) {
+            case "bye":
                 System.out.println("Goodbye!");
                 System.out.println("------------------------------------------------------------");
+                return;
+            case "list":
+                for (int i = 0; i < numSavedTasks; i++) {
+                    System.out.println((i+1) + ") " + savedTasks[i]);
+                }
                 break;
-            } else {
-                System.out.println(userInput);
-                System.out.println("------------------------------------------------------------");
+            default:
+                savedTasks[numSavedTasks] = userInput;
+                numSavedTasks++;
+                System.out.println("Task added: " + userInput);
             }
+            System.out.println("------------------------------------------------------------");
         }
     }
 }
