cf294f6cccc2e34284e4f325aab018e6e0f7babd
chooyikai <chooyikai@gmail.com>
2022-01-22 10:57:34 +0800

    Add command echoing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hi, I'm Robit!");
-        System.out.println("What would you like to do?");
-        System.out.println("============================================================");
-        System.out.println("Goodbye!");
+        System.out.println("What would you like me to do?");
+        System.out.println("------------------------------------------------------------");
+
+        while (true) {
+            System.out.print("> ");
+            String userInput = in.nextLine();
+            System.out.println("------------------------------------------------------------");
+            if (userInput.equals("bye")) {
+                System.out.println("Goodbye!");
+                System.out.println("------------------------------------------------------------");
+                break;
+            } else {
+                System.out.println(userInput);
+                System.out.println("------------------------------------------------------------");
+            }
+        }
     }
 }
