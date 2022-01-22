1f1fd24944897d013a102f79170885058c0f6a77
YitHien <e0028345@u.nus.edu>
2022-01-22 13:17:52 +0800

    Rename Duke to AEON and add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "     /\\   |  ____/ __ \\| \\ | |\n"
+                + "    /  \\  | |__ | |  | |  \\| |\n"
+                + "   / /\\ \\ |  __|| |  | | . ` |\n"
+                + "  / ____ \\| |___| |__| | |\\  |\n"
+                + " /_/    \\_\\______\\____/|_| \\_|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm AEON\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String response = in.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println(response);
+            response = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________\n");
-
     }
 }
