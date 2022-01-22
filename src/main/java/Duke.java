caf535dc7c56e7f9f9b33a49946fc3ec2dce6908
SeanHo <seanhowenbin@gmail.com>
2022-01-22 17:32:29 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,54 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String line = "____________________________________________________________\n";
 
-        String greet = " Hello! I'm Duke\n" +
+    public static void greeting() {
+        String logo = "███████╗██╗  ██╗██████╗ ███████╗██╗  ██╗\n" +
+                "██╔════╝██║  ██║██╔══██╗██╔════╝██║ ██╔╝\n" +
+                "███████╗███████║██████╔╝█████╗  █████╔╝ \n" +
+                "╚════██║██╔══██║██╔══██╗██╔══╝  ██╔═██╗ \n" +
+                "███████║██║  ██║██║  ██║███████╗██║  ██╗\n" +
+                "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n" +
+                "                                        \n";
+
+        String shrekLogo = "⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ \n" +
+                "⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ \n" +
+                "⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ \n" +
+                "⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉";
+
+        String greet = " Oh! Hello there! I'm Shrek\n" +
                 " What can I do for you?";
-
-        String bye = " Bye. Hope to see you again soon!";
-
-        String line = "____________________________________________________________";
         System.out.println(line);
-        System.out.println("Hello from\n" + logo);
-        System.out.println(greet + "\n" + line + "\n" + bye + "\n" + line);
+        System.out.println(logo + "\n" + shrekLogo + "\n" + line + greet);
+    }
+    public static void goodbye(){
+        String bye = " Bye bye! See you later!";
+        System.out.println(line + bye + "\n" + line);
+    }
+    public static void takeInput(String userInput){
+        System.out.println(line + userInput + "\n" + line );
+    }
+    public static void main(String[] args) {
+        greeting();
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        while (!userInput.toLowerCase().equals("bye")){
+            takeInput(userInput);
+            userInput = in.nextLine();
+        }
+        goodbye();
     }
 }
