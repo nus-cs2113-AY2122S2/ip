664a7fae157bc50cc2cf2dc554da86eddab569ce
wangzy <1060892810@qq.com>
2022-01-23 15:24:30 +0800

    finish level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,41 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echoCommand(String command){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t"+command);
+        System.out.println("\t____________________________________________________________");
+        System.out.println();
+    }
+
+    public static void sayGoobye(String command){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("\t"+logo);
+        System.out.println(""+logo);
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello I'm Duke.");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
         System.out.println();
 
+        String command;
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            command = in.nextLine();
 
+            if (command.contentEquals("bye")){
+                sayGoobye(command);
+                break;
+            }
+            else {
+                echoCommand(command);
+            }
 
+        }
 
-        System.out.println("list");
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tlist");
-        System.out.println("\t____________________________________________________________");
-        System.out.println();
-
-
-
-
-        System.out.println("blah");
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tblah");
-        System.out.println("\t____________________________________________________________");
-        System.out.println();
-
-
-
-        
-        System.out.println("bye");
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
     }
 }
