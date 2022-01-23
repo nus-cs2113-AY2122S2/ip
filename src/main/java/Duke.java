a5eb34920829fcf78ae46bc9d64e8b5ef680cac6
Tan Wei Li <davidtwl99@gmail.com>
2022-01-23 11:58:10 +0800

    Add increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,21 +4,46 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String userInput = "";
+
+        String[] textList = new String[100];
+        int count = 0;
+
         String line = "------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
+        System.out.println(line);
+
         while (!userInput.equals("bye")) {
-            userInput = Echo(in, line);
+            userInput = in.nextLine();
+            switch (userInput) {
+            case "list":
+                System.out.println(line);
+                listTextList(textList,count);
+                System.out.println(line);
+                break;
+            case "bye":
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            default:
+                System.out.println(line);
+                System.out.println("added: " + userInput);
+                System.out.println(line);
+                addToTextList(textList, userInput, count);
+                count++;
+                break;
+            }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static String Echo(Scanner in, String line) {
-        String userInput;
-        System.out.println(line);
-        userInput = in.nextLine();
-        System.out.println(userInput);
-        return userInput;
+    private static void addToTextList(String[] textList, String userInput, int count) {
+        textList[count] = userInput;
+    }
+
+    private static void listTextList(String[] textList, int count) {
+        for (int i = 0;i < count;i++) {
+            System.out.println(i + ". " + textList[i]);
+        }
     }
 }
