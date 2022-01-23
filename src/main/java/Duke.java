077447999eabc3bb6a4a14297b741d91fa9a3022
wangzy <1060892810@qq.com>
2022-01-23 15:38:45 +0800

    finish level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class Duke {
 
+    //level 0
+
+    public static void greeting(){
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(""+logo);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello I'm Duke.");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        System.out.println();
+    }
+
+    //level 1
+
     public static void echoCommand(String command){
         System.out.println("\t____________________________________________________________");
         System.out.println("\t"+command);
@@ -15,20 +33,33 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
     }
 
+    //level 2
+
+    public static String[] addAbility(String command, String[] abilities, int abilitiesCount){
+        abilities[abilitiesCount] = command;
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tadded: " + command);
+        System.out.println("\t____________________________________________________________");
+
+        return abilities;
+    }
+
+    public static void displayList(String[] abilities, int abilitiesCount){
+        System.out.println("\t____________________________________________________________");
+        for(int i = 0; i < abilitiesCount; i++){
+            System.out.println(String.valueOf(i + 1) + ". " + abilities[i]);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(""+logo);
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello I'm Duke.");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
-        System.out.println();
 
         String command;
+        String[] abilities = new String[100];
+        int abilitiesCount = 0;
+
+        greeting();
         while (true) {
             Scanner in = new Scanner(System.in);
             command = in.nextLine();
@@ -37,8 +68,15 @@ public class Duke {
                 sayGoobye(command);
                 break;
             }
+            else if (command.contentEquals("list")){
+                displayList(abilities, abilitiesCount); //level 2
+            }
             else {
-                echoCommand(command);
+                //echoCommand(command); //for level 1
+                abilities = addAbility(command, abilities, abilitiesCount); //level 2
+                abilitiesCount += 1;
+
+
             }
 
         }
