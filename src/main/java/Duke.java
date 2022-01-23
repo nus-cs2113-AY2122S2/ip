d065772e9b01e6ebf98ce6c6aa4157797d3f2f46
Tan Wei Li <davidtwl99@gmail.com>
2022-01-23 13:28:44 +0800

    Add increment: Level-3
    Implement a simple validation method for marking and unmarking the
    list also.
    
    Prevents ArrayIndexOutOfBounds exception, NumberFormatException
    and prevent the user from accessing item list numbers that are not
    yet created.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,46 +5,77 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String userInput = "";
 
-        String[] textList = new String[100];
+        Task[] taskList = new Task[100];
+        for (int i = 0; i < taskList.length;i++) {
+            taskList[i] = new Task(); //loops through and individually initialise each
+        }                             //Task in the array, else they remain as null
         int count = 0;
 
         String line = "------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
 
         while (!userInput.equals("bye")) {
+            System.out.println(line);
             userInput = in.nextLine();
             String[] words = userInput.split(" ");
             switch (words[0]) {
             case "list":
-                System.out.println(line);
-                listTextList(textList,count);
-                System.out.println(line);
+                listTaskList(taskList,count);
+                break;
+            case "mark":
+            case "unmark":
+                editList(taskList, words[0], words[1], count);
                 break;
             case "bye":
-                System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             default:
-                System.out.println(line);
-                System.out.println("added: " + userInput);
-                System.out.println(line);
-                addToTextList(textList, userInput, count);
+                addToTaskList(taskList, userInput, count);
                 count++;
                 break;
             }
         }
     }
 
-    private static void addToTextList(String[] textList, String userInput, int count) {
-        textList[count] = userInput;
+    private static void addToTaskList(Task[] taskList, String userInput, int count) {
+        System.out.println("added: " + userInput);
+        taskList[count].setDescription(userInput);
     }
 
-    private static void listTextList(String[] textList, int count) {
+    private static void listTaskList(Task[] taskList, int count) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0;i < count;i++) {
-            System.out.println(i + 1 + ". " + textList[i]);
+            System.out.println(i + 1 +".[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+        }
+    }
+
+    private static void editList(Task[] taskList, String command, String taskNumber, int count) {
+        if (!validateTaskNumber(taskNumber)) {
+            System.out.println("Oops! Seems like a number was not given. Try again!");
+            return;
+        }
+        int number = Integer.parseInt(taskNumber) - 1;
+        if (number >= count) {
+            System.out.println("Oops! The number given is larger than the list currently. Try again!");
+        } else if (command.equals("mark")) {
+            taskList[number].setIsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("[" + taskList[number].getStatusIcon() + "] " + taskList[number].getDescription());
+        } else {
+            taskList[number].unsetIsDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("[" + taskList[number].getStatusIcon() + "] " + taskList[number].getDescription());
+        }
+    }
+
+    private static boolean validateTaskNumber(String taskNumber) {
+        try {
+            int number = Integer.parseInt(taskNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task() {
+        this("");
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone() {
+        isDone = true;
+    }
+
+    public void unsetIsDone() {
+        isDone = false;
+    }
+}
