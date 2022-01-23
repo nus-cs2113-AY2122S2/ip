efcd545fb4ea11860a773126bd378a1ac040c618
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-01-23 21:21:35 +0800

    (Level 1) Update Sora.java to greet, echo, and exit

--- src/main/java/Sora.java
+++ src/main/java/Sora.java
@@ -1,24 +1,80 @@
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Scanner;
+
 public class Sora {
-    private static void printGreetings() {
+    private int hourOfDay = Helper.getHourOfDay();
+
+    // Accessors of fields in Sora class
+    public int getHourOfDay() {
+        return this.hourOfDay;
+    }
+
+    private void printGreetings() {
         String logo = "     _______.  ______   .______          ___      \n"
                 + "    /       | /  __  \\  |   _  \\        /   \\     \n"
                 + "   |   (----`|  |  |  | |  |_)  |      /  ^  \\    \n"
                 + "    \\   \\    |  |  |  | |      /      /  /_\\  \\   \n"
                 + ".----)   |   |  `--'  | |  |\\  \\----./  _____  \\  \n"
-                + "|_______/     \\______/  | _| `._____/__/     \\__\\ \n";
+                + "|_______/     \\______/  | _| `._____/__/     \\__\\";
 
-        String line = "____________________________________________________________";
-
+        // Print banner
         System.out.println(logo);
-        System.out.println(line);
-        System.out.println("Hello! I'm Sora");
+        Helper.printLine();
+
+        // Print greetings
+        if (getHourOfDay() < 12) {
+            System.out.print("Good morning! ");
+        } else if (getHourOfDay() < 18) {
+            System.out.print("Good afternoon! ");
+        } else {
+            System.out.print("Good evening. ");
+        }
+
+        System.out.println("I'm Sora ヽ(・∀・)ﾉ");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Helper.printLine();
+    }
+
+    public void printGoodbye() {
+        if (getHourOfDay() < 18) {
+            System.out.println("Goodbye! Have a great day ahead (⌒▽⌒)☆");
+        } else if (getHourOfDay() < 22) {
+            System.out.println("Goodbye! Have a good evening <(￣︶￣)>");
+        } else {
+            System.out.println("Good night, have a good rest... (－ω－) zzZ");
+        }
+
+        System.out.println("See you again soon~");
+        Helper.printLine();
     }
 
     public static void main(String[] args) {
-        printGreetings();
+        // Start Sora
+        Sora sora = new Sora();
+        sora.printGreetings();
+
+        // Get user command
+        Scanner in = new Scanner(System.in);
+        String userInput = "";
+
+        System.out.print("> ");
+        userInput = in.nextLine();
+        Helper.printLine();
+
+        while (!userInput.toLowerCase().equals("bye")) {
+
+            // Echo the user input
+            System.out.println(userInput);
+            Helper.printLine();
+
+            // Get next user prompt
+            System.out.print("> ");
+            userInput = in.nextLine();
+            Helper.printLine();
+        }
+
+        // User has entered "bye"
+        sora.printGoodbye();
     }
 }
