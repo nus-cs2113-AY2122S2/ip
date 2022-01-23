af23cab777488ea6894795ee75073b09c4b923db
Tan Wei Li <davidtwl99@gmail.com>
2022-01-23 10:59:31 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userInput = "";
         String line = "------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        while (!userInput.equals("bye")) {
+            System.out.println(line);
+            userInput = in.nextLine();
+            System.out.println(userInput);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
