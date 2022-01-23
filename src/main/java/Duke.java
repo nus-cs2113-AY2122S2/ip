7b5573b7ac2f2e508a1d0cad5eab6c7c95912628
ngys117 <e0540880@u.nus.edu>
2022-01-23 12:17:31 +0900

    Add Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echoInput() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("Echoing Input, type bye to exit");
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(line);
+        } while (true);
+    }
+
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n"
-                + "Hello! I'm Yae\n"
+                + "Hello, nice to meet you. I'm Yae! (*^▽^*)\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n");
+                + "____________________________________________________________");
+        echoInput();
+        System.out.println("Goodbye, see you next time!");
     }
 }
