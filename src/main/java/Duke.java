f9f6090931b18dd7dc912aa66c5052254c677160
Aiman Imtiaz <aiman.imtiaz@u.yale-nus.edu.sg>
2022-01-23 19:28:50 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,32 @@ import java.util.Scanner;
 
 public class Duke {
 
-    static String[] textList = new String[100];
+    static String[] taskList = new String[100];
+    static Boolean[] taskStatusList = new Boolean [100];
     static int currentCount = 0;
     static String dashedLine = "\t____________________________________________________________";
 
+    public static void markTask(int index){
+        taskStatusList[index - 1] = true;
+        String message = dashedLine + "\n" +
+                "\t Nice! I've marked this task as done: \n" +
+                "\t \t [X] " + taskList[index - 1] + "\n" +
+                dashedLine ;
+        System.out.println(message);
+    }
+
+    public static void unmarkTask(int index){
+        taskStatusList[index - 1] = false;
+        String message = dashedLine + "\n" +
+                "\tOK, I've marked this task as not done yet: \n" +
+                "\t \t [ ] " + taskList[index - 1] + "\n" +
+                dashedLine ;
+        System.out.println(message);
+    }
+
     public static void addToList(String line){
-        textList[currentCount] = line;
+        taskList[currentCount] = line;
+        taskStatusList[currentCount] = false;
         currentCount += 1;
         String message = dashedLine + "\n" +
                 "\t" + "added:" + line + "\n" +
@@ -18,8 +38,12 @@ public class Duke {
     public static void printList(){
         System.out.print(dashedLine);
         for (int j = 0; j < currentCount; j++){
+            String indicator;
+            if (taskStatusList[j]){
+                indicator = "[X]";
+            } else indicator = "[ ]";
             System.out.print("\n");
-            System.out.print("\t" + (j+1) + ". " + textList[j]);
+            System.out.print("\t" + (j+1) + "." + indicator + " " + taskList[j]);
         }
         System.out.println("\n" + dashedLine);
     }
@@ -51,9 +75,16 @@ public class Duke {
         while (!line.contains("bye")){
             if (line.equals("list")){
                 printList();
+            } else if (line.startsWith("mark")){
+                int indexToMark = Integer.parseInt(line.substring(5));
+                markTask(indexToMark);
+            } else if(line.startsWith("unmark")){
+                int indexToUnmark = Integer.parseInt(line.substring(7));
+                unmarkTask(indexToUnmark);
             } else addToList(line);
             line = in.nextLine();
         }
+
         System.out.print(bye);
     }
 }
