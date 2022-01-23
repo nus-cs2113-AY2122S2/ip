4fbcc004e1671f67a0cc6ead97ba7f4ad5595456
Tan Wei Li <davidtwl99@gmail.com>
2022-01-23 11:29:32 +0800

    Refactoring by extracting echo as a method to be called for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,10 +9,16 @@ public class Duke {
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
         while (!userInput.equals("bye")) {
-            System.out.println(line);
-            userInput = in.nextLine();
-            System.out.println(userInput);
+            userInput = Echo(in, line);
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static String Echo(Scanner in, String line) {
+        String userInput;
+        System.out.println(line);
+        userInput = in.nextLine();
+        System.out.println(userInput);
+        return userInput;
     }
 }
