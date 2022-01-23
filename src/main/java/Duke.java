c3b66c0f1a0cb6f67c5399070e0516827acd42e8
ngys117 <e0540880@u.nus.edu>
2022-01-23 14:57:57 +0900

    Add Level-3 Mark as Done
    Added mark and unmark functionality to tasks.
    
    Update Task.java class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,23 +6,54 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[100];
 
+        System.out.println("Storing Up to 100 Tasks, type list to show all tasks,mark or unmark to set task status"
+                + " and bye to exit");
+
         do {
-            System.out.println("Storing Up to 100 Tasks, type list to show all tasks, bye to exit");
+            System.out.println("Waiting for your input");
             line = in.nextLine();
-            if (line.equalsIgnoreCase("bye")) {
-                break;
-            } else if (line.equalsIgnoreCase("list")) {
+            String[] words = line.toLowerCase().split(" ");
+            int taskNumber = 0;
+
+            switch (words[0]) {
+            case "list":
                 printTasks(tasks);
-            } else {
+                break;
+            case "mark":
+                if (words.length == 2) {
+                    taskNumber = Integer.parseInt(words[1]);
+                }
+                if (taskNumber > Task.getNumOfTasks()) {
+                    System.out.println("You don't have that many tasks ><!");
+                    break;
+                }
+                tasks[taskNumber - 1].setDone();
+                System.out.println("I have marked the task as done!");
+                System.out.println(tasks[taskNumber - 1].getStatusIcon() + tasks[taskNumber - 1].getTaskDescription());
+                break;
+            case "unmark":
+                if (words.length == 2) {
+                    taskNumber = Integer.parseInt(words[1]);
+                }
+                if (taskNumber > Task.getNumOfTasks()) {
+                    System.out.println("You don't have that many tasks ><!");
+                    break;
+                }
+                tasks[taskNumber - 1].setUndone();
+                System.out.println("I have marked the task as not done!");
+                System.out.println(tasks[taskNumber - 1].getStatusIcon() + tasks[taskNumber - 1].getTaskDescription());
+                break;
+            default:
                 int numOfTasks = Task.getNumOfTasks();
                 tasks[numOfTasks] = new Task(line);
+                break;
             }
-        } while (Task.getNumOfTasks() <= 100);
+        } while ((!line.equalsIgnoreCase("bye")) && Task.getNumOfTasks() < 100);
     }
 
     public static void printTasks(Task[] tasks) {
         for (int i = 0; i < Task.getNumOfTasks(); i += 1) {
-            System.out.println(i+1 + ". " + tasks[i].getTaskDescription());
+            System.out.println(i+1 + ". " + tasks[i].getStatusIcon() + tasks[i].getTaskDescription());
         }
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,11 @@
 public class Task {
     private final String taskDescription;
+    private boolean isDone;
     private static int numOfTasks = 0;
 
     public Task(String taskDescription) {
         this.taskDescription = taskDescription;
+        this.isDone = false;
         numOfTasks += 1;
     }
 
@@ -13,5 +15,17 @@ public class Task {
 
     public String getTaskDescription() {
         return taskDescription;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X] " : "[ ] ");
     }
 }
