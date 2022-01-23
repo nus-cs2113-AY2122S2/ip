f380df8a449f8a9243e4a08c2ea41cf63253e432
JL <tha.junlim@gmail.com>
2022-01-23 15:32:18 +0800

    Update greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    public static void printLine() {
+        System.out.print("_______________"
+                + "_______________"
+                + "_______________"
+                + "_______________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLine();
+        System.out.print("\nHello! I'm Duke\n" +
+                "What can I do for you?\n");
+        printLine();
+        System.out.println("\nBye. Hope to see you again soon!");
+        printLine();
     }
 }
