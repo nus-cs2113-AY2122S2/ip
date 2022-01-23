c019daeedce263a1940eba69c97b7ad1143284ab
JL <tha.junlim@gmail.com>
2022-01-23 19:00:09 +0800

    Change Bob's function from echo machine to a to-do list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String[] storage = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int num = 0;
     private static final String line = "_______________"
                                + "_______________"
@@ -11,31 +11,55 @@ public class Duke {
     public static void list() {
         int index = 1;
         for (int i = 0; i < num; i++) {
-            System.out.println(index + ". " + storage[i]);
+            System.out.println(index + ". ["
+                               + tasks[i].getStatusIcon()
+                               + "] "
+                               + tasks[i].getDescription());
             index++;
         }
     }
 
     public static void taskManager(String input) {
         System.out.println(line);
-        if (input.equals("list")) {
+        String[] words = input.split(" ");
+        int i;
+        switch(words[0]) {
+        case "list":
             list();
-        } else {
-            storage[num++] = input;
+            break;
+        case "mark":
+            i = Integer.parseInt(words[1]) - 1;
+            tasks[i].markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n"
+                               + "  [" + tasks[i].getStatusIcon()
+                               + "] "
+                               + tasks[i].getDescription());
+            break;
+        case "unmark":
+            i = Integer.parseInt(words[1]) - 1;
+            tasks[i].unmark();
+            System.out.println("Ok, I've marked this task as" +
+                    " not done yet:\n  ["
+                    + tasks[i].getStatusIcon()
+                    + "] "
+                    + tasks[i].getDescription());
+            break;
+        default:
+            Task t = new Task(input);
+            tasks[num++] = t;
             System.out.println("added: " + input);
         }
         System.out.println(line);
     }
 
     public static void main(String[] args) {
-        String input;
         Scanner in = new Scanner(System.in);
         System.out.println(line);
         System.out.print("Hello! I'm Bob,\n"
                 + "your friendly neighbourhood assistant.\n"
                 + "How can I help you today?\n");
         System.out.println(line);
-        input = in.nextLine();
+        String input = in.nextLine();
         while (!input.equals("bye")) {
             taskManager(input);
             input = in.nextLine();
