4dc0317dcbe793a1b3106992dafd9f32707fc7fe
Musfirahe0556596 <Musfirah@LAPTOP-C85KLE6R>
2022-01-24 23:55:04 +0800

    Add increment Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void displayGreeting() {
+        String border = "_____________________________________________________";
+        String logo = " _______     __   __  \n"
+                + "|   _   |[x]| | / / [x]  \n"
+                + "|  | |  || ||  / /  | |\n"
+                + "|  | |  || ||  <    | |\n"
+                + "|__/ |__||_||_| |_| |_|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(border);
+        System.out.println("Hello! I'm Niki");
+        System.out.println("What can I do for you?");
+        System.out.println(border);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(border);
+    }
+
+    public static void main(String[] args) {
+        displayGreeting();
     }
 }
