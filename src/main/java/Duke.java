45c29b9af1b7db77fb173faab91229a7fb716c9d
mafpovbul <justynphoa@gmail.com>
2022-01-24 18:27:37 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm KaiKai.");
         System.out.println("What can I do for you?");
+        System.out.println("______________________________________");
 
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        while (!str.equals("bye")) {
+            System.out.println(str);
+            System.out.println("______________________________________");
+            str = sc.nextLine();
+        }
         System.out.println("Bye! Hope to see you again!");
     }
 }
