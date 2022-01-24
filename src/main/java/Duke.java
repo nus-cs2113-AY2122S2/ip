833299f3c405975cd9c530a71a24f3f9943f934e
Joshua <joshuajr13@gmail.com>
2022-01-24 23:17:47 +0800

    Duke.java: add Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("-----------------------------");
+        System.out.println("Hello! I'm Olivia, your lovely personal assistant.");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------------");
+        System.out.println("Bye. Hope to see you again soon! With Love, Olivia");
     }
 }
