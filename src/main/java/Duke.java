8683c2738beeb010e9268cbc1ce6e80ba4d96e48
Joshua <joshuajr13@gmail.com>
2022-01-25 02:32:42 +0800

    Duke.java add Increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,16 +14,32 @@ public class Duke {
         System.out.println("What can Olivia do for you my love?");
         System.out.println("-----------------------------");
         String userInput;
+        ArrayList<String> taskList=new ArrayList<String>();
         do {
             Scanner sc = new Scanner(System.in);
             userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 break;
             }
-            System.out.println("-----------------------------");
-            System.out.println(userInput);
-            System.out.println("-----------------------------");
+            else if (userInput.equals("list")) {
+                System.out.println("-----------------------------");
+                for (int i = 0; i < taskList.size(); i++) {
+                    //System.out.println(45+5 + "=" +45+5)
+                    System.out.printf(i+1 + ". ");
+                    System.out.println(taskList.get(i) + " ");
+                }
+                System.out.println("-----------------------------");
+            }
+            else {
+                taskList.add(userInput);
+                System.out.println("-----------------------------");
+                System.out.print("added: ");
+                System.out.println(userInput);
+                System.out.println("-----------------------------");
+            }
         } while(!userInput.equals("bye"));
+        System.out.println("-----------------------------");
         System.out.println("Bye. Hope to see you again soon! With Love, Olivia");
+        System.out.println("-----------------------------");
     }
 }
