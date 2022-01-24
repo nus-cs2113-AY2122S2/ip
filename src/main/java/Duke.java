9adc438a05593739889a1ab967228fc95b813ecf
khseah <seahkithan55@gmail.com>
2022-01-24 11:15:54 +0800

    Add store and list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,28 @@ public class Duke {
         System.out.println(PURPLE_BOLD_BRIGHT + logo + RESET);
 
         String command;
+        int numTask = 0;
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?\n");
         command = in.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println(command + "\n");
+            if (command.equals("list")) {
+                if (numTask == 0) {
+                    System.out.println("No tasks yet!");
+                } else {
+                    for (int i = 0; i < numTask; i++) {
+                        System.out.println(i + 1 + ". " + tasks[i]);
+                    }
+                }
+                System.out.print(System.lineSeparator());
+            } else {
+                tasks[numTask] = command;
+                System.out.println("Added: " + command + "\n");
+                numTask++;
+            }
             command = in.nextLine();
         }
         System.out.println("Bye! Hope to see you again soon!");
