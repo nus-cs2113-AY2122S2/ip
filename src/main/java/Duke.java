e5c7e5676a76b8ef17ec80cc74c50a503fffe9d4
Joshua <joshuajr13@gmail.com>
2022-01-25 02:01:01 +0800

    Duke.java: add Increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,8 +10,19 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("-----------------------------");
         System.out.println("Hello! I'm Olivia, your lovely personal assistant.");
-        System.out.println("What can I do for you?");
+        System.out.println("What can Olivia do for you my love?");
         System.out.println("-----------------------------");
+        String userInput;
+        do {
+            Scanner sc = new Scanner(System.in);
+            userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println("-----------------------------");
+            System.out.println(userInput);
+            System.out.println("-----------------------------");
+        } while(!userInput.equals("bye"));
         System.out.println("Bye. Hope to see you again soon! With Love, Olivia");
     }
 }
