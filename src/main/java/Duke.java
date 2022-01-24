065d91cb95ddd32cb7deeebc25d5c2da72a66395
mafpovbul <justynphoa@gmail.com>
2022-01-24 19:33:29 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,17 +9,44 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numTasks = 0;
+        int taskNum;
 
         while (!str.equals("bye")) {
             if (str.equals("list")){
-                for (int i=0; i<numTasks; i++){
-                    System.out.println((i+1) + ". " + tasks[i]);
+                printTasks(tasks, numTasks);
+            }
+            else if (str.equals("mark")){
+                printTasks(tasks, numTasks);
+                System.out.println("Which task would you like to mark as completed?");
+                taskNum = sc.nextInt();
+                sc.nextLine();
+                if (taskNum > numTasks || taskNum <= 0){
+                    System.out.println("Uh oh! It seems like that task doesn't exist!");
+                }
+                else {
+                    tasks[taskNum - 1].setDone(true);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    tasks[taskNum - 1].printTask();
+                }
+            }
+            else if (str.equals("unmark")){
+                printTasks(tasks, numTasks);
+                System.out.println("Which task would you like to mark as incomplete?");
+                taskNum = sc.nextInt();
+                sc.nextLine();
+                if (taskNum > numTasks || taskNum <= 0){
+                    System.out.println("Uh oh! It seems like that task doesn't exist!");
+                }
+                else {
+                    tasks[taskNum - 1].setDone(false);
+                    System.out.println("Okie, I've marked this task as not done yet:");
+                    tasks[taskNum - 1].printTask();
                 }
             }
             else {
-                tasks[numTasks] = str;
+                tasks[numTasks] = new Task(str);
                 numTasks++;
                 System.out.println("added " + str);
             }
@@ -27,5 +54,12 @@ public class Duke {
             str = sc.nextLine();
         }
         System.out.println("Bye! Hope to see you again!");
+    }
+
+    public static void printTasks(Task[] tasks, int numTasks){
+        for (int i=0; i<numTasks; i++){
+            System.out.print((i+1) + ". ");
+            tasks[i].printTask();
+        }
     }
 }
