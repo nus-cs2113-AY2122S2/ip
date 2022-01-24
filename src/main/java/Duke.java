92ed3fc00467559447f68f444d58f005c1c8f94c
Daryl <ithinkmynameisdaryl@gmail.com>
2022-01-24 11:26:07 +0800

    Add echo, exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+         */
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("----------------------------------");
+        System.out.println("Hello! I'm Bim!");
+        System.out.println("What can I do for you?");
+        System.out.println("----------------------------------");
+        while(in.hasNext()) {
+            String input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Have a nice day!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+            System.out.println("----------------------------------");
+        }
     }
 }
