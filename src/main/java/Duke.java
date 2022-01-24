7335696667fb3226d8ceeee3c0cc0af976519b7d
sethl <sethlxk@gmail.com>
2022-01-25 02:32:33 +0800

    Duke.java add increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,18 +12,30 @@ public class Duke {
         System.out.println("Ayo my name's Duke!");
         System.out.println("What's up?");
         System.out.println("=============================");
+        ArrayList<String> array = new ArrayList<String>();
+        int i = 0;
         Scanner sc = new Scanner(System.in);
         String stringInput;
-        do{
-            stringInput = sc.next();
-            if (stringInput.equals("bye")){
+        do {
+            stringInput = sc.nextLine();
+            if (stringInput.equals("bye")) {
                 break;
+            } else if (stringInput.equals("list")) {
+                System.out.println("=============================");
+                for (int j = 0; j < array.size(); j++) {
+                    System.out.println(j + 1 + ". " + array.get(j));
+                }
+                System.out.println("=============================");
+            } else {
+                array.add(stringInput);
+                System.out.println("=============================");
+                System.out.println("added: " + stringInput);
+                System.out.println("=============================");
             }
-            System.out.println("=============================");
-            System.out.println(stringInput);
-            System.out.println("=============================");
-        } while (!stringInput.equals("bye"));
+        }while (!stringInput.equals("bye"));
+        System.out.println("=============================");
         System.out.println("Cheers! See you!");
+        System.out.println("=============================");
 
 
 
