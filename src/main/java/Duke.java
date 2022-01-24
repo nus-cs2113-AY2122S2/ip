7394ea36005514f836fd021b0c933ad37414e244
Musfirahe0556596 <Musfirah@LAPTOP-C85KLE6R>
2022-01-25 00:05:50 +0800

    Add increment Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,13 +13,31 @@ public class Duke {
         System.out.println("Hello! I'm Niki");
         System.out.println("What can I do for you?");
         System.out.println(border);
+    }
 
+    public static void main(String[] args) {
+        displayGreeting();
+        String[] taskList = new String[100];
+        int taskCount = 0;
+
+        String border = "_____________________________________________________";
         String userInput;
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
         while (!userInput.equalsIgnoreCase("bye")) {
             System.out.println(border);
-            System.out.println(userInput);
+
+            if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i += 1) {
+                    System.out.println(i + 1 + ". " + taskList[i]);
+                }
+            }
+            else{
+                taskList[taskCount] = userInput;
+                System.out.println("added: "+ userInput);
+                taskCount += 1;
+            }
+
             System.out.println(border);
             userInput = in.nextLine();
         }
@@ -27,9 +45,5 @@ public class Duke {
         System.out.println(border);
         System.out.println("Bye!!! Hope to see you again soon! :)");
         System.out.println(border);
-    }
-
-    public static void main(String[] args) {
-        displayGreeting();
     }
 }
