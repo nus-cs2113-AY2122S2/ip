9b7da77ecbd67763f276e27c4e016934318ef7b9
Daryl <ithinkmynameisdaryl@gmail.com>
2022-01-24 11:57:02 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public void setAsDone() {
+        this.isDone = true;
+    }
+
+    public void setAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
