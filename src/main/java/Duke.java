296b5f6f7ed1c587c2387d3858a30025314a308b
khseah <seahkithan55@gmail.com>
2022-01-24 16:57:53 +0800

    Add ability to mark tasks as done or not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,43 @@ public class Duke {
     public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
     public static final String RESET = "\033[0m";
 
+    public static void listCommand(Task[] tasks) {
+        if (Task.getNumTask() == 0) {
+            System.out.println("No tasks yet!");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < Task.getNumTask(); i++) {
+                if (tasks[i].getStatus()) {
+                    System.out.print(i+1 + ".[X] ");
+                } else {
+                    System.out.print(i+1 + ".[ ] ");
+                }
+                System.out.println(tasks[i].getDescription());
+            }
+        }
+        System.out.print(System.lineSeparator());
+    }
+
+    public static void unmarkCommand(String command, Task[] tasks){
+        int taskIndex = Integer.parseInt(command.substring(7));
+        tasks[taskIndex-1].unmarkAsDone();
+        System.out.println("OK, I've marked this task as not done: ");
+        System.out.println("[ ] " + tasks[taskIndex-1].getDescription() + System.lineSeparator());
+    }
+
+    public static void markCommand(String command, Task[] tasks) {
+        int taskIndex = Integer.parseInt(command.substring(5));
+        tasks[taskIndex-1].markAsDone();
+        System.out.println("Nice, I've marked this task as done: ");
+        System.out.println("[X] " + tasks[taskIndex-1].getDescription() + System.lineSeparator());
+    }
+
+    public static void addTask(String command, Task[] tasks) {
+        Task newTask = new Task(command);
+        tasks[Task.getNumTask()-1] = newTask;
+        System.out.println("Added: " + command + "\n");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,8 +50,7 @@ public class Duke {
         System.out.println(PURPLE_BOLD_BRIGHT + logo + RESET);
 
         String command;
-        int numTask = 0;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         Scanner in = new Scanner(System.in);
 
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?\n");
@@ -22,18 +58,13 @@ public class Duke {
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                if (numTask == 0) {
-                    System.out.println("No tasks yet!");
-                } else {
-                    for (int i = 0; i < numTask; i++) {
-                        System.out.println(i + 1 + ". " + tasks[i]);
-                    }
-                }
-                System.out.print(System.lineSeparator());
+                listCommand(tasks);
+            } else if (command.contains("unmark")) {
+                unmarkCommand(command, tasks);
+            } else if (command.contains("mark")) {
+                markCommand(command, tasks);
             } else {
-                tasks[numTask] = command;
-                System.out.println("Added: " + command + "\n");
-                numTask++;
+                addTask(command, tasks);
             }
             command = in.nextLine();
         }
