25d2a8a2640dff1f5588582d43320dc35d357c75
IsaacLai <e0540509@u.nus.edu>
2022-01-24 21:39:54 +0800

    Add increment: level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,64 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void greet() {
+        printLine();
+        System.out.println("Hi there! I'm Domo the chatbot.\nWhat would you like to do? " +
+                "Type a command/anything you wish!");
+    }
+    public static String getCommand() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+    public static void echoCommand() {
+        String command = getCommand();
+        Boolean isBye = command.equals("bye");
+        while (!isBye) {
+            System.out.println("The command you typed is: " + command);
+            command = getCommand();
+            isBye = command.equals("bye");
+        }
+    }
+    public static void exit() {
+        printLine();
+        System.out.println( "Goodbye. Hope to see you again soon!");
+        printDomo();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String horizontalLine = "_______________________________\n";
-        System.out.println(horizontalLine + "Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+        greet();
+        echoCommand();
+        exit();
+    }
+    public static void printDomo() {
+        String domoLogo = "─────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄─────────\n" +
+                "───────▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄───────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒███▒▒▒▒▒▒▒▒███▒▒▒▌──────\n" +
+                "▄▄────▐▒▒▒███▒▒▒▒▒▒▒▒███▒▒▒▌────▄▄\n" +
+                "▌▒▀▄──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──▄▀▒▐\n" +
+                "▌▒▒▒▀▄█▒▒▒▄▀▄▄▀▀▄▄▀▀▄▄▀▄▒▒▒█▄▀▒▒▒▐\n" +
+                "▀▄▒▒▒▒▐▒▒▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌▒▒▌▒▒▒▒▄▀\n" +
+                "──▀▄▒▒▐▒▒▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌▒▒▌▒▒▄▀──\n" +
+                "────▀▄▐▒▒▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌▒▒▌▄▀────\n" +
+                "──────█▒▒▐▄▀▄▀▀▄▄▀▀▄▄▀▀▄▌▒▒█──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▌──────\n" +
+                "──────▐▒▒▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▓▓▄▀▀▄▓▓▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▌──▐▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▌──▐▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▌──▐▒▒▒▒▒▒▒▒▌──────\n" +
+                "──────▐▒▒▒▒▒▒▒▒▌──▐▒▒▒▒▒▒▒▒▌──────\n" +
+                "───────▀▀▀▀▀▀▀▀────▀▀▀▀▀▀▀▀───────";
+        System.out.println(domoLogo);
+    }
+    public static void printLine() {
+        String horizontalLine = "───────────────────────────────────";
+        System.out.println(horizontalLine);
     }
 }
