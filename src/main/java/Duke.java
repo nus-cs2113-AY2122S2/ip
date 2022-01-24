fd8fb70d2e958e4c1882275e7fd24a9c089f85c9
khseah <seahkithan55@gmail.com>
2022-01-24 10:23:25 +0800

    Add colour to banner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
 public class Duke {
+    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
+    public static final String RESET = "\033[0m";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
+        System.out.println(PURPLE_BOLD_BRIGHT + logo + RESET);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
 
