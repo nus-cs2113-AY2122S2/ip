da3ba8dd22d6a2b2d644ba9ea7486c89e8431512
OzairHasan <e0407597@u.nus.edu>
2022-01-24 16:09:55 +0800

    Add echo function. Personality change as well.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    //method prints a horizontal line
-    public static void dispLine(){
+    //method prints a horizontal line.
+    public static void displayLine(){
         System.out.println("____________________________________________________________");
     }
 
-    //method prints Duke greeting
+    //method prints Duke greeting.
     public static void greeting(){
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        dispLine();
+        System.out.println("Hey there! Duke here!");
+        System.out.println("How can I serve you today?");
+        displayLine();
     }
 
-    //method prints Duke termination
+    //method prints Duke termination.
     public static void goodbye(){
-        System.out.println("Bye. Hope to see you again soon!");
-        dispLine();
+        System.out.println("Goodbye. See you in the funny papers.");
+        displayLine();
+    }
+
+    //method runs main echo functionality of duke.
+    public static void echo(){
+        Scanner in = new Scanner(System.in);
+        while(true){
+            //read input.
+            String input = in.nextLine();
+            displayLine();
+
+            //check for 'bye'. Exit if so.
+            if(input.equals("bye")){
+                return;
+            }
+
+            //print input if not bye.
+            System.out.println(input);
+            displayLine();
+        }
+
     }
 
     public static void main(String[] args) {
@@ -24,10 +46,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        dispLine();
+        System.out.println("You are entering the\n" + logo + "\nZone...\n");
 
+        //opening sequence
+        displayLine();
         greeting();
+
+        //echo loop
+        echo();
+
+        //ending sequence
         goodbye();
 
     }
