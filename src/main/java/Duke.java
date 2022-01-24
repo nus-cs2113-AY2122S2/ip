29654a7be340b1414fa586e9a3870e9dffade4a3
sethl <sethlxk@gmail.com>
2022-01-24 23:19:04 +0800

    Duke.java add increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("=============================");
+        System.out.println("Ayo my name's Duke!");
+        System.out.println("What's up?");
+        System.out.println("=============================");
+        System.out.println("Cheers! See you!");
     }
 }
