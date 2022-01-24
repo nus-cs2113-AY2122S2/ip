c986bcdfc918c0b965e3f55ac6f19ed07c20c59b
Musfirahe0556596 <Musfirah@LAPTOP-C85KLE6R>
2022-01-24 23:58:33 +0800

    Add increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
-public class Duke {
+import java.util.Scanner;
 
+public class Duke {
     public static void displayGreeting() {
         String border = "_____________________________________________________";
         String logo = " _______     __   __  \n"
@@ -12,7 +13,19 @@ public class Duke {
         System.out.println("Hello! I'm Niki");
         System.out.println("What can I do for you?");
         System.out.println(border);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(border);
+            System.out.println(userInput);
+            System.out.println(border);
+            userInput = in.nextLine();
+        }
+
+        System.out.println(border);
+        System.out.println("Bye!!! Hope to see you again soon! :)");
         System.out.println(border);
     }
 
