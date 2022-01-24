08226a7dcc84235fa4a69b327fddba88ff9d0645
tuan0369 <tuannguyen111202@gmail.com>
2022-01-24 22:35:47 +0800

    Add mark and unmark function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void linePrinter() {
+        System.out.print("\t");
         for(int i = 0; i < 70; i++) {
             System.out.print("\u2500");
         }
@@ -11,8 +11,8 @@ public class Duke {
 
     public static void greeting() {
         linePrinter();
-        System.out.println(" I have been waiting for you, Mister Winters");
-        System.out.println(" Forgive my manners, call me the Duke. Now to business. " +
+        System.out.println("\t" + " I have been waiting for you, Mister Winters");
+        System.out.println("\t" + " Forgive my manners, call me the Duke. Now to business. " +
                 "Weapons, ammunition, healing salves – anything you desire, I can provide.");
         linePrinter();
 
@@ -22,21 +22,43 @@ public class Duke {
         linePrinter();
 
         for (String item : items) {
-            System.out.println(item);
+            System.out.println("\t" + " " + item );
         }
 
         linePrinter();
     }
 
+    public static void printMark(String markedItem) {
+        if (markedItem == null) {
+            echo("Ah...The item you choose doesn't exist on your list.");
+        } else {
+            linePrinter();
+            System.out.println("\t" + " Aha! An interesting selection!");
+            System.out.println("\t" + "   " + markedItem);
+            linePrinter();
+        }
+    }
+
+    public static void printUnmark(String unmarkedItem) {
+        if (unmarkedItem == null) {
+            echo("Ah...The item you choose doesn't exist on your list.");
+        } else {
+            linePrinter();
+            System.out.println("\t" + " Ah, if only you had the funds...");
+            System.out.println("\t" + "   " + unmarkedItem);
+            linePrinter();
+        }
+    }
+
     public static void exitLine() {
         linePrinter();
-        System.out.println(" Good day, then!");
+        System.out.println("\t" + " Good day, then!");
         linePrinter();
     }
 
     public static void echo (String line) {
         linePrinter();
-        System.out.println(line);
+        System.out.println("\t" + " " + line);
         linePrinter();
     }
 
@@ -51,12 +73,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println("Hello from\n" + logo);
+
         greeting();
         line = input.nextLine();
+
         while(!line.equalsIgnoreCase("bye")) {
-            if (line.startsWith("Add")) {
+            if (line.startsWith("Add ")) {
                 list.addToList(line);
                 echo(line.replace("Add", "Added") + " to the cart");
+            } else if (line.startsWith("Mark ")) {
+                String markedItem = list.mark(line);
+                printMark(markedItem);
+            } else if (line.startsWith("Unmark ")){
+                String unmarkedItem = list.unmark(line);
+                printUnmark(unmarkedItem);
             } else if (line.equals("List")){
                 printList(list.listTheList());
             } else {
@@ -65,6 +95,7 @@ public class Duke {
 
             line = input.nextLine();
         }
+
         exitLine();
     }
 }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 public class List {
-    private String[] list;
+    private String[][] list;
     private int numberOfItems;
 
-    public String[] getList() {
+    public String[][] getList() {
         return list;
     }
 
@@ -13,22 +13,47 @@ public class List {
     }
 
     public List() {
-        this.list = new String[100];
+        this.list = new String[2][100];
         this.numberOfItems = 0;
     }
 
     public void addToList(String line) {
-        this.list[numberOfItems] = (line.substring(3)).trim();
+        this.list[0][numberOfItems] = (line.substring(3)).trim();
+        this.list[1][numberOfItems] = "[ ]";
         this.numberOfItems += 1;
     }
 
     public String[] listTheList() {
-        String[] items = Arrays.copyOf(list, numberOfItems);
+        String[] items = Arrays.copyOf(list[0], numberOfItems);
 
         for (int i = 1; i <= items.length; i++) {
-            items[i - 1] = Integer.toString(i) + ". " + items[i - 1];
+            items[i - 1] = Integer.toString(i) + "." + list[1][i-1] + " " + items[i - 1];
         }
 
         return items;
+    }
+
+    public String mark(String line) {
+        String ans = null;
+        int addIndex = Integer.parseInt(line.substring(5)) - 1;
+
+        if ((addIndex >= 0) && (addIndex < numberOfItems)) {
+            this.list[1][addIndex] = "[X]";
+            ans = this.list[1][addIndex] + " " + this.list[0][addIndex];
+        }
+
+        return ans;
+    }
+
+    public String unmark(String line) {
+        String ans = null;
+        int addIndex = Integer.parseInt(line.substring(7)) - 1;
+
+        if ((addIndex >= 0) && (addIndex < numberOfItems)) {
+            this.list[1][addIndex] = "[ ]";
+            ans = this.list[1][addIndex] + " " + this.list[0][addIndex];
+        }
+
+        return ans;
     }
 }
