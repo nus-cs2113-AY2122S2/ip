92d5a5a564c84669e9e86430918a8f7755e53b2d
sethl <sethlxk@gmail.com>
2022-01-25 23:41:33 +0800

    Duke.java add increment: Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
         System.out.println("Ayo my name's Duke!");
         System.out.println("What's up?");
         System.out.println("=============================");
-        ArrayList<String> array = new ArrayList<String>();
+        ArrayList<Task> array = new ArrayList<Task>();
         int i = 0;
         Scanner sc = new Scanner(System.in);
         String stringInput;
@@ -22,12 +22,32 @@ public class Duke {
                 break;
             } else if (stringInput.equals("list")) {
                 System.out.println("=============================");
+                System.out.println("Here's what you got to do! Seize the day!");
                 for (int j = 0; j < array.size(); j++) {
-                    System.out.println(j + 1 + ". " + array.get(j));
+                    System.out.println(j + 1 + ". " + array.get(j).getStatusIcon() + array.get(j).getDescription());
                 }
                 System.out.println("=============================");
+            } else if (stringInput.contains("unmark")) {
+                String[] temp = new String[100];
+                temp = stringInput.split(" ");
+                int num = Integer.parseInt(temp[1]);
+                array.get(num-1).markAsUndone();
+                System.out.println("=============================");
+                System.out.println("What are you waiting for? Just do it!");
+                System.out.println(array.get(num-1).getStatusIcon() + array.get(num-1).getDescription());
+
+            } else if (stringInput.contains("mark")){
+                String[] temp = new String[100];
+                temp = stringInput.split(" ");
+                int num = Integer.parseInt(temp[1]);
+                array.get(num-1).markAsDone();
+                System.out.println("=============================");
+                System.out.println("Go get it king, well done!");
+                System.out.println(array.get(num-1).getStatusIcon() + array.get(num-1).getDescription());
+
             } else {
-                array.add(stringInput);
+                Task t = new Task(stringInput);
+                array.add(t);
                 System.out.println("=============================");
                 System.out.println("added: " + stringInput);
                 System.out.println("=============================");
