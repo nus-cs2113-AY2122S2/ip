8f31378d9b80e2b93a82a6f4b6fc362a7c6423ed
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-01-25 19:37:30 +0800

    Add Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,12 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "-----------------------------";
+        Scanner in = new Scanner(System.in);
+        String input;
+
         System.out.println("Hello from\n" + logo);
         System.out.println(line);
         System.out.println("Hello there! I'm Duke :D");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye! Hope to see you soon :D");
-        System.out.println(line);
+
+        while (true) {
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye! Hope to see you soon :D");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(input + System.lineSeparator() + line);
+            }
+        }
     }
 }
