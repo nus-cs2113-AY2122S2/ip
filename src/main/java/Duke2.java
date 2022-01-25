fa9fcfa3c24e44b3ef9e442dc9697fe16298fe8b
lestersimjj <lester.sim@u.nus.edu>
2022-01-26 01:00:24 +0800

    Added increment: Level-0

---
+++ src/main/java/Duke2.java
@@ -0,0 +1,11 @@
+public class Duke2 {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
