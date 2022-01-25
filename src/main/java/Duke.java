7369cd6749a6e649bdf1158029a649249e93cf3b
hearobe <tan.yl1210@gmail.com>
2022-01-26 00:00:50 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,12 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("____________________\n"+
-                         "Hello! I'm Duke\n"+
-                         "What can I do for you?\n");
+                          "Hello! I'm Duke\n"+
+                          "What can I do for you?\n");
+        boolean userToQuit = false;
+        String input;
+        while(!userToQuit){
+            input = sc.nextLine();
+            switch(input){
+                case "bye":
+                    userToQuit = true;
+                    break;
+                default:
+                    echo(input);
+            }
+        }
+
         System.out.print("____________________\n"+
                 "Bye. Hope to see you again soon!\n"+
                 "____________________\n");
+    }
+
+    private static void echo(String input){
+        System.out.printf("%s\n", input);
     }
 }
