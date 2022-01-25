627b003f8c6dfc65e1df15acf01da63a28f4c436
zhenster <bobicusj@gmail.com>
2022-01-25 16:41:17 +0800

    Duke.java: Add greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Greet and say good-bye
+        String line = "_____________________________________________\n";
+        String greeting = "Hello! I'm Duke \nWhat can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + greeting);
+        System.out.println(line + goodbye + line);
     }
 }
