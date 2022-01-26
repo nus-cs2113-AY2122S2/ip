95d2900549e753f0c7835131e16cc392fcc25a80
chihyingho <User@DESKTOP-JLMCFN6>
2022-01-27 04:10:35 +0800

    Add exit message to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,11 +7,18 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        exit();
     }
+
     public static void greet() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
