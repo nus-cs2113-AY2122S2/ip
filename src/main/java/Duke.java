5d9f4e2d2102d79c8a2a695d3a09940cfe12edaa
Franke Tang <franketang@gmail.com>
2022-01-26 15:52:02 +0800

    Add response functionality to "bye" for Boba (Level-1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         printSeparator();
         System.out.println(" Hello! I'm Boba");
         System.out.println(" What can I do for you?");
+        printSeparator();
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while(!input.equals("bye")) {
+            printSeparator();
+            System.out.println(" " + input);
+            printSeparator();
+            input = scan.nextLine();
+        }
         printSeparator();
         System.out.println(" Bye. Hope to see you again soon!");
         printSeparator();
