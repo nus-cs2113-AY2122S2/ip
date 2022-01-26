f2ae749cfae2f8840984a9e951efee904005ad8f
yuzhongng <yuzhongng@gmail.com>
2022-01-27 01:27:20 +0800

    Add echo of user input and exits program when "bye" is typed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+
+        String endOfSection = "\n___________________________________________________\n";
+
+        System.out.println(logo + "\nHello! I'm Duke\nWhat can I do for you?" + endOfSection);
+
+        line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(line + endOfSection);
+            line = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!" + endOfSection);
     }
 }
