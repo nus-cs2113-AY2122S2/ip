89ef1fa53edbd53d873a05515e343a97a746b9d4
ibrahimisramos <64336014+ibrahimisramos@users.noreply.github.com>
2022-01-26 17:18:00 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void printList(String[] list, int counter){
+        for(int i = 0; i < counter; i++){
+            System.out.println(i + 1 + ". " + list[i]);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,11 +17,23 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
+        String[] list=new String[100];
+        int listCounter = 0;
+
         String input;
         Scanner sc = new Scanner(System.in);
         input = sc.nextLine();
+
         while(!input.equalsIgnoreCase("bye")){
-            System.out.println(input);
+            // print list
+            if (input.equalsIgnoreCase("list")){
+                printList(list,listCounter);
+            }
+            else {
+                list[listCounter] = input;
+                listCounter++;
+                System.out.println("added: "+input);
+            }
             input = sc.nextLine();
 
         }
