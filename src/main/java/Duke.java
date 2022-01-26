498c97868eaca34a8c98f0c891b0c1e9750fc103
Tianai <e0661454@u.nus.edu>
2022-01-26 21:38:51 +0800

    Add greeting and goodbye function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
 public class Duke {
+
+    public static void printLine(){
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void greeting(){
+        System.out.println("Hi! This is Duke!");
+        System.out.println("I'm glad to be at your service.");
+        System.out.println("What can I help you with?");
+        printLine();
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printLine();
+        greeting();
+
+        bye();
+
     }
 }
