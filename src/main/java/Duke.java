92d6ad8e22e871854f2250846688fede28cf4ac1
Roy Tang <roy_tql@hotmail.com>
2022-01-26 15:03:37 +0800

    Add Greet, Echo, Exit responses

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,56 @@
+import java.util.Scanner;
+
 public class Duke {
+    // String assets for Dukebot GUI
+    final static String BORDER = "__________________________________________________";
+    final static String INTRO_LOGO = "   ___             _               ___             _     \n" +
+            "  |   \\   _  _    | |__    ___    | _ )    ___    | |_   \n" +
+            "  | |) | | +| |   | / /   / -_)   | _ \\   / _ \\   |  _|  \n" +
+            "  |___/   \\_,_|   |_\\_\\   \\___|   |___/   \\___/   _\\__|  \n" +
+            "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n" +
+            "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' ";
+    final static String OUTRO_LOGO = "                               __\n" +
+            "                            _ / /\n" +
+            "  __ ___      ____      __ (_) | \n" +
+            " / _` \\ \\ /\\ / /\\ \\ /\\ / /   | | \n" +
+            "| (_| |\\ V  V /  \\ V  V /   _| | \n" +
+            " \\__,_| \\_/\\_/    \\_/\\_/   (_) | \n" +
+            "                              \\_\\";
+
+    public static void printIntro(){
+        System.out.println(INTRO_LOGO);
+        System.out.println(BORDER);
+        System.out.println(" Good Morning sir, I am DukeBot, your personal assistant! \n What can I do for you today?");
+        System.out.println(BORDER);
+    }
+
+    public static void printOutro(){
+        System.out.println(OUTRO_LOGO);
+        System.out.println(BORDER);
+        System.out.println(" I guess it's time to say good bye... \n Please come again soon!");
+        System.out.println(BORDER);
+    }
+
+    public static void printBorder(){
+        System.out.println(BORDER);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("_______________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("_______________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("_______________________________________________");
+        printIntro();
+        while (true){
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+            switch (command){
+            case "bye":
+                printOutro();
+                System.exit(0);
+                break;
+            default:
+                printBorder();
+                System.out.println(" "+command);
+                printBorder();
+                break;
+            }
+        }
     }
 }
