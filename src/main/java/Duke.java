d876f2469163550f9d4e11b1a2ff3540a3b6238a
ibrahimisramos <64336014+ibrahimisramos@users.noreply.github.com>
2022-01-26 17:07:31 +0800

    Level-1: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,8 +8,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        String input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        while(!input.equalsIgnoreCase("bye")){
+            System.out.println(input);
+            input = sc.nextLine();
+
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
