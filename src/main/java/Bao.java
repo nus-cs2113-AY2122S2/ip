2bb701f5e4b4b5d9ed85cfa0036f23a40c62e5f6
allyfern72 <79740655+allyfern72@users.noreply.github.com>
2022-01-27 04:15:42 +0800

    Add Increment - Level 3: Mark as Done

--- src/main/java/Bao.java
+++ src/main/java/Bao.java
@@ -2,43 +2,61 @@ import java.util.Scanner;
 
 public class Bao {
     private static Scanner in = new Scanner(System.in);
-    private static String[] lines = new String[100];
-    private static int numLines=0;
+    private static Task[] tasks = new Task[100];
+    private static int numTasks=0;
 
     private static void greet(){
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
         System.out.println("Hello there! Bao here!");
         System.out.println("How can I help?");
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
     }
 
     private static void echo(String msg){
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
         System.out.println(msg);
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
     }
 
-    private static void addLine(String msg){
-        System.out.println("__________________________________________________");
-        lines[numLines++]=msg;
+    private static void addTask(String msg){
+        System.out.println("______________________________________________________________________________________");
+        tasks[numTasks++]=new Task(msg);
         System.out.println("Yup yup yup, " + msg + ", annnd there we go, it's been added!");
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
     }
 
-    private static void listLines(){
+    private static void markTask(String msg){
+        System.out.println("______________________________________________________________________________________");
+        int ind = Integer.parseInt(msg.substring(msg.indexOf(' ')+1));
+        tasks[--ind].setIsDone(true);
+        System.out.println(tasks[ind].toString());
+        System.out.println("Boom! Another task done already???");
+        System.out.println("______________________________________________________________________________________");
+    }
+
+    private static void unmarkTask(String msg){
+        System.out.println("______________________________________________________________________________________");
+        int ind = Integer.parseInt(msg.substring(msg.indexOf(' ')+1));
+        tasks[--ind].setIsDone(false);
+        System.out.println(tasks[ind].toString());
+        System.out.println("Oh oops, overlooked that one did we?");
+        System.out.println("______________________________________________________________________________________");
+    }
+
+    private static void listTasks(){
         int x=0;
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
         System.out.println("Here you go:");
-        while(x<numLines){
-            System.out.println(x+1 + ". " + lines[x++]);
+        while(x<numTasks){
+            System.out.println(x+1 + ". " + tasks[x++].toString());
         }
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
     }
 
     private static void farewell(){
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
         System.out.println("Alright, goodbye. See you later alligator!");
-        System.out.println("__________________________________________________");
+        System.out.println("______________________________________________________________________________________");
     }
 
     public static void main(String[] args) {
@@ -54,13 +72,15 @@ public class Bao {
         userInput = in.nextLine();
         while(!userInput.equals("bye")){
             if(userInput.equals("list")){
-                listLines();
-                userInput=in.nextLine();
-                continue;
+                listTasks();
+            } else if(userInput.startsWith("mark")){
+                markTask(userInput);
+            } else if(userInput.startsWith("unmark")){
+                unmarkTask(userInput);
+            } else {
+                addTask(userInput);
             }
-            addLine(userInput);
             userInput = in.nextLine();
-
         }
         farewell();
     }
