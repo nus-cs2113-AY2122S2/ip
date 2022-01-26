7a00a2eab1c128409159bb3eb442eb64d30b8f20
shunyao643 <63784032+shunyao643@users.noreply.github.com>
2022-01-26 16:55:00 +0800

    Add Echo functionality to Duke to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -8,12 +10,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
          */
-        String greetString = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(greetString);
+
+        Scanner s = new Scanner(System.in);
+
+        String INDENT_LINE = "____________________________________________________________\n";
+        String GREET_STRING = INDENT_LINE +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                INDENT_LINE;
+        String BYE_STRING = INDENT_LINE +
+                "     Bye. Hope to see you again soon!\n" +
+                INDENT_LINE;
+        String input = "";
+
+        System.out.println(GREET_STRING);
+        input = s.nextLine();
+        while (!input.equals("bye")){
+            System.out.println(INDENT_LINE+"    "+input+System.lineSeparator()+INDENT_LINE);
+            input = s.nextLine();
+        }
+
+        System.out.println(BYE_STRING);
+
     }
 }
