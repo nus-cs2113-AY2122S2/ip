d476875aa7828f6eddf63e41c6da402afdd124af
siewyangzhi <syangzhi54@gmail.com>
2022-01-27 01:30:38 +0800

    ChatBot class:  Add printList method as well as addTaskToList method in accordance to the level-2 increment. Remove echoCommand method in accordance to the level-2 increment. Duke class: Update bot's variable name. Add printList method as well as addTaskToList method in accordance to the level-2 increment. Remove echoCommand method in accordance to the level-2 increment.

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,11 +1,21 @@
 public class ChatBot {
     private final String BOT_NAME = "Big Bob";
+    private String[] listOfTasks = new String[100];
+    private int numOfTaskInList = 0;
     public ChatBot() {
-        System.out.println("    Greetings Human! I'm "+BOT_NAME+".");
+        System.out.println("    Greetings Human! I'm " + BOT_NAME + ".");
         System.out.println("    How may i be of service to you?");
     }
-    public void echoCommand(String command) {
-        System.out.println("    "+command);
+    public void printList() {
+        for (int i = 0; i < numOfTaskInList; i++) {
+            int taskNumber = i + 1;
+            System.out.println("    " + taskNumber + ". " + listOfTasks[i]);
+        }
+    }
+    public void addTaskToList(String taskName) {
+        listOfTasks[numOfTaskInList] = taskName;
+        numOfTaskInList++;
+        System.out.println("    added: " + taskName);
     }
     public void echoFarewellGreeting() {
         System.out.println("    Good bye.See you soon :)) !");
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,18 @@ public class Duke {
     public static void startBot(Scanner in, boolean shouldExitProgram) {
         final String HORIZONTAL_LINE = "    ------------------------------------------------------------";
         System.out.println(HORIZONTAL_LINE);
-        ChatBot yz = new ChatBot();
+        ChatBot bigBob = new ChatBot();
         System.out.println(HORIZONTAL_LINE);
         while (!shouldExitProgram) {
             String userInput = in.nextLine();
             System.out.println(HORIZONTAL_LINE);
             if (userInput.equals("bye")) {
-                yz.echoFarewellGreeting();
+                bigBob.echoFarewellGreeting();
                 shouldExitProgram = true;
+            } else if (userInput.equals("list")) {
+                bigBob.printList();
             } else {
-                yz.echoCommand(userInput);
+                bigBob.addTaskToList(userInput);
             }
             System.out.println(HORIZONTAL_LINE);
         }
@@ -22,6 +24,6 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean shouldExitProgram = false;
-        startBot(in,shouldExitProgram);
+        startBot(in, shouldExitProgram);
     }
 }
