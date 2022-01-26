a91f0e8a660054285a5acfd6fc57c6a2e315754f
heekit73098 <bangheekit@gmail.com>
2022-01-26 13:48:29 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,18 +11,31 @@ public class Duke {
         System.out.println(" Hello! I'm Duke\n What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] list = new String[100];
+        int i = 0;
         Scanner in = new Scanner(System.in);
         while (true){
             String line = in.nextLine();
             if (line.equals("bye")){
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+            if (line.equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int j = 0; j < i; j++){
+                    System.out.println(j+1 + ". " + list[j]);
+                }
+                System.out.println("____________________________________________________________");
+            }else{
+                list[i] = line;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + line);
+                System.out.println("____________________________________________________________");
+                i++;
+            }
+
         }
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
