806bf7245d0f081e466906e1cfcb145a3a5c65da
zhenster <bobicusj@gmail.com>
2022-01-26 13:31:49 +0800

    Add Level-1: Greeet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 public class Duke {
+    public static void echo() {
+        // Greeting
+        String division = "_____________________________________________\n";
+        String greeting = "Hello! I'm Duke, the greatest virtual assistant in the world :) \nWhat can I do for you?\n";
+        System.out.println(division);
+        System.out.println(greeting);
+        System.out.println(division);
+
+        // Echoing behavior
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(division);
+            System.out.println(line);
+            System.out.println(division);
+            line = in.nextLine();
+        }
+        System.out.println(division);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(division);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +29,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        echo();
 
-        // Greet and say good-bye
-        String line = "_____________________________________________\n";
-        String greeting = "Hello! I'm Duke \nWhat can I do for you?\n";
-        String goodbye = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + greeting);
-        System.out.println(line + goodbye + line);
     }
 }
