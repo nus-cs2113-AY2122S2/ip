28e200504394db408b3168dc338410d4b0d24296
FaliciaOng <ongfali@gmail.com>
2022-01-27 01:30:42 +0800

    TaskManager class: add list method to store tasks and printList method to print all task stored

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class TaskManager {
     private final String BOT_NAME = "[iWish]: ";
+    public String[] taskList = new String[100];
+    public int trackList = 0;
 
-    public void echoInput() {
+    public void startUp() {
         String userInput = "";
         Scanner in = new Scanner(System.in);
         while (true) {
@@ -11,9 +13,21 @@ public class TaskManager {
             if (userInput.equals("bye")) {
                 System.out.println(BOT_NAME + "Bye! Hope to hear from you soon :)");
                 break;
+            } else if (userInput.equals("list")) {
+                printList();
             } else {
-                System.out.println(BOT_NAME + userInput);
+                taskList[trackList] = userInput;
+                trackList++;
+                System.out.println(BOT_NAME + " added " + userInput);
             }
         }
+    }
+
+    public void printList() {
+        System.out.println(BOT_NAME + " ** These are your wishes **");
+        for (int i = 0; i < trackList; i++) {
+            System.out.println((i + 1) + ". " + taskList[i]);
+        }
+        System.out.println("We reached the end of the list. Anymore wish?");
     }
 }
