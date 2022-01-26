b0db68667a2c2859834eea9c434afd8d7f471799
heekit73098 <bangheekit@gmail.com>
2022-01-26 14:47:14 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,69 @@
 import java.util.Scanner;
 public class Duke {
+    public static void listTasks(Task[] list, int i){
+        System.out.println("____________________________________________________________");
+        for (int j = 0; j < i; j++) {
+            if (list[j].getMarked()){
+                System.out.println(j + 1 + ". [X] " + list[j].getName());
+            }else{
+                System.out.println(j + 1 + ". [ ] " + list[j].getName());
+            }
+        }
+        System.out.println("____________________________________________________________");
+    }
+    public static void updateMarkTask(Task[] list, int i, String line, boolean mark){
+        int index;
+        try{
+            index = Integer.parseInt(line.split(" ")[1]) - 1;
+        }catch (NumberFormatException e){
+            System.out.println("____________________________________________________________");
+            System.out.println(" Error: Invalid index (Not an integer).");
+            System.out.println("____________________________________________________________");
+            return;
+        }
+        if (index >= i){
+            System.out.println("____________________________________________________________");
+            System.out.println(" Error: Invalid index (Out of bounds).");
+            System.out.println("____________________________________________________________");
+            return;
+        }
+        System.out.println("____________________________________________________________");
+        if (mark){
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("  [X] " + list[index].getName());
+        }else{
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("  [ ] " + list[index].getName());
+        }
+        System.out.println("____________________________________________________________");
+        list[index].setMarked(mark);
+    }
+    public static int addNewTask(Task[] list, int i, String line){
+        list[i] = new Task(line, false);
+        System.out.println("____________________________________________________________");
+        System.out.println(" added: " + line);
+        System.out.println("____________________________________________________________");
+        return i + 1;
+    }
+    public static void waitForInput(Task[] list, Scanner in){
+        int i = 0;
+        while (true){
+            String line = in.nextLine();
+            if (line.equals("bye")){
+                break;
+            }
+            if (line.equals("list")) {
+                listTasks(list, i);
+            }else if (line.split(" ")[0].equals("mark")){
+                updateMarkTask(list, i, line, true);
+            }else if (line.split(" ")[0].equals("unmark")){
+                updateMarkTask(list, i, line, false);
+            }else{
+                i = addNewTask(list, i, line);
+            }
+
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,29 +75,9 @@ public class Duke {
         System.out.println(" Hello! I'm Duke\n What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        String[] list = new String[100];
-        int i = 0;
+        Task[] list = new Task[100];
         Scanner in = new Scanner(System.in);
-        while (true){
-            String line = in.nextLine();
-            if (line.equals("bye")){
-                break;
-            }
-            if (line.equals("list")){
-                System.out.println("____________________________________________________________");
-                for (int j = 0; j < i; j++){
-                    System.out.println(j+1 + ". " + list[j]);
-                }
-                System.out.println("____________________________________________________________");
-            }else{
-                list[i] = line;
-                System.out.println("____________________________________________________________");
-                System.out.println(" added: " + line);
-                System.out.println("____________________________________________________________");
-                i++;
-            }
-
-        }
+        waitForInput(list, in);
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String name;
+    private boolean marked = false;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getMarked() {
+        return marked;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
+    public Task(String name, boolean marked) {
+        this.name = name;
+        this.marked = marked;
+    }
+}
