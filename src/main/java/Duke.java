d15ea8b42e0fdde79473b24a0af165686984d2ac
Nnythingy <ngjongrayedward8@gmail.com>
2022-01-27 01:47:39 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String welcome = "____________________________________________________________\n" +
-                " Hello! I'm Nnythingy\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
-        String goodBye = " Bye. Hope to see you again soon!\n" +
+        welcomeMessage();
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        while(!userInput.equals("bye")) {
+            userInput = wrapMessage(userInput);
+            System.out.println(userInput);
+            userInput = input.nextLine();
+        }
+        byeMessage();
+    }
+
+    public static String wrapMessage(String message) {
+        message = "____________________________________________________________\n "+
+                message +
+                " \n____________________________________________________________\n";
+
+        return message;
+    }
+
+    public static void welcomeMessage() {
+        String welcome="____________________________________________________________\n"+
+                " Hello! I'm Nnythingy\n"+
+                " What can I do for you?\n"+
                 "____________________________________________________________\n";
         System.out.println(welcome);
+    }
+
+    public static void byeMessage() {
+        String goodBye = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
         System.out.println(goodBye);
     }
 }
