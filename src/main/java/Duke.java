bd9a4462a00cfc2c95dc7f14211586f814fee5f1
Zhou Qi <>
2022-01-27 00:01:23 +0800

    Week 3 tasks done (Level-1,2,3; A-CodingStandard)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String boundary = "____________________________________________________________\n";
+        String boundary = "____________________________________________________________" + System.lineSeparator();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,8 +10,37 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(boundary+ logo);
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you?" + System.lineSeparator() + boundary);
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        Task[] taskList = new Task[100];
+        int countTask = 0;
 
-        System.out.print(boundary + "Bye. Hope to see you again soon!\n" + boundary);
+        while (!line.equalsIgnoreCase("bye")) {
+            if (line.equalsIgnoreCase("list")) {
+                System.out.print(boundary);
+                for (int i = 0; i < countTask; i ++) {
+                    System.out.println((i + 1) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
+                }
+                System.out.print(boundary);
+            } else if (line.toLowerCase().startsWith("mark")) {
+                int toMark = Integer.parseInt(line.substring(5)) - 1;
+                taskList[toMark].markDone();
+                System.out.println(boundary + "Nice! I've marked this task as done:");
+                System.out.println("[" + taskList[toMark].getStatusIcon() + "] " + taskList[toMark].description + System.lineSeparator() + boundary);
+            } else if (line.toLowerCase().startsWith("unmark")) {
+                int toMark = Integer.parseInt(line.substring(7)) - 1;
+                taskList[toMark].markNotDone();
+                System.out.println(boundary + "OK, I've marked this task as not done yet:");
+                System.out.println("[" + taskList[toMark].getStatusIcon() + "] " + taskList[toMark].description + System.lineSeparator() + boundary);
+            } else {
+                taskList[countTask] = new Task(line);
+                countTask ++;
+                System.out.println(boundary + "added: " + line + System.lineSeparator() + boundary);
+            }
+            in = new Scanner(System.in);
+            line = in.nextLine();
+        }
+        System.out.print(boundary + "Bye. Hope to see you again soon!" + System.lineSeparator() + boundary);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setTask(String description) {
+        this.description = description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+}
