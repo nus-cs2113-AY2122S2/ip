29166174c86527a67da0388dd80346c759261b3d
quitejasper <jasper.chia9@gmail.com>
2022-01-27 03:49:23 +0800

    added lines into greet and exit method, for visual

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,14 @@ public class Duke {
     }
 
     public static void greet(){
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 
     public static void exit(){
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
