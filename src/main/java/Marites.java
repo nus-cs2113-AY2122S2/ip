aa5a6be538c90f3cdf302dab4f3c88f20c3525f8
Dan Baterisna <danbaterisna@u.nus.edu>
2022-01-26 23:39:14 +0800

    Modify initial greeting

---
+++ src/main/java/Marites.java
@@ -0,0 +1,14 @@
+public class Marites {
+    public static void main(String[] args) {
+
+        // Found in https://emojicombos.com/kaomoji
+        String logo = "(งツ)ว";
+
+        System.out.println(logo);
+        System.out.println("Hi, I'm Marites! I've heard so many things about you!");
+        System.out.println("I have a lot of stories to share, but first, how can I help you?");
+        System.out.println("=============================");
+
+        System.out.println("See you next time!");
+    }
+}
