7539318b4c564d10ab0ccee8f15d2ac464be186d
Dan Baterisna <danbaterisna@u.nus.edu>
2022-01-27 00:41:18 +0800

    Add task listing feature

--- src/main/java/Marites.java
+++ src/main/java/Marites.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Marites {
     static void printSeparator() {
@@ -13,19 +14,34 @@ public class Marites {
         System.out.println("I have a lot of stories to share, but first, how can I help you?");
         printSeparator();
     }
+    static void listTasks(ArrayList<String> tasks) {
+        for (int i = 1; i <= tasks.size(); ++i) {
+            System.out.printf("%d. %s%n", i, tasks.get(i-1));
+        }
+    }
     public static void main(String[] args) {
         printIntroduction();
 
         Scanner in = new Scanner(System.in);
+
+        ArrayList<String> tasks = new ArrayList<>();
+
         while (true) {
             String input = in.nextLine();
             String[] tokens = input.split("\\s");
             if (tokens[0].equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                printSeparator();
+                listTasks(tasks);
+                printSeparator();
+            } else {
+                tasks.add(input);
+
+                printSeparator();
+                System.out.println("added: " + input);
+                printSeparator();
             }
-            printSeparator();
-            System.out.println(input);
-            printSeparator();
         }
 
         printSeparator();
