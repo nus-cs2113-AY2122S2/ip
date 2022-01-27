b92341c8086a78b71d3916f1e35ec0f0dcaf64e2
Shyunyin <shyunyin321@gmail.com>
2022-01-27 10:01:26 +0800

    Add personalised Eliz.java

---
+++ src/main/java/Eliz.java
@@ -0,0 +1,15 @@
+public class Eliz {
+    public static void main(String[] args) {
+        String logo = " ____    __       __     ______ \n"
+                    + "|  __|  |  |     |  |   |___  /\n"
+                    + "| |__   |  |     |  |      / /  \n"
+                    + "| |__|  |  |     |  |     / /  \n"
+                    + "| |__   |  |__   |  |    / /__\n";
+                    + "|____|  |_____|  |__|   |_____|\n"
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Eliz");
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
