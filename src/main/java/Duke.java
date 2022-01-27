58978d6ee8b55232836e5e9e5ba36241f2423cd9
quitejasper <jasper.chia9@gmail.com>
2022-01-28 01:11:56 +0800

    added converse switch function that looks at user input and uses different methods based on that. added listTasks: to list all tasks on hand. added addList: to add tasks to the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,7 +9,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String[] list = new String[100];
+        int taskCount = 0;
+
         greet();
+        converse(list, taskCount);
         exit();
     }
 
@@ -27,5 +33,40 @@ public class Duke {
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
+    }
+
+    public static void converse(String[] list, int taskCount){
+        Scanner sc = new Scanner(System.in);
+        String response = sc.nextLine();
+        while (!(response.equals("bye"))){
+            switch(response){
+                case "list":
+                    listTasks(list, taskCount);
+                    break;
+                case "bye":
+                    return;
+                default:
+                    list = addList(list, taskCount, response);
+                    taskCount++;
+            }
+            response = sc.nextLine();
+        }
+
+    }
+
+    public static void listTasks(String[] list, int taskCount){
+        printLine();
+        for (int i = 0; i < taskCount; i++){
+            System.out.println(Integer.toString(i+1) + ". " + list[i]);
+        }
+        printLine();
+    }
+
+    public static String[] addList(String[] list, int taskCount, String response){
+        printLine();
+        list[taskCount] = response;
+        System.out.println("added: " + response);
+        printLine();
+        return list;
     }
 }
