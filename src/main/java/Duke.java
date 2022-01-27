fe8f3720801e4a07c4db89ad6e07223a73cbb6cc
wli-linda <wli62442@gmail.com>
2022-01-27 23:11:32 +0800

    Add echoing feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-/*         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-         System.out.println("Hello from\n" + logo); */
-
          String greet = "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+         System.out.println(greet);
+
+        String bye = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
-         System.out.println(greet);
+
+        int num = 1;
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (num == 1) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                num = 0;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        line + "\n" +
+                        "____________________________________________________________");
+            }
+        }
+
+        System.out.println(bye);
     }
 }
