6a63b1c342732de5bb4d78e888449939258bb298
HansHengGit <hans.hmk1@gmail.com>
2022-01-27 11:27:05 +0800

    level-1 greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Hage");
         System.out.println("What can I do for you?");
+
+        Scanner scan = new Scanner(System.in);
+        String inCommand = scan.nextLine();
+        while (!inCommand.equals("bye")) {
+            System.out.println(inCommand);
+            inCommand = scan.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
