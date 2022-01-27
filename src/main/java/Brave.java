8818d7e6f03c01d51c6b47dcc7450ac22d4a7450
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-01-27 22:54:26 +0800

    Week 3 Part 3

--- src/main/java/Brave.java
+++ src/main/java/Brave.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
+import java.lang.String;
 
-public class Duke {
+public class Brave {
     public static void main(String[] args) {
         String command;
         Scanner in = new Scanner(System.in);
-        String[] task = new String[100];
+        Task[] task = new Task[100];
         int numberOfTask = 0;
 
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello I'm Duke");
+        System.out.println("\tHello I'm Brave");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
 
         command = in.nextLine();
 
         while (!command.equals("bye")) {
-            if (command.equals("list")) {
+            if (command.split(" ")[0].equals("mark")) {
+                String[] words = command.split(" ");
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                task[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task[taskIndex].getStatusIcon() + " " + task[taskIndex].getDescription());
+            }
+
+            else if (command.split(" ")[0].equals("unmark")) {
+                String[] words = command.split(" ");
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                task[taskIndex].unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task[taskIndex].getStatusIcon() + " " + task[taskIndex].getDescription());
+            }
+
+            else if (command.equals("list")) {
                 System.out.println("\t____________________________________________________________");
                 for (int i=0; i<numberOfTask; i++) {
-                    System.out.println("\t" + (i+1) + "." + task[i]);
+                    System.out.println("\t" + (i+1) + "." + task[i].getStatusIcon() + " " + task[i].getDescription());
                 }
                 System.out.println("\t____________________________________________________________");
             }
@@ -26,7 +43,7 @@ public class Duke {
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\tadded: " + command);
                 System.out.println("\t____________________________________________________________");
-                task[numberOfTask] = command;
+                task[numberOfTask] = new Task(command);
                 numberOfTask++;
             }
 
