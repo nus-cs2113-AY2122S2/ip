28ad6a113f2ace4e2fa0b0e5036e42ca86d043f0
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-01-27 19:19:13 +0800

    Week 3 Part 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,23 +4,37 @@ public class Duke {
     public static void main(String[] args) {
         String command;
         Scanner in = new Scanner(System.in);
+        String[] task = new String[100];
+        int numberOfTask = 0;
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
 
         command = in.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
+            if (command.equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int i=0; i<numberOfTask; i++) {
+                    System.out.println("\t" + (i+1) + "." + task[i]);
+                }
+                System.out.println("\t____________________________________________________________");
+            }
+            else {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tadded: " + command);
+                System.out.println("\t____________________________________________________________");
+                task[numberOfTask] = command;
+                numberOfTask++;
+            }
+
             command = in.nextLine();
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye, Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye, Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
