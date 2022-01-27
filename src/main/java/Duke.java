cb81cf287c5bae782549739098f0e6b00957b240
chihyingho <User@DESKTOP-JLMCFN6>
2022-01-27 21:05:17 +0800

    Modify exit message to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,7 @@ public class Duke {
     }
 
     public static void exit() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
