65898c7cccf009c91a689985c8dbbc5d282d16ad
limjierui <jieruilim@u.nus.edu>
2022-01-27 22:34:49 +0800

    A - Classes

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public boolean markAsDone() {
+        return isDone = true;
+    }
+
+    public boolean unMark() {
+        return isDone = false;
+    }
+
+}
+
