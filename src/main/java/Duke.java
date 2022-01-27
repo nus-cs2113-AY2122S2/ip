25d1b25b8a4f8b4faca3ec58d46821bf54c107db
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-01-27 19:01:58 +0800

    week 3 part 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String command;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        command = in.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+            command = in.nextLine();
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye, Hope to see you again soon!");
         System.out.println("____________________________________________________________");
