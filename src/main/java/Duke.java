635c6298dfac76ca4ba707fb31978e24d677d456
wli-linda <wli62442@gmail.com>
2022-01-27 23:55:55 +0800

    Add Task class and mark as done feature
    Also, refactoring some code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,48 +2,77 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-         String greet = "____________________________________________________________\n" +
+/*         String greet = "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
-         System.out.println(greet);
+         System.out.println(greet);*/
 
-        String bye = "____________________________________________________________\n" +
+        printFormat(" Hello! I'm Duke\n" +
+                " What can I do for you?");
+
+/*        String bye = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                "____________________________________________________________";*/
 
         int num = 1;
         String line;
         Scanner in = new Scanner(System.in);
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int i = 0;
 
         while (num == 1) {
             line = in.nextLine();
-            if (line.equals("bye")) {
+            switch (line) {
+            case "bye":
                 num = 0;
-            } else {
-                if (line.equals("list")) {
-                    System.out.println("____________________________________________________________");
-                    for (int k = 0; k < i; k++) {
-                        System.out.println(Integer.toString(k+1) + ". " + list[k]);
-                    }
-                    System.out.println("____________________________________________________________\n");
+                printFormat(" Bye. Hope to see you again soon!\n");
+                break;
+            case "list":
+                System.out.println("____________________________________________________________");
+                for (int k = 0; k < i; k++) {
+                    Task curr = list[k];
+                    System.out.println(Integer.toString(k+1) + ". " +
+                            "[" + curr.getStatusIcon() + "] " + curr.description);
+                }
+                System.out.println("____________________________________________________________\n");
+                break;
+            default:
+                if (line.contains("unmark")) {
+                    markStatus(false, line, list);
+                } else if (line.contains("mark")) {
+                    markStatus(true, line, list);
                 } else {
-                    list[i] = line;
+                    list[i] = new Task(line);
                     i++;
                     printFormat("added: " + line);
                 }
             }
         }
-
-        System.out.println(bye);
     }
 
     public static void printFormat(String s) {
         System.out.println("____________________________________________________________\n" +
                 s + "\n" +
                 "____________________________________________________________");
+    }
+
+    public static void markStatus(Boolean mark, String line, Task[] list) {
+        try {
+            int taskNum = Integer.parseInt(line.split(" ", 0)[1]);
+            Task curr = list[taskNum - 1];
+            if (mark) {
+                curr.setDone(true);
+                printFormat(" Nice! I've marked this task as done:\n" +
+                        "[" + curr.getStatusIcon() + "] " + curr.description);
+            } else {
+                curr.setDone(false);
+                printFormat(" OK, I've marked this task as not done yet:\n" +
+                        "[" + curr.getStatusIcon() + "] " + curr.description);
+            }
+        } catch (Exception e) {
+            printFormat("Please mark / unmark a number that's in the list :')");
+        }
     }
 }
