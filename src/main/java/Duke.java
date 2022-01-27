c941a57e54be35312caf69416828a0f1da55659a
yuzhongng <yuzhongng@gmail.com>
2022-01-27 13:48:22 +0800

    Fixed coding standard violation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,6 +23,7 @@ public class Duke {
             if (allTasks.isEmpty()) {
                 if (!line.equals("list")) {
                     allTasks.add(new Task(line));
+
                     System.out.println("Added: " + String.format("%s", line));
                     System.out.println(endOfSection);
                 }
@@ -30,27 +31,33 @@ public class Duke {
                 if (line.equals("list")) {
                     for (int i = 0; i < allTasks.size(); ++i) {
                         Task currentTask = allTasks.get(i);
+
                         System.out.println(String.format("%d. [%s] %s", i + 1, currentTask.getStatusIcon(), currentTask.getTask()));
                     }
                     System.out.println(endOfSection);
                 } else if (line.equals("mark")) {
                     int pos = in.nextInt();
+
                     Task currentTask = allTasks.get(pos - 1);
                     currentTask.markAsDone();
+
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println(String.format("[%s] %s", currentTask.getStatusIcon(), currentTask.getTask()));
                     System.out.println(endOfSection);
                     in.nextLine();
                 } else if (line.equals("unmark")) {
                     int pos = in.nextInt();
+
                     Task currentTask = allTasks.get(pos - 1);
                     currentTask.unmark();
+
                     System.out.println("Ok, I've marked this task as not done yet:");
                     System.out.println(String.format("[%s] %s", currentTask.getStatusIcon(), currentTask.getTask()));
                     System.out.println(endOfSection);
                     in.nextLine();
                 } else {
                     allTasks.add(new Task(line));
+
                     System.out.println("Added: " + String.format("%s", line));
                     System.out.println(endOfSection);
                 }
