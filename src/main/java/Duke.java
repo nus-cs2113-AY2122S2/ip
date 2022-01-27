e6b064e5e361a81a62ddeed449464e9e62725b42
ElaineQT <wangqianteng@gmail.com>
2022-01-27 12:55:26 +0800

    add Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetStr = "\t____________________________________________________________\n" +
+                "\t Hello! I'm Maahes\n" +
+                "\t What can I do for you?\n";
+        String byeStr = "\t____________________________________________________________\n" +
+                "\t Bye. Hope to see you again soon!\n" +
+                "\t____________________________________________________________";
 
-        String greet = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        System.out.println(greetStr);
+        echo();
+        System.out.println(byeStr);
+    }
 
-        System.out.println(greet);
+    public static void echo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t Type \"bye\" to exit.");
+        String input = sc.nextLine();
+        while(!input.equals("bye")){
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + input);
+            System.out.println("\t____________________________________________________________");
+            input = sc.nextLine();
+        }
     }
 }
