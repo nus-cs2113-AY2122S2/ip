980b88fa2a8e6644a3435fb04a9ba550f56a5aca
z0723-julie <ZHAO0348@e.ntu.edu.sg>
2022-01-27 22:36:26 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
 // import libraries here
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-
+    private static ArrayList<String> descriptions = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,14 +22,30 @@ public class Duke {
         // get user input
         line = in.nextLine();
         while (!line.equals("bye")) {
+            if (line.equals("list")) {
+                showList();
+                line = in.nextLine();
+                continue;
+            }
             System.out.println("    ____________________________________________________________");
-            System.out.println("     " + line);
+            System.out.println("added: " + line);
+            descriptions.add(line);
             System.out.println("    ____________________________________________________________");
             line = in.nextLine();
         }
+        if (line.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     Bye. Hope to see you again soon!");
+            System.out.println("    ____________________________________________________________");
+        }
+    }
+
+    private static void showList() {
         System.out.println("    ____________________________________________________________");
-        System.out.println("     Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("     Here are the tasks in your list:");
+        for (int i = 0; i < descriptions.size(); i++) {
+            System.out.println(i+1 + "." + descriptions.get(i));
+        }
     }
 
     private static void greeting() {
