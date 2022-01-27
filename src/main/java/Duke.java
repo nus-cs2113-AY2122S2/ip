3d1192bed28571d0a1e257efdbdcbd388e104991
chihyingho <User@DESKTOP-JLMCFN6>
2022-01-27 21:45:47 +0800

    Add echo function to echo user's inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        echo();
         exit();
     }
 
@@ -15,6 +18,19 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        String lowerCaseUserInput = userInput.toLowerCase();
+        while (!lowerCaseUserInput.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+            userInput = in.nextLine();
+            lowerCaseUserInput = userInput.toLowerCase();
+        };
     }
 
     public static void exit() {
