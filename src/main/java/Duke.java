520b1c4b438c0c9f5804d4ccbecc995baaf4c378
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-01-28 01:11:04 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100]; //holds all tasks given
+        int taskCounter = 0; //counts number of tasks
         String greeting = "____________________________________________________________\n"
                 + " Hello! I'm Duke\n"
-                + " What can I do for you?\n\n"
+                + " What can I do for you?\n"
                 + "____________________________________________________________\n";
+
 
         System.out.println(greeting);
 
@@ -13,13 +16,22 @@ public class Duke {
             String line = sc.nextLine();
             switch (line) {
             case "bye":
-                System.out.println("____________________________________________________________\n");
-                System.out.println("     Bye; Don't restart me.");
+                System.out.println("____________________________________________________________");
+                System.out.println("  Bye; Don't restart me.");
                 System.out.println("____________________________________________________________\n");
                 System.exit(0);
-            default:
+            case "list":
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println("   " + (i + 1) + ". " + tasks[i]);
+                }
                 System.out.println("____________________________________________________________\n");
-                System.out.println("     " + line);
+                break;
+            default:
+                tasks[taskCounter] = line;
+                taskCounter++;
+                System.out.println("____________________________________________________________");
+                System.out.println("  added: " + line);
                 System.out.println("____________________________________________________________\n");
             }
         }
