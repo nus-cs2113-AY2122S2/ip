a4cbbeb7283bbc4c9b2674da26a66fa72cb2f4d1
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-01-28 01:03:21 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String greeting = "____________________________________________________________\n"
                 + " Hello! I'm Duke\n"
                 + " What can I do for you?\n\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + greeting);
+
+        System.out.println(greeting);
+
+        while (true) {
+            String line = sc.nextLine();
+            switch (line) {
+            case "bye":
+                System.out.println("____________________________________________________________\n");
+                System.out.println("     Bye; Don't restart me.");
+                System.out.println("____________________________________________________________\n");
+                System.exit(0);
+            default:
+                System.out.println("____________________________________________________________\n");
+                System.out.println("     " + line);
+                System.out.println("____________________________________________________________\n");
+            }
+        }
     }
 }
