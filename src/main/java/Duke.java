c16d014cd1717d2288ec7fee88ed0387e6ddb30c
z0723-julie <ZHAO0348@e.ntu.edu.sg>
2022-01-27 22:00:02 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+// import libraries here
+
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +11,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String greetings = " Hello! I'm Duke\n" + " What can I do for you?";
-        System.out.println("____________________________________________________________");
-        String exits = " Bye. Hope to see you again soon!";
-        System.out.println(greetings);
-        System.out.println("____________________________________________________________");
-        System.out.println(exits);
-        System.out.println("____________________________________________________________");
+        greeting();
+        echo();
+    }
+
+    private static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        // get user input
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + line);
+            System.out.println("    ____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void greeting() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
     }
 }
