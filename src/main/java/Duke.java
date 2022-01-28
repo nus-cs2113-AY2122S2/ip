e6cfbb5dbaeba1b65542ee282e41d933eaf96662
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-01-28 12:36:06 +0800

    Implement task management

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,22 @@
 import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        print("Hello from JRobo! I'm your personal assistant!\n\t" + "Nice to meet you. What can I do for you?");
+        print("Hello from JRobo! I'm your personal assistant!\n\tNice to meet you. What can I do for you?");
 
         String input;
 
         while (true) {
             input = scanner.nextLine();
+            if (input.startsWith("mark")) {
+                markTask(tasks, input);
+                continue;
+            }
+            if (input.startsWith("unmark")) {
+                unmarkTask(tasks, input);
+                continue;
+            }
             if (input.equals("list")) {
                 displayList(tasks);
                 continue;
@@ -16,7 +24,8 @@ public class Duke {
             if (input.equals("bye")) {
                 break;
             }
-            tasks.add(input);
+            Task task = new Task(input);
+            tasks.add(task);
             print("added: " + input);
         }
 
@@ -30,12 +39,55 @@ public class Duke {
         + "\n\t____________________________________________________________");
     }
 
-    public static void displayList(List<String> tasks) {
+    public static void displayList(List<Task> tasks) {
         System.out.println("\t____________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
         int index = 1;
-        for (String s : tasks) {
-            System.out.println("\t" + index++ + ". " + s);
+        for (Task t : tasks) {
+            System.out.println("\t" + index++ + ".[" + t.getStatusIcon() + "] " + t.getDescription());
         }
         System.out.println("\t____________________________________________________________");
+    }
+
+    public static void markTask(List<Task> tasks, String input) {
+        String numberString = input.substring(4).trim();
+        try {
+            int taskNumber = Integer.parseInt(numberString);
+            Task task = tasks.get(taskNumber - 1);
+            if (task.getStatusIcon().equals("X")) {
+                print("This task is already marked");
+                return;
+            }
+            task.markAsDone();
+            print("Nice! I've marked this task as done:\n\t\t[" + task.getStatusIcon() + "] " + task.getDescription()); 
+            return;
+        } catch(NumberFormatException e) {
+            print("Invalid command");
+            return;
+        } catch(IndexOutOfBoundsException e) {
+            print("Please enter a valid task number to mark");
+            return;
+        }  
+    }
+
+    public static void unmarkTask(List<Task> tasks, String input) {
+        String numberString = input.substring(6).trim();
+        try {
+            int taskNumber = Integer.parseInt(numberString);
+            Task task = tasks.get(taskNumber - 1);
+            if (!task.getStatusIcon().equals("X")) {
+                print("This task is already unmarked");
+                return;
+            }
+            task.markAsUndone();
+            print("Nice! I've marked this task as undone:\n\t\t[" + task.getStatusIcon() + "] " + task.getDescription()); 
+            return;
+        } catch(NumberFormatException e) {
+            print("Invalid command");
+            return;
+        } catch(IndexOutOfBoundsException e) {
+            print("Please enter a valid task number to unmark");
+            return;
+        }  
     }
 }
