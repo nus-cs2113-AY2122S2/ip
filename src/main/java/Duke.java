2b53327df9a83f0e535d3352f54c2663b36e2001
unknown <brendanawjang@gmail.com>
2022-01-28 11:21:35 +0800

    Add exception handling for todo and invalid commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,46 +13,54 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         ArrayList<Task> store = new ArrayList<Task>();
-
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < store.size(); i++) {
-                    System.out.println(i + 1 + ". " + store.get(i).getTask());
+            try {
+                if (userInput.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < store.size(); i++) {
+                        System.out.println(i + 1 + ". " + store.get(i).getTask());
+                    }
+                } else if (userInput.startsWith("mark")) {
+                    int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
+                    Task currentTask = store.get(taskIndex - 1);
+                    currentTask.setDone(true);
+                    System.out.println(currentTask.getTask());
+                } else if (userInput.startsWith("unmark")) {
+                    int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
+                    Task currentTask = store.get(taskIndex - 1);
+                    currentTask.setDone(false);
+                    System.out.println(currentTask.getTask());
+                } else if (userInput.startsWith("todo")) {
+                    try {
+                        if (userInput.split("todo").length <= 1) {
+                            throw new DukeException();
+                        }
+                        String task = userInput.split("todo")[1].trim();
+                        ToDo newTask = new ToDo(task);
+                        store.add(newTask);
+                        printTask(newTask, store.size());
+                    } catch (DukeException e) {
+                        System.out.println("The description of a todo cannot be empty.");
+                    }
+                } else if (userInput.startsWith("deadline")) {
+                    String[] processedString = userInput.split("/by");
+                    String by = processedString[1].trim();
+                    String task = processedString[0].split("deadline")[1].trim();
+                    Deadline newTask = new Deadline(task, by);
+                    store.add(newTask);
+                    printTask(newTask, store.size());
+                } else if (userInput.startsWith("event")) {
+                    String[] processedString = userInput.split("/at");
+                    String at = processedString[1].trim();
+                    String task = processedString[0].split("event")[1].trim();
+                    Event newTask = new Event(task, at);
+                    store.add(newTask);
+                    printTask(newTask, store.size());
+                } else {
+                    throw new DukeException();
                 }
-            } else if (userInput.startsWith("mark")) {
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
-                Task currentTask = store.get(taskIndex - 1);
-                currentTask.setDone(true);
-                System.out.println(currentTask.getTask());
-            } else if (userInput.startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
-                Task currentTask = store.get(taskIndex - 1);
-                currentTask.setDone(false);
-                System.out.println(currentTask.getTask());
-            } else if (userInput.startsWith("todo")) {
-                String task = userInput.split("todo")[1].trim();
-                ToDo newTask = new ToDo(task);
-                store.add(newTask);
-                printTask(newTask, store.size());
-            } else if (userInput.startsWith("deadline")) {
-                String[] processedString = userInput.split("/by");
-                String by = processedString[1].trim();
-                String task = processedString[0].split("deadline")[1].trim();
-                Deadline newTask = new Deadline(task, by);
-                store.add(newTask);
-                printTask(newTask, store.size());
-            } else if (userInput.startsWith("event")) {
-                String[] processedString = userInput.split("/at");
-                String at = processedString[1].trim();
-                String task = processedString[0].split("event")[1].trim();
-                Event newTask = new Event(task, at);
-                store.add(newTask);
-                printTask(newTask, store.size());
-            } else {
-                Task newTask = new Task(userInput);
-                store.add(newTask);
-                System.out.println("added: " + userInput);
+            } catch (DukeException e) {
+                System.out.println("I'm sorry, but I don't know what that means");
             }
             userInput = sc.nextLine();
         }
