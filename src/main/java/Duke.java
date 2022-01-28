a571204131088e6af3d66a53946ff70148af5a3a
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-01-28 15:25:05 +0800

    Implement Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,6 +8,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            String exitMessage = "bye";
+            if(line.equals(exitMessage)){
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            }
+            System.out.println(line);
+        }
     }
 }
