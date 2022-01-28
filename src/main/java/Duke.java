9e0198d1237226da6d6dcd45666cffdab4ae34b3
DESKTOP-91KNFFF\LI HAO <yeaoming@gmail.com>
2022-01-28 10:23:12 +0800

    Add Increment: Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
 
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
     private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+    private static final int MAX_NUM_OF_TASKS = 100;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -20,14 +23,29 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String userInput;
+        String[] tasks = new String[MAX_NUM_OF_TASKS];
+        int taskCount = 0;
 
         do {
             userInput = in.nextLine();
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println(" " + userInput);
-            System.out.println(HORIZONTAL_LINE);
+
+            if (userInput.equalsIgnoreCase(LIST_COMMAND)) {
+                System.out.println(HORIZONTAL_LINE);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(HORIZONTAL_LINE);
+            }
+            else if (userInput.equalsIgnoreCase(EXIT_COMMAND) == false) {
+                tasks[taskCount] = userInput;
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println(" added: " + userInput);
+                System.out.println(HORIZONTAL_LINE);
+                taskCount++;
+            }
         } while (!userInput.equalsIgnoreCase(EXIT_COMMAND));
 
+        System.out.println(HORIZONTAL_LINE);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
     }
