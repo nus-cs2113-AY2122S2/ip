59d058c52ee4d248128bb3f9b31fcbc04f2bbf06
Daryl <ithinkmynameisdaryl@gmail.com>
2022-01-28 20:09:17 +0800

    Add Deadline, Event, ToDo class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,21 @@
+public class Deadline extends Task{
+    protected String deadline;
+
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + deadline + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,21 @@
+public class Event extends Task{
+    protected String date;
+
+    public Event(String description, String date) {
+        super(description);
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + date + ")";
+    }
+}
