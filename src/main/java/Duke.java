1fa4646e7b1c075b8913252f892c230342991d41
Daryl <ithinkmynameisdaryl@gmail.com>
2022-01-28 19:39:02 +0800

    Replace if-branch with switch and minor update to responses

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,31 +16,39 @@ public class Duke {
             String[] words = input.split(" ");
             String command = words[0];
 
-            if (command.equals("bye")) {
+            switch (command) {
+            case "bye":
                 System.out.println("Have a nice day!");
-                break;
-            } else if (command.equals("list")) {
+                System.exit(0);
+            case "list":
+                if (taskStore.isEmpty()) {
+                    System.out.println("404 Not Found");
+                }
                 for (int i = 0; i < taskStore.size(); i++) {
                     Task currentTask = taskStore.get(i);
                     System.out.println((i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
                 }
-            } else if (command.equals("mark") || command.equals("unmark")) {
+                break;
+            case "mark":
+                // Fallthrough
+            case "unmark":
                 int index = Integer.parseInt(words[1]) - 1;
                 Task currentTask = taskStore.get(index);
                 if (command.equals("mark")) {
-                    System.out.println("Sure thing!");
+                    System.out.println("Okie Dokie!");
                     currentTask.setAsDone();
                 } else {
-                    System.out.println("I'm sorry to hear that..");
+                    System.out.println("Oh no! Anyways..");
                     currentTask.setAsNotDone();
                 }
                 System.out.println("\t[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
-            } else {
+                break;
+            default:
                 Task newTask = new Task(input);
                 taskStore.add(newTask);
                 System.out.println("added: " + input);
+                break;
             }
-
             System.out.println("----------------------------------");
         }
     }
