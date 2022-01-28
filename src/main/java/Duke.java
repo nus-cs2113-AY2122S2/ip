2ef4b256c4af9a4df9279189a2fb99a15727ac1a
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-01-28 11:47:36 +0800

    Implement a skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n"
-                + "Hello from JRobo! I'm your personal assistant!\n"
-                + "Nice to meet you. What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        print("Hello from JRobo! I'm your personal assistant!\n\t" + "Nice to meet you. What can I do for you?");
+
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            print(input);
+        }
+
+        print("Bye. Hope to see you again soon!");
+        scanner.close();
+    }
+
+    public static void print(String s) {
+        System.out.println("\t____________________________________________________________\n\t" 
+        + s
+        + "\n\t____________________________________________________________");
     }
 }
