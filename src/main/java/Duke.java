e5086fa65f2fcce0f0816e1834e5a0f26e780236
Kang Hwee Thern <kanghweethern@gmail.com>
2022-01-28 15:06:50 +0800

    Added Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,12 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        
+
         System.out.println("____________________________________________________________"
-                        + "Hello! I'm Duke"
-                        + "What can I do for you?"
-                        + "____________________________________________________________"
-                        + "Bye. Hope to see you again soon!"
-                        + "____________________________________________________________");
+                + "\nHello! I'm Dook!"
+                + "\nWhat can I do for you?"
+                + "\n____________________________________________________________");
+        Scanner scannerInput = new Scanner(System.in);
+        while (true) {
+            String userInput = scannerInput.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        scannerInput.close();
+
+        System.out.println("Bye. Hope to see you again soon!"
+                + "\n____________________________________________________________");
     }
 }
