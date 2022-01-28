3fee1e916d3da3a9ac7be18863f412b972670d8a
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-01-28 12:00:10 +0800

    Implement task history

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         print("Hello from JRobo! I'm your personal assistant!\n\t" + "Nice to meet you. What can I do for you?");
 
@@ -8,10 +9,15 @@ public class Duke {
 
         while (true) {
             input = scanner.nextLine();
+            if (input.equals("list")) {
+                displayList(tasks);
+                continue;
+            }
             if (input.equals("bye")) {
                 break;
             }
-            print(input);
+            tasks.add(input);
+            print("added: " + input);
         }
 
         print("Bye. Hope to see you again soon!");
@@ -22,5 +28,14 @@ public class Duke {
         System.out.println("\t____________________________________________________________\n\t" 
         + s
         + "\n\t____________________________________________________________");
+    }
+
+    public static void displayList(List<String> tasks) {
+        System.out.println("\t____________________________________________________________");
+        int index = 1;
+        for (String s : tasks) {
+            System.out.println("\t" + index++ + ". " + s);
+        }
+        System.out.println("\t____________________________________________________________");
     }
 }
