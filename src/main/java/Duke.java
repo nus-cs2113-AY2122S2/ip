73b9bf0b851ff0ed214a90e9b4d93154f1214fb9
DESKTOP-91KNFFF\LI HAO <yeaoming@gmail.com>
2022-01-28 14:32:15 +0800

    Update Duke to use task manager

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,6 @@ public class Duke {
     private static final String LIST_COMMAND = "list";
     private static final String MARK_COMMAND = "mark";
     private static final String UNMARK_COMMAND = "unmark";
-    private static final int MAX_NUM_OF_TASKS = 100;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -25,11 +24,10 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String userInput;
-        String[] tasks = new String[MAX_NUM_OF_TASKS];
-        int taskCount = 0;
+        TaskManager t = new TaskManager();
+
         String firstWord = "";
         String index;
-        String currentTask;
 
         do {
             userInput = in.nextLine();
@@ -39,34 +37,23 @@ public class Duke {
 
             if (userInput.equalsIgnoreCase(LIST_COMMAND)) {
                 System.out.println(HORIZONTAL_LINE);
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
-                }
+                t.listTasks();
                 System.out.println(HORIZONTAL_LINE);
             }
             else if (firstWord.equalsIgnoreCase(MARK_COMMAND)) {
                 index = userInput.substring(userInput.indexOf(" ") + 1);
-                currentTask = tasks[Integer.parseInt(index) - 1].substring(4);
-                tasks[Integer.parseInt(index) - 1] = "[X] " + currentTask;
-                System.out.println(" Nice! I've marked this task as done:");
-                System.out.println("   " + tasks[Integer.parseInt(index) - 1]);
+                t.markAsDone(Integer.parseInt(index) - 1);
                 System.out.println(HORIZONTAL_LINE);
             }
             else if (firstWord.equalsIgnoreCase(UNMARK_COMMAND)) {
                 index = userInput.substring(userInput.indexOf(" ") + 1);
-                currentTask = tasks[Integer.parseInt(index) - 1].substring(4);
-                tasks[Integer.parseInt(index) - 1] = "[ ] " + currentTask;
-                System.out.println(" OK, I've marked this task as not done yet:");
-                System.out.println("   " + tasks[Integer.parseInt(index) - 1]);
+                t.unmarkAsNotDone(Integer.parseInt(index) - 1);
                 System.out.println(HORIZONTAL_LINE);
             }
             else if (userInput.equalsIgnoreCase(EXIT_COMMAND) == false) {
-                tasks[taskCount] = "[ ] " + userInput;
                 System.out.println(HORIZONTAL_LINE);
-                System.out.println(" added: " + userInput);
+                t.addTask(userInput);
                 System.out.println(HORIZONTAL_LINE);
-                taskCount++;
             }
         } while (!userInput.equalsIgnoreCase(EXIT_COMMAND));
 
