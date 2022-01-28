5f12f60898d8462630b08fb5b19eb40561f0fb62
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-01-28 15:45:26 +0800

    Implement Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void addList(String[] list, int listCounter, String line) {
+        list[listCounter] = line;
+    }
+
+    public static void printList(String[] list, int listCounter) {
+        for (int i = 0; i < listCounter; i++) {
+            int index = i + 1;
+            System.out.println(index + ". " + list[i]);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,16 +20,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?\n");
+        String[] list = new String[100];
+        int listCounter = 0;
         while (true) {
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             String exitMessage = "bye";
+            String printMessage = "list";
             if(line.equals(exitMessage)){
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
             }
-            System.out.println(line);
+            if(line.equals(printMessage)){
+                printList(list, listCounter);
+                continue;
+            }
+            addList(list, listCounter, line);
+            listCounter++;
+            System.out.println("added: " + line);
         }
     }
 }
