a7af81db0bac528021e5ed17cc6c76bb9966058e
Joshua <joshuajr13@gmail.com>
2022-01-29 02:56:08 +0800

    Update Duke to include inheritance

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,105 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        printWelcomeMessage();
+        ArrayList<Task> taskList = new ArrayList<Task>();
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println("-----------------------------");
+            if (userInput.equals("list")) {
+                displayTaskListMenu();
+                displayAllTasks(taskList);
+            } else if (userInput.startsWith("unmark")) {
+                Task task = getTask(userInput,taskList);
+                task.markAsNotDone();
+                System.out.println(task);
+            } else if (userInput.startsWith("mark")) {
+                Task task = getTask(userInput,taskList);
+                task.markAsDone();
+                System.out.println(task);
+            } else if (userInput.startsWith("deadline")) {
+                Deadline deadline = getDeadline(userInput);
+                taskList.add(deadline);
+                System.out.println(deadline);
+                printTotalNumberOfTasks(taskList);
+            } else if (userInput.startsWith("event")) {
+                Event event = getEvent(userInput);
+                taskList.add(event);
+                System.out.println(event);
+                printTotalNumberOfTasks(taskList);
+            } else {
+                Todo todo = getTodo(userInput);
+                taskList.add(todo);
+                System.out.println(todo);
+                printTotalNumberOfTasks(taskList);
+            }
+            userInput = sc.nextLine();
+        }
+        printGoodbyeMessage();
+    }
+
+    private static void printTotalNumberOfTasks(ArrayList<Task> taskList) {
+        System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+    }
+
+    private static Todo getTodo(String userInput) {
+        String description = userInput.split("todo")[1].trim();
+        Todo todo = new Todo(description);
+        return todo;
+    }
+
+    private static Event getEvent(String userInput) {
+        String[] tokenArray = stringToToken(userInput,"/at");
+        String duration = tokenArray[1].trim();
+        String description = tokenArray[0].split("event")[1].trim();
+        Event event = new Event(description,duration);
+        return event;
+    }
+
+    private static Deadline getDeadline(String userInput) {
+        String[] tokenArray = stringToToken(userInput,"/by");
+        String by = tokenArray[1].trim();
+        String description = tokenArray[0].split("deadline")[1].trim();
+        Deadline deadline = new Deadline(description,by);
+        return deadline;
+    }
+
+    private static Task getTask(String userInput,ArrayList<Task> taskList) {
+        int taskIndex = getTaskIndex(userInput);
+        return taskList.get(taskIndex);
+    }
+
+    private static int getTaskIndex(String userInput) {
+        String[] tokenArray = stringToToken(userInput," ");
+        return Integer.valueOf(tokenArray[1]) - 1; //index is raw value - 1
+    }
+
+    private static String[] stringToToken(String userInput,String delimiter) {
+        return userInput.split(delimiter);
+    }
+
+    private static void displayAllTasks(ArrayList<Task> taskList) {
+        for (Task task : taskList) {
+            System.out.print(task.getTaskId() + ".");
+            System.out.println(task);
+        }
+        System.out.println("-----------------------------");
+    }
+
+    private static void displayTaskListMenu() {
+        System.out.println("Olivia presents you a list of tasks for you to do:");
+    }
+
+    private static void printGoodbyeMessage() {
+        System.out.println("-----------------------------");
+        System.out.println("Bye. Hope to see you again soon! With Love, Olivia");
+        System.out.println("-----------------------------");
+    }
+
+    private static void printWelcomeMessage() {
         String botLogo = "  ___                             __  \n"
                        + "|  _  |  |   |  \\        / |     /  \\ \n"
                        + "| | | |  |   |   \\      /  |    / _  \\ \n"
@@ -14,53 +113,6 @@ public class Duke {
         System.out.println("-----------------------------");
         System.out.println("Greetings! I'm Olivia, your lovely personal assistant.");
         System.out.println("What can Olivia do for you my love?");
-        System.out.println("-----------------------------");
-        String userInput;
-        ArrayList<Task> taskList=new ArrayList<Task>();
-
-        do {
-            Scanner sc = new Scanner(System.in);
-            userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
-                break;
-            }
-            else if (userInput.equals("list")) {
-                System.out.println("-----------------------------");
-                System.out.println("Olivia presents you a list of tasks for you to do:");
-                for (Task task : taskList) {
-                    System.out.print(task.getTaskId()+".");
-                    System.out.print("["+task.getTaskStatus()+"] ");
-                    System.out.println(task.getDescription());
-                }
-                System.out.println("-----------------------------");
-            }
-            else if (userInput.contains("mark")) {
-                System.out.println("-----------------------------");
-                String[] arraySplit = userInput.split(" "); //determine raw value of task number to mark/unmark
-                int indexAlter = Integer.valueOf(arraySplit[1]) -1; //index of description to mark/unmark
-                if (userInput.contains("un")) {
-                    taskList.get(indexAlter).markAsNotDone();
-                    System.out.println("Hey!!! Please get the task done.");
-                    System.out.println("    [" + taskList.get(indexAlter).getTaskStatus() + "] " + taskList.get(indexAlter).getDescription());
-                }
-                else {
-                    taskList.get(indexAlter).markAsDone();
-                    System.out.println("Olivia thanks you for completing the task! :)");
-                    System.out.println("    [" + taskList.get(indexAlter).getTaskStatus() + "] " + taskList.get(indexAlter).getDescription());
-                }
-                System.out.println("-----------------------------");
-            }
-            else {
-                Task task = new Task(userInput); //create new task
-                taskList.add(task); //append task to arraylist
-                System.out.println("-----------------------------");
-                System.out.println("added: " + userInput);
-                System.out.println("-----------------------------");
-
-            }
-        } while(!userInput.equals("bye"));
-        System.out.println("-----------------------------");
-        System.out.println("Bye. Hope to see you again soon! With Love, Olivia");
         System.out.println("-----------------------------");
     }
 }
