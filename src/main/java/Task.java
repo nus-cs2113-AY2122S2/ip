62f7c5fbd6bcf0ad271ac4c664c0474ca582841c
DESKTOP-91KNFFF\LI HAO <yeaoming@gmail.com>
2022-01-28 14:31:23 +0800

    Add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+    public void unmarkAsNotDone() {
+        isDone = false;
+    }
+}
