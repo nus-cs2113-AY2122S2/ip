09e68cad952dc3f305126514bb412c814f25820c
Shyunyin <shyunyin321@gmail.com>
2022-01-28 14:14:44 +0800

    Edit Eliz.java to greet echo and exit

--- src/main/java/Eliz.java
+++ src/main/java/Eliz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Eliz {
     public static void main(String[] args) {
         String logo = " ____    __       __     ______ \n"
@@ -9,7 +11,14 @@ public class Eliz {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Eliz");
         System.out.println("What can I do for you?");
-        System.out.println(" ");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        String bye = "bye";
+        while (!line.equals(bye)) { //while command to end is not entered
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
