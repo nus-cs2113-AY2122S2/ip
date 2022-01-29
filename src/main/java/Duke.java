b62b49f4baa167203992e99673ec05b8e6e2ada9
Kang Hwee Thern <kanghweethern@gmail.com>
2022-01-30 00:48:34 +0800

    Added Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Duke {
+
+    public static final String COMMAND_FORMAT = "(\\S+)(.*)";
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,25 +18,52 @@ public class Duke {
                 + "\nHello! I'm Dook!"
                 + "\nWhat can I do for you?"
                 + "\n____________________________________________________________");
-        String[] userInputList = new String[100];
+        //this setup assumes the user is being cooperative
+        String[] taskList = new String[100];
+        Boolean[] markedTaskList = new Boolean[100];
         int userInputIndex = 0;
         Scanner scannerInput = new Scanner(System.in);
         boolean loopInput = true;
+        int markIndex;
         while (loopInput) {
             String userInput = scannerInput.nextLine();
-            switch (userInput) {
+            //split userinput by regex
+            Pattern commandPattern = Pattern.compile(COMMAND_FORMAT);
+            Matcher commandMatcher = commandPattern.matcher(userInput);
+            commandMatcher.find();
+            String commandInput = commandMatcher.group(1);
+            switch (commandInput) {
             case "bye":
                 loopInput = false;
                 break;
             case "list":
                 System.out.println("____________________________________________________________\n");
+                System.out.println("Here are the tasks in your list");
                 for (int i = 0; i<userInputIndex; i++) {
-                    System.out.println(String.valueOf(i+1)+". "+userInputList[i]);
+                    String printString = String.format("%d. [%c] %s", i+1, markedTaskList[i]?'X':' ', taskList[i]);
+                    System.out.println(printString);
                 }
                 System.out.println("____________________________________________________________\n");
                 break;
+            case "mark":
+                markIndex = Integer.valueOf(commandMatcher.group(2).strip());
+                markedTaskList[markIndex] = true;
+                System.out.println("____________________________________________________________"
+                        + "\nNice! I've marked this task as done:"
+                        + "\n[X] " + taskList[markIndex]
+                        + "\n____________________________________________________________");
+                break;
+            case "unmark":
+                markIndex = Integer.valueOf(commandMatcher.group(2).strip());
+                markedTaskList[markIndex] = false;
+                System.out.println("____________________________________________________________"
+                        + "\nOK, I've marked this task as not done yet:"
+                        + "\n[ ] " + taskList[markIndex]
+                        + "\n____________________________________________________________");
+                break;
             default:
-                userInputList[userInputIndex] = userInput;
+                taskList[userInputIndex] = userInput;
+                markedTaskList[userInputIndex] = false;
                 System.out.println("____________________________________________________________"
                         + "\nadded: " + userInput
                         + "\n____________________________________________________________");
@@ -42,7 +73,8 @@ public class Duke {
         }
         scannerInput.close();
 
-        System.out.println("Bye. Hope to see you again soon!"
+        System.out.println("____________________________________________________________"
+                + "\nBye. Hope to see you again soon!"
                 + "\n____________________________________________________________");
     }
 }
