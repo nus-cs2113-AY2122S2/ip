d98427fcaf041b2f4fe7a80a95d524f2916a87f2
Lester Sim <lester.sim@u.nus.edu>
2022-01-29 10:51:04 +0800

    Added Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,43 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] commandHistory = new String[100];
+        int numCommands = 0;
+        boolean isLoop = true;
+
         System.out.println("   ____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        while (true) {
+        while (isLoop) {
             Scanner sc = new Scanner(System.in);
             String command = sc.nextLine();
 
-            if (command.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
+            switch (command) {
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < numCommands; i++) {
+                        System.out.println(String.format("%d. %s", i + 1, commandHistory[i]));
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "bye":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    isLoop = false;
+                    break;
+                default:
+                    commandHistory[numCommands] = command;
+                    numCommands += 1;
+
+                    System.out.println("____________________________________________________________");
+                    System.out.println(String.format("added: %s", command));
+                    System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
         }
-
     }
 }
