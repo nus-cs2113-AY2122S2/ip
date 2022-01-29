3fe9fd67280bfb650b14109c11dd501cfea30e28
Yzkkk <yangzikun0511@gmail.com>
2022-01-29 23:38:00 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,23 +13,31 @@ public class Duke {
                 "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
         String bye = "bye";
+        String list = "list";
         System.out.println(greet);
 
         String instruction;
+        String[] instructions = new String[100];
 
-
-        for(int i = 0; i < 99999; i++) { // can have 99999 echoes
+        for(int i = 0; i < 100; i++) { // can have 100 tasks
             Scanner in = new Scanner(System.in);
             //System.out.print("Type something: ");
             instruction = in.nextLine();
-            
-            boolean isSame = instruction.equals(bye); //if true then exit
 
-            if(isSame) {
+            boolean isBye = instruction.equals(bye); //if true then exit
+            boolean isList = instruction.equals(list);
+            instructions[i] = instruction;
+
+            if(isBye) {
                 System.out.println(exit);
                 break;
+            } else if(isList) {
+                for(int j = 0; j < 100 && instructions[j + 1] != null; j++) {
+                    System.out.println((j + 1) + ". " + instructions[j]);
+                }
             } else {
-                System.out.println(instruction);
+                //instructions[i] = instruction;
+                System.out.println("added: " + instruction);
             }
         }
     }
