42305900e84bb0751c4507e56eac1868135709af
yzkkk <yangzikun0511@gmail.com>
2022-01-29 17:21:43 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,8 +10,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
         String greet = "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!\n";
+                "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        String bye = "bye";
         System.out.println(greet);
+
+        String instruction;
+
+
+        for(int i = 0; i < 99999; i++) { // can have 99999 echoes
+            Scanner in = new Scanner(System.in);
+            //System.out.print("Type something: ");
+            instruction = in.nextLine();
+            
+            boolean isSame = instruction.equals(bye); //if true then exit
+
+            if(isSame) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(instruction);
+            }
+        }
     }
 }
