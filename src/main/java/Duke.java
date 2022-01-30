6025acaed7d59897d02c4d770e1370ced05db7df
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-01-30 11:33:01 +0800

    Follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,55 +1,66 @@
-import java.awt.*;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void addList(Task[] list, int listCounter, String line) {
-        list[listCounter] = new Task(line);
-    }
+    public static final String EXIT_MESSAGE = "bye";
+    public static final String PRINT_MESSAGE = "list";
+    public static final String MARK_MESSAGE = "mark";
+    public static final String UNMARK_MESSAGE = "unmark";
 
-    public static void printList(Task[] list, int listCounter) {
-        for (int i = 0; i < listCounter; i++) {
-            int index = i + 1;
-            System.out.println(index + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
-        }
-    }
-
-    public static void main(String[] args) {
+    private static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?\n");
-        Task[] list = new Task[100];
-        int listCounter = 0;
-        while (true) {
-            String line;
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
-            String exitMessage = "bye";
-            String printMessage = "list";
-            String markMessage = "mark";
-            String unmarkMessage = "unmark";
-            if(line.equals(exitMessage)){
-                System.out.println("Bye. Hope to see you again soon!\n");
-                break;
-            }
-            if(line.equals(printMessage)){
-                printList(list, listCounter);
-                continue;
-            }
-            if(line.split(" ")[0].equals(markMessage)) {
-                list[Integer.parseInt(line.split(" ")[1]) - 1].markAsDone();
-                continue;
-            }
-            if(line.split(" ")[0].equals(unmarkMessage)) {
-                list[Integer.parseInt(line.split(" ")[1]) - 1].markAsUndone();
-                continue;
-            }
-            addList(list, listCounter, line);
-            listCounter++;
-            System.out.println("added: " + line);
+    }
+
+    public static void printList(Task[] list, int listCounter) {
+        for (int i = 0; i < listCounter; i++) {
+            int listIndex = i + 1;
+            System.out.println(listIndex + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
         }
     }
+
+    public static int getTaskIndex(String userInput) {
+        String taskNumber = userInput.split(" ")[1];
+        int taskIndex = Integer.parseInt(taskNumber) - 1;
+        return taskIndex;
+    }
+
+    public static void addList(Task[] list, int listCounter, String line) {
+        list[listCounter] = new Task(line);
+        System.out.println("added: " + line);
+    }
+
+    private static void acceptInput() {
+        Task[] list = new Task[100];
+        int listCounter = 0;
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+        while(!userInput.equals(EXIT_MESSAGE)){
+            if (userInput.equals(PRINT_MESSAGE)) {
+                printList(list, listCounter);
+            } else if (userInput.startsWith(MARK_MESSAGE)) {
+                int taskIndex = getTaskIndex(userInput);
+                list[taskIndex].markAsDone();
+            } else if (userInput.startsWith(UNMARK_MESSAGE)) {
+                int taskIndex = getTaskIndex(userInput);
+                list[taskIndex].markAsUndone();
+            } else {
+                addList(list, listCounter, userInput);
+                listCounter++;
+            }
+            userInput = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    public static void main(String[] args) {
+        printWelcomeMessage();
+        acceptInput();
+    }
+
 }
