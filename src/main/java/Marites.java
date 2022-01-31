5ea6dbe3f738e311d6ec1d0a4e29cd591a6a4a97
Dan Baterisna <danbaterisna@u.nus.edu>
2022-02-01 01:58:25 +0800

    Extract string constants

--- src/main/java/Marites.java
+++ src/main/java/Marites.java
@@ -5,10 +5,30 @@ import static java.lang.Integer.parseInt;
 
 public class Marites {
 
+    /* STRING CONSTANTS */
+
+    // Logo found in https://emojicombos.com/kaomoji
+    private static final String INTRO_MESSAGE = """
+            (งツ)ว
+            "Hi, I'm Marites! I've heard so many things about you!
+            "I have a lot of stories to share, but first, how can I help you?
+            """;
+    private static final String EXIT_MESSAGE = "See you next time!";
+    private static final String COMMAND_EXIT = "bye";
+    private static final String COMMAND_LIST = "list";
+    private static final String COMMAND_MARK = "mark";
+    private static final String COMMAND_UNMARK = "unmark";
+    private static final String LIST_TASK_ITEM_FORMAT_STRING = "%d. %s%n";
+    private static final String ADD_TASK_FORMAT_STRING = "added: %s%n";
+    private static final String MARK_DONE_MESSAGE = "Good job on getting this done!";
+    private static final String MARK_UNDONE_MESSAGE = "Okay, I've marked this as not yet done:";
+    private static final String SET_TASK_STATUS_FORMAT_STRING = "%s%n    %s%n";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     /** The task list being tracked by Marites. */
     private static final ArrayList<Task> tasks = new ArrayList<>();
+
 
     public static void main(String[] args) {
         printIntroduction();
@@ -32,19 +52,14 @@ public class Marites {
      * Prints an introductory message.
      */
     private static void printIntroduction() {
-        // Found in https://emojicombos.com/kaomoji
-        String logo = "(งツ)ว";
-
-        System.out.println(logo);
-        System.out.println("Hi, I'm Marites! I've heard so many things about you!");
-        System.out.println("I have a lot of stories to share, but first, how can I help you?");
+        System.out.println(INTRO_MESSAGE);
         printSeparator();
     }
     /**
      * Prints an exit message.
      */
     private static void printExitMessage() {
-        System.out.println("See you next time!");
+        System.out.println(EXIT_MESSAGE);
         printSeparator();
     }
 
@@ -65,13 +80,13 @@ public class Marites {
      */
     private static String processUserCommand(String userInput) {
         String[] tokens = userInput.split("\\s");
-        if (userInput.equals("bye")) {
+        if (userInput.equals(COMMAND_EXIT)) {
             return executeExit();
-        } else if (userInput.equals("list")) {
+        } else if (userInput.equals(COMMAND_LIST)) {
             return executeListTasks();
-        } else if (tokens[0].equals("mark")) {
+        } else if (tokens[0].equals(COMMAND_MARK)) {
             return executeSetTaskStatus(tokens, true);
-        } else if (tokens[0].equals("unmark")) {
+        } else if (tokens[0].equals(COMMAND_UNMARK)) {
             return executeSetTaskStatus(tokens, false);
         } else {
             return executeAddTask(userInput);
@@ -92,7 +107,7 @@ public class Marites {
     private static String executeListTasks() {
         StringBuilder taskList = new StringBuilder();
         for (int i = 1; i <= tasks.size(); ++i) {
-            String task = String.format("%d. %s%n", i, tasks.get(i-1));
+            String task = String.format(LIST_TASK_ITEM_FORMAT_STRING, i, tasks.get(i-1));
             taskList.append(task);
         }
         return taskList.toString();
@@ -105,7 +120,7 @@ public class Marites {
      */
     private static String executeAddTask(String userInput) {
         tasks.add(new Task(userInput));
-        return String.format("added: %s%n", userInput);
+        return String.format(ADD_TASK_FORMAT_STRING, userInput);
     }
 
     /**
@@ -119,9 +134,9 @@ public class Marites {
         int taskIndex = parseInt(tokens[1]);
         Task taskToMark = tasks.get(taskIndex - 1);
         taskToMark.setDone(isDone);
-        String message = (isDone ? "Good job on getting this done!" :
-                "Okay, I've marked this as not yet done:");
-        return String.format(message + "%n    %s%n", taskToMark);
+        String message = (isDone ? MARK_DONE_MESSAGE :
+                MARK_UNDONE_MESSAGE);
+        return String.format(SET_TASK_STATUS_FORMAT_STRING, message, taskToMark);
     }
 
     /**
