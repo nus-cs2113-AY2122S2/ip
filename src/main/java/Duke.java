90daaad3e7c01c066368df3b2d60b56466793c19
Haofeng <walterhuang123@gmail.com>
2022-01-31 10:36:50 +0800

    Add list function to store userInput for Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,16 +9,25 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-
+        String[] userinput = new String[100];
+        int inputCount = 0;
         while(true) {
             String line;
             Scanner input = new Scanner(System.in);
             line = input.nextLine();
+            if (line.equals("list")){
+                for (int i = 0; i < inputCount; i++) {
+                    System.out.println((i+1) + ". " + userinput[i]);
+                }
+                continue;
+            }
+            userinput[inputCount] = line;
+            inputCount++;
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(line);
+            System.out.println("Add: " + line);
         }
     }
 }
