96346fd701ac993bdd81e41038ad23c6b1fed2e3
Haofeng <walterhuang123@gmail.com>
2022-01-31 14:04:17 +0800

    Add Mark as Done function for Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,48 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String[] userinput = new String[100];
+        //String[] userinput = new String[100];
+        Task[] userinput = new Task[100];
         int inputCount = 0;
         while(true) {
             String line;
             Scanner input = new Scanner(System.in);
             line = input.nextLine();
             if (line.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < inputCount; i++) {
-                    System.out.println((i+1) + ". " + userinput[i]);
+                    System.out.println((i+1) + ".[" +  userinput[i].getStatusIcon() +"] "+ userinput[i].description);
                 }
                 continue;
             }
-            userinput[inputCount] = line;
+
+            if (line.contains(" ")){
+                if ((line.substring(0, line.indexOf(" "))).equals("mark")) {
+                    if (Integer.parseInt(line.substring(line.indexOf(" ") + 1)) <= inputCount){
+                        userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].markAsDone();
+                        System.out.println("Nice! I've marked this task as done: ");
+                        System.out.println("[" + userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].getStatusIcon() + "] " + userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].description);
+                        continue;
+                    }
+                    else {
+                        System.out.println("Invalid Command!");
+                        continue;
+                    }
+                }
+                else if ((line.substring(0, line.indexOf(" "))).equals("unmark")) {
+                    if (Integer.parseInt(line.substring(line.indexOf(" ") + 1)) <= inputCount) {
+                        userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].markAsNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("[" + userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].getStatusIcon() + "] " + userinput[Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1].description);
+                        continue;
+                    }
+                    else {
+                        System.out.println("Invalid Command!");
+                        continue;
+                    }
+                }
+            }
+            userinput[inputCount] = new Task(line);
             inputCount++;
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
