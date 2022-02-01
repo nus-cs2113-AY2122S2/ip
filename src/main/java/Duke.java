a3cce825cc7aaa28ac737171668f75ca97ab4e67
Musfirahe0556596 <Musfirah@LAPTOP-C85KLE6R>
2022-02-01 20:46:23 +0800

    Add increment A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,111 +4,126 @@ import java.util.Scanner;
 public class Duke {
 
     public static final int MAX_TASKS = 100;
+    public static final String BORDER_DECORATION = "_____________________________________________________";
+    private static Task[] taskLists = new Task[MAX_TASKS];
 
     public static void displayGreeting() {
-        String border = "_____________________________________________________";
         String logo = " _______     __   __  \n"
                 + "|   _   |[x]| | / / [x]  \n"
                 + "|  | |  || ||  / /  | |\n"
                 + "|  | |  || ||  <    | |\n"
                 + "|__/ |__||_||_| |_| |_|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println(border);
-        System.out.println("Hello! I'm Niki");
-        System.out.println("What can I do for you?");
-        System.out.println(border);
+        System.out.println("Hello from\n" + logo +"\n" + BORDER_DECORATION);
+        System.out.println("Hello! I'm Niki\nWhat can I do for you?\n" + BORDER_DECORATION);
     }
 
-    public static void main(String[] args) {
-        displayGreeting();
-        Task[] taskLists = new Task[MAX_TASKS];
+    public static void printList() {
+        for (int i = 0; i < Task.getNumberOfTasks(); i++) {
+            System.out.println((i + 1) + "."+ taskLists[i].toString());
+        }
+    }
 
-        String border = "_____________________________________________________";
+    public static void addTask(String userInput) {
+        String[] arrayOfTaskStrings = userInput.split(" ");
+        String extractTaskDescription = "";
+        for (int i = 1; i < arrayOfTaskStrings.length; i++) {
+            extractTaskDescription += arrayOfTaskStrings[i] + " ";
+        }
+
+        Task newTask = new Todo(extractTaskDescription);
+        taskLists[Task.getNumberOfTasks() - 1] = newTask;
+        System.out.println("Task added:\n\t" + newTask.toString());
+        System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
+    }
+
+    public static void addTaskWithTime(String userInput, String stringSeparator) {
+        String[] arrayOfTaskStrings = userInput.split(" ");
+        String extractStringsWithoutCommand = "";
+        for (int i = 1; i < arrayOfTaskStrings.length; i++) {
+            extractStringsWithoutCommand += arrayOfTaskStrings[i] + " ";
+        }
+
+        String extractTaskDescription = extractStringsWithoutCommand.split(stringSeparator)[0];
+        String extractTaskDeadlineTime = extractStringsWithoutCommand.split(stringSeparator)[1];
+        Task newTask;
+        if (stringSeparator.equals("/by ")) {
+            newTask = new Deadline(extractTaskDescription, extractTaskDeadlineTime);
+        } else {
+            newTask = new Event(extractTaskDescription, extractTaskDeadlineTime);
+        }
+
+        taskLists[Task.getNumberOfTasks() - 1] = newTask;
+        System.out.println("Task added:\n\t" + newTask.toString());
+        System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
+    }
+
+    public static boolean isWithinTaskRange(int taskNumber) {
+        if (taskNumber > Task.getNumberOfTasks() || taskNumber <= 0) {
+            System.out.println("Task does not exist!");
+            return false;
+        }
+        return true;
+    }
+
+    public static void markTask(boolean isMarked, String userInput) {
+        int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+        if (isWithinTaskRange(taskNumber)) {
+            if (isMarked) {
+                taskLists[taskNumber - 1].markAsDone();
+                System.out.println("Fantastic! This task is done:");
+            } else {
+                taskLists[taskNumber - 1].markAsUndone();
+                System.out.println("Uh oh! This task is undone:");
+            }
+            System.out.println(taskLists[taskNumber - 1].toString());
+        }
+    }
+
+    private static void printExit() {
+        System.out.println(BORDER_DECORATION);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(BORDER_DECORATION);
+    }
+
+    public static void executeCommand() {
         String userInput;
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
 
         while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println(border);
+            System.out.println(BORDER_DECORATION);
+            String command = userInput.split(" ")[0];
 
-            if (userInput.equalsIgnoreCase("list")) {
-                for (int i = 0; i < Task.getNumberOfTasks(); i++) {
-                    System.out.println(i + 1 + "."+ taskLists[i].toString());
-                }
-            } else if (userInput.toLowerCase().startsWith("todo")) {
-                String[] arrayOfTaskDescriptions = userInput.split(" ");
-                String extractTaskDescription = "";
-                for (int i = 1; i < arrayOfTaskDescriptions.length; i++) {
-                    extractTaskDescription += arrayOfTaskDescriptions[i] + " ";
-                }
-
-                Task addNewTask = new Todo(extractTaskDescription);
-                taskLists[Task.getNumberOfTasks() - 1] = addNewTask;
-                System.out.println("Task added:\n\t" + addNewTask.toString());
-                System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
-
-            } else if (userInput.toLowerCase().startsWith("deadline")) {
-                String[] arrayOfTaskDescriptions = userInput.split(" ");
-                String extractStrings = "";
-                for (int i = 1; i < arrayOfTaskDescriptions.length; i++) {
-                    extractStrings += arrayOfTaskDescriptions[i] + " ";
-                }
-
-                String extractTaskDescription = extractStrings.split("/by ")[0];
-                String extractDeadlineTime = extractStrings.split("/by ")[1];
-
-                Task addNewTask = new Deadline(extractTaskDescription, extractDeadlineTime);
-                taskLists[Task.getNumberOfTasks() - 1] = addNewTask;
-                System.out.println("Task added:\n\t" + addNewTask.toString());
-                System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
-
-            } else if (userInput.toLowerCase().startsWith("event")) {
-                String[] arrayOfTaskDescriptions = userInput.split(" ");
-                String extractStrings = "";
-                for (int i = 1; i < arrayOfTaskDescriptions.length; i++) {
-                    extractStrings += arrayOfTaskDescriptions[i] + " ";
-                }
-
-                String extractTaskDescription = extractStrings.split("/at ")[0];
-                String extractDeadlineTime = extractStrings.split("/at ")[1];
-
-                Task addNewTask = new Event(extractTaskDescription, extractDeadlineTime);
-                taskLists[Task.getNumberOfTasks() - 1] = addNewTask;
-                System.out.println("Task added:\n\t" + addNewTask.toString());
-                System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
-
-            } else if (userInput.toLowerCase().startsWith("unmark")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-
-                if (taskNumber > Task.getNumberOfTasks() || taskNumber <= 0) {
-                    System.out.println("Task does not exist!");
-                } else {
-                    taskLists[taskNumber - 1].markAsUndone();
-                    System.out.println("Uh oh! This task is undone:");
-                    System.out.println(taskLists[taskNumber - 1].toString());
-                }
-            } else if (userInput.toLowerCase().startsWith("mark")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-
-                if (taskNumber > Task.getNumberOfTasks() || taskNumber <= 0) {
-                    System.out.println("Task does not exist!");
-                } else {
-                    taskLists[taskNumber - 1].markAsDone();
-                    System.out.println("Fantastic! This task is done:");
-                    System.out.println(taskLists[taskNumber - 1].toString());
-                }
-            } else {
-                Task addNewTask = new Todo(userInput);
-                taskLists[Task.getNumberOfTasks() - 1] = addNewTask;
-                System.out.println("Task added:\n\t" + addNewTask.toString());
-                System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in your list!");
+            switch (command) {
+            case "list":
+                printList();
+                break;
+            case "todo":
+                addTask(userInput);
+                break;
+            case "deadline":
+                addTaskWithTime(userInput, "/by ");
+                break;
+            case "event":
+                addTaskWithTime(userInput,"/at ");
+                break;
+            case "unmark":
+                markTask(false, userInput);
+                break;
+            case "mark":
+                markTask(true, userInput);
+                break;
+            default:
+                System.out.println("Invalid command.");
             }
-            System.out.println(border);
+            System.out.println(BORDER_DECORATION);
             userInput = in.nextLine();
         }
+    }
 
-        System.out.println(border);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(border);
+    public static void main(String[] args) {
+        displayGreeting();
+        executeCommand();
+        printExit();
     }
 }
