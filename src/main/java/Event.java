1ea6961d2193201c751a0536d0d561e9ee3c2d61
yuzhongng <yuzhongng@gmail.com>
2022-02-02 22:21:35 +0800

    Add event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,31 @@
+public class Event extends Task {
+    protected boolean isDone;
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+        this.isDone = false;
+    }
+
+    @Override
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String status = null;
+        if (isDone) {
+            status = "X";
+        } else {
+            status = " ";
+        }
+        return String.format("[E][%s] %s (at: %s)", status, super.toString().trim(), this.at.trim());
+    }
+}
