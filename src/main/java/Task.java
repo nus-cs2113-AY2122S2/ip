e547ad464fbb403c366149ced90bdd628c58b29f
WangYiying <>
2022-02-02 23:19:34 +0800

    Modify code according to coding standard

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,7 +2,7 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    private static Task[] taskList = new Task[100];
+    private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
 
     public Task(String description){
@@ -12,14 +12,14 @@ public class Task {
 
     public static void addTask(String taskText){
         Task newTask = new Task(taskText);
-        taskList[taskCount] = newTask;
+        tasks[taskCount] = newTask;
         taskCount++;
     }
 
     public static void listTasks(){
         for(int i = 0; i < taskCount; i++){
             System.out.print(i+1);
-            System.out.println("." + "[" + taskList[i].getStatusIcon() + "]" + taskList[i].description);
+            System.out.println("." + "[" + tasks[i].getStatusIcon() + "]" + tasks[i].description);
         }
     }
 
@@ -36,12 +36,12 @@ public class Task {
     }
 
     public static void mark(int index){
-        taskList[index-1].markAsDone();
-        System.out.println("[" + taskList[index-1].getStatusIcon() + "]" + taskList[index-1].description);
+        tasks[index-1].markAsDone();
+        System.out.println("[" + tasks[index-1].getStatusIcon() + "]" + tasks[index-1].description);
     }
 
     public static void unmark(int index){
-        taskList[index-1].markAsUndone();
-        System.out.println("[" + taskList[index-1].getStatusIcon() + "]" + taskList[index-1].description);
+        tasks[index-1].markAsUndone();
+        System.out.println("[" + tasks[index-1].getStatusIcon() + "]" + tasks[index-1].description);
     }
 }
