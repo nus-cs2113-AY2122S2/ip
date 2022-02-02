68ca3191a3546ea70e860b528d6be2a0c3d64fbe
Anvitha-r <anvithar1@gmail.com>
2022-02-02 17:12:43 +0800

    Level-1
    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         /*
@@ -8,13 +9,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         */
-        String indentation = "____________________________________________________________";
+        String indentation = "._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.._._.";
         System.out.println(indentation);
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Tom");
         System.out.println("What can I do for you?");
         System.out.println(indentation);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(indentation);
 
+        while(true) {
+            Scanner input1 = new Scanner(System.in);
+            String command = input1.nextLine();
+            System.out.println(indentation);
+            //System.out.println(command);
+            if (command.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+            break;
+        }else{
+                    System.out.println(command);
+                    System.out.println(indentation);
+            }
+        }
     }
+        //System.out.println(indentation);
 }
