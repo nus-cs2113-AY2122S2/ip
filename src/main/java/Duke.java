c1a7a75fbac1050c30fa9b1e327088f6a944b655
emilysim00 <emilysim00@gmail.com>
2022-02-02 16:22:10 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,78 +1,126 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
+    public static final String LINESEPARATOR = "____________________________________________________________\n";
+    public static final int MAX_TASKS = 100;
+    public static final Task[] taskList = new Task[MAX_TASKS];
+
     public static void displayLogo() {
         String logo = "                       ___\n"
-                +"                      / ()\\\n"
-                +"                    _|_____|_\n"
-                +"                   | | === | |\n"
-                +" _____  _      _   |_|  0  |_|\n"
-                +"| ___| | \\    / |   ||  0  ||\n"
-                +"| |__  |  \\  /  |   ||__*__||\n"
-                +"|  __| |   \\/   |  |~ \\___/ ~| \n"
-                +"| |___ | |\\__/| |  /=\\ /=\\ /=\\ \n"
-                +"|_____||_|    |_|__[_]_[_]_[_]______________________________\n";
-        String greetings = "____________________________________________________________\n"
+                + "                      / ()\\\n"
+                + "                    _|_____|_\n"
+                + "                   | | === | |\n"
+                + " _____  _      _   |_|  0  |_|\n"
+                + "| ___| | \\    / |   ||  0  ||\n"
+                + "| |__  |  \\  /  |   ||__*__||\n"
+                + "|  __| |   \\/   |  |~ \\___/ ~| \n"
+                + "| |___ | |\\__/| |  /=\\ /=\\ /=\\ \n"
+                + "|_____||_|    |_|__[_]_[_]_[_]______________________________\n";
+        String greetings = LINESEPARATOR
                 + "Hello! I'm EM\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n";
+                + LINESEPARATOR;
         System.out.println(logo + greetings);
     }
 
+    /*Display goodbye message*/
     public static void displayFarewell() {
-        String farewell = "____________________________________________________________\n"
+        String farewell = LINESEPARATOR
                 + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+                + LINESEPARATOR;
         System.out.println(farewell);
     }
 
-    public static void displayTaskList(int listCount, Task[] taskList) {
-        String lineSeparator = "____________________________________________________________\n";
-        System.out.println(lineSeparator);
+    public static void displayTaskList(Task[] taskList) {
+        System.out.println(LINESEPARATOR);
         System.out.println("Here are the tasks in your list:");
-        if (listCount == 0) {
+        if (Task.getTaskCount() == 0) {
             System.out.println("List is Empty!");
         }
 
-        for(int i = 0; i < listCount; i++) {
-            System.out.println((i+1) + ". " + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+        for (int i = 0; i < Task.getTaskCount(); i++) {
+            System.out.println((i + 1) + ". " + taskList[i].toString());
         }
-        System.out.println(lineSeparator);
+        System.out.println(LINESEPARATOR);
     }
 
-    public static void main(String[] args) {
-        String lineSeparator = "____________________________________________________________\n";
-        displayLogo();
+    public static void addTask(String userInput, Task[] taskList) {
+        String[] taskDesc = userInput.split(" ", 2);
+        Task newTask = new ToDo(taskDesc[1]);
+        taskList[Task.getTaskCount() - 1] = newTask;
+        System.out.println(LINESEPARATOR + "Got it. I've added this task:");
+        System.out.println(taskList[Task.getTaskCount()-1].toString());
+        System.out.println("Now you have " + Task.getTaskCount() + " tasks in the list.\n" + LINESEPARATOR);
+    }
+
+    public static void addTaskAndTime(String userInput, Task[] taskList) {
+        String[] arrayOfUserInput = userInput.split(" ", 2);
+        arrayOfUserInput = userInput.split("/", 2);
+        String taskLongDesc = arrayOfUserInput[0];
+        String[] taskDesc = taskLongDesc.split(" ",2);
+        String timeWithTaskType = arrayOfUserInput[1];
+        String[] timing = timeWithTaskType.split(" ", 2);
+
+        Task newTask;
+        if (timing[0].equals("by")) {
+            newTask = new Deadline(taskDesc[1], timing[1]);
+        } else{
+            newTask = new Event(taskDesc[1], timing[1]);
+        }
+        taskList[Task.getTaskCount() - 1] = newTask;
+        System.out.println(LINESEPARATOR + "Got it. I've added this task:");
+        System.out.println(taskList[Task.getTaskCount()-1].toString());
+        System.out.println("Now you have " + Task.getTaskCount() + " tasks in the list.\n" + LINESEPARATOR);
+    }
+
+    public static boolean isTaskValid(int taskNumber) {
+        if (taskNumber > Task.getTaskCount() || taskNumber <= 0) {
+            System.out.println(LINESEPARATOR + "Task does not exist!\n" + LINESEPARATOR);
+            return false;
+        }
+        return true;
+    }
+
+    public static void processAction() {
         String userInput;
-        Task[] taskList = new Task[100];
-        int listCount = 0;
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
-        while(!userInput.equalsIgnoreCase("Bye")) {
-            if (userInput.equalsIgnoreCase("List")) {
-                displayTaskList(listCount, taskList);
-            } else if (userInput.toLowerCase().startsWith("mark")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                if (taskNumber > listCount || taskNumber <= 0) {
-                    System.out.println(lineSeparator + "Task does not exist!\n" +lineSeparator);
-                }else {
-                    taskList[taskNumber-1].markAsDone(taskNumber, taskList);
+        while (!userInput.equalsIgnoreCase("Bye")) {
+            String action = userInput.split(" ")[0];
+            switch (action.toLowerCase()) {
+            case "list":
+                displayTaskList(taskList);
+                break;
+            case "mark":
+                int markTaskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                if (isTaskValid(markTaskNumber)) {
+                    taskList[markTaskNumber - 1].markAsDone(markTaskNumber, taskList);
                 }
-            } else if (userInput.toLowerCase().startsWith("unmark")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                if (taskNumber > listCount || taskNumber <= 0) {
-                    System.out.println(lineSeparator + "Task does not exist!\n" + lineSeparator);
-                }else {
-                    taskList[taskNumber-1].markAsUndone(taskNumber, taskList);
+                break;
+            case "unmark":
+                int unmarkTaskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                if (isTaskValid(unmarkTaskNumber)) {
+                    taskList[unmarkTaskNumber - 1].markAsUndone(unmarkTaskNumber, taskList);
                 }
-            } else {
-                Task newTask = new Task(userInput.toLowerCase());
-                taskList[listCount] = newTask;
-                listCount ++;
-                System.out.println(lineSeparator + "added: " + userInput.toLowerCase() + "\n" + lineSeparator);
+                break;
+            case "todo":
+                addTask(userInput, taskList);
+                break;
+            case "event":
+            case "deadline":
+                addTaskAndTime(userInput, taskList);
+                break;
+            default:
+                System.out.println("Invalid");
             }
             userInput = in.nextLine();
         }
+    }
+
+    public static void main(String[] args) {
+        displayLogo();
+        processAction();
         displayFarewell();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,13 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-    public String lineSeparator = "____________________________________________________________\n";
+    public static final String LINESEPARATOR = "____________________________________________________________\n";
+    private static int taskCount = 0;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        taskCount++;
     }
 
     public String getDescription() {
@@ -20,19 +22,31 @@ public class Task {
         return (isDone ? "X" : " ");
     }
 
+    public static int getTaskCount() {
+        return taskCount;
+    }
+
     public void markAsDone(int taskNumber, Task[] taskList) {
-            isDone = true;
-            System.out.println(lineSeparator);
-            System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("[" + taskList[taskNumber-1].getStatusIcon() + "] " + taskList[taskNumber-1].getDescription());
-            System.out.println(lineSeparator);
+        isDone = true;
+        System.out.print(LINESEPARATOR);
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("[" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].getDescription());
+        System.out.print(LINESEPARATOR);
     }
 
     public void markAsUndone(int taskNumber, Task[] taskList) {
         isDone = false;
-        System.out.println(lineSeparator);
+        System.out.print(LINESEPARATOR);
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("[" + taskList[taskNumber-1].getStatusIcon() + "] " + taskList[taskNumber-1].getDescription());
-        System.out.println(lineSeparator);
+        System.out.println("[" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].getDescription());
+        System.out.print(LINESEPARATOR);
+    }
+
+    public String toString() {
+        if (isDone) {
+            return "[X] " + description;
+        } else {
+            return "[ ] " + description;
+        }
     }
 }
