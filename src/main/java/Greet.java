cd624bece56007ca36e49fda4e3e5fa12bedee7c
Xunyi <73570047+icthenic@users.noreply.github.com>
2022-02-02 10:56:07 +0800

    Add, List

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -6,12 +6,26 @@ public class Greet {
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
 
-        String input;
-        input = sc.nextLine();
-        while(!input.equals("bye")){
-            System.out.println("   "+ input);
-            input = sc.nextLine();
+        String[] input = new String[100];
+        int idx = 0;
+        input[0] = sc.nextLine();
+        while(!input[idx].equals("bye")){
+            if(input[idx].equals("list")){
+                idx--;
+                for(int i =0; i<=idx ;i++){
+                    System.out.println("  " + (i+1) + ". " + input[i]);
+                }
+            }
+            else {
+                if (idx >= 100) {
+                    break;
+                }
+                System.out.println("   added: " + input[idx]);
+            }
+            idx++;
+            input[idx] = sc.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again soon！");
     }
 }
