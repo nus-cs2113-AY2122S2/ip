b38e395edb172a153ca6d6300d209f5062e4b6b9
Anvitha-r <anvithar1@gmail.com>
2022-02-02 18:07:28 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,6 +15,28 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(indentation);
 
+        //public static void listItems(String [100] listName) {
+            String list[]=new String[100];
+            for(int i = 0; i < 100; i++){
+                Scanner input1 = new Scanner(System.in);
+                String command = input1.nextLine();
+                list[i] = command;
+                if (command.equals("list")){
+                    for(int j = 0; j < i; j++ ) {
+                        System.out.println((j + 1) + "." + list[j]);
+                    }
+                    }else if(command.equals("bye")){
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break;
+                    }else{
+                        //continue;
+                        System.out.println("added:" + command);
+                    }
+
+                }
+                }
+            }
+        /*
         while(true) {
             Scanner input1 = new Scanner(System.in);
             String command = input1.nextLine();
@@ -30,3 +52,5 @@ public class Duke {
     }
         //System.out.println(indentation);
 }
+
+         */
