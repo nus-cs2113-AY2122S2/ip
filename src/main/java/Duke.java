b0542f65bfd717dcaed1e04dcbc98fad57c2f305
chintaiann <chintaiann23@gmail.com>
2022-02-03 01:29:37 +0800

    Level 1-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
          System.out.println("---------------------");
         System.out.println(("Hello! I'm Duke"));
         System.out.println(("What can i do for you?"));
         System.out.println("---------------------");
-        System.out.println(("Bye. Hope to see you again soon!"));
-        System.out.println("---------------------");
+        Boolean run = true;
+        while (run) {
+            Scanner sc = new Scanner(System.in);
+            String anyString = sc.nextLine();
+            if (anyString.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                run = false;
+            }
+
+
+            else {
+                System.out.println(anyString);
+            }
+
+        }        
     }
 }
