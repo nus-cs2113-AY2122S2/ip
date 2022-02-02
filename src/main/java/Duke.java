1c764079d9db2486ff88d81dd6fd6103c05b2561
chintaiann <chintaiann23@gmail.com>
2022-02-03 01:42:39 +0800

    Level 2-9

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,20 +7,32 @@ public class Duke {
         System.out.println(("Hello! I'm Duke"));
         System.out.println(("What can i do for you?"));
         System.out.println("---------------------");
-        Boolean run = true;
-        while (run) {
+        String[] strings = new String[100];
+
+        Boolean run = true; 
+        int arrayIndex = 0; 
+        while (run) { 
             Scanner sc = new Scanner(System.in);
             String anyString = sc.nextLine();
-            if (anyString.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                run = false;
+
+            if (anyString.equals("bye")) { 
+                System.out.println("Bye! Hope to see you again.");
+                run = false; 
             }
 
+            else if (anyString.equals("list")){
+                for (int i = 0; i<arrayIndex; i++) {
+                    System.out.println(strings[i]);
+                }
+            }
 
             else {
-                System.out.println(anyString);
+                strings[arrayIndex] = anyString; 
+                arrayIndex++; 
             }
-
-        }        
+        }
+        
+           
+      
     }
 }
