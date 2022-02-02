bcfbc0de009467037e0f12775b763ccf3524e7b0
Xunyi <73570047+icthenic@users.noreply.github.com>
2022-02-02 10:10:43 +0800

    Greet, echo, exit

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Greet {
     public static void main(String[] args){
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+        input = sc.nextLine();
+        while(!input.equals("bye")){
+            System.out.println("   "+ input);
+            input = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon！");
     }
 }
