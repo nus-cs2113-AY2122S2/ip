5e12eadcd03ee61fe27c5a943a3793a540f97819
LAPTOP-ABRSSH4C\clare <clarencechuayinghow@hotmail.com>
2022-02-02 20:14:08 +0800

    Added Level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+
+    private static String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n"
+        + HORIZONTAL_LINE + "\n"
+        + "Hello! I'm Duke\n"
+        + "What can I do for you?\n"
+        + HORIZONTAL_LINE + "\n"
+        + "Bye. Hope to see you again soon!\n"
+        + HORIZONTAL_LINE);
     }
 }
