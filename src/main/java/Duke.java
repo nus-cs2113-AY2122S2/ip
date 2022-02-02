3d1bb0f84b7a5e7a1631863484091c36910fb37e
Franke Tang <franketang@gmail.com>
2022-02-02 13:50:05 +0800

    Add new classes: Todo, Deadline, and Event
    New classes are subclasses of Task
    The implementation of inheritance allows for further subsection of what user may want.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,26 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
+    private static Task[] taskList = new Task[100];
+    private static int taskCount = 0;
+
     private static final int MARK_NUMBER_INDEX = 5;
     private static final int NEWLINE_STRIP = 2;
-    private static final int TASK_CAPACITY = 100;
     private static final int UNMARK_NUMBER_INDEX = 7;
 
     public static void main(String[] args) {
         botResponse("Hello! I'm Boba\n\tWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        ArrayList<Task> taskList = new ArrayList<>();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                int index = 1;
                 // StringBuilder to reduce constructing a new string due to concatenation
                 StringBuilder sb = new StringBuilder();
-                for (Task item : taskList) {
-                    sb.append(index++); // Post-increment to keep index up to date
+                for (int i = 0; i < taskCount; i++) {
+                    sb.append(i + 1); // Post-increment to keep index up to date
                     sb.append(". ");
-                    sb.append(item.getStatusIcon());
-                    sb.append(item.description);
+                    sb.append(taskList[i].toString());
                     sb.append("\n\t");
                 }
                 // substring used to ignore the last newline character
@@ -31,28 +29,48 @@ public class Duke {
                 // based on our current implementation unmark must appear before mark
                 // similar reasoning to "mark <number>"
                 int index = Integer.parseInt(input.substring(UNMARK_NUMBER_INDEX)) - 1;
-                Task unmarkedTask = taskList.get(index);
+                Task unmarkedTask = taskList[index];
                 unmarkedTask.markAsNotDone();
                 String unmarkedResponse = "Boop beep! I've marked this task as not done:\n\t\t";
                 botResponse(unmarkedResponse + unmarkedTask.getStatusIcon() + unmarkedTask.description);
             } else if (input.contains("mark")) {
                 // The number starts at index 5 assuming the command is "mark <number>"
                 int index = Integer.parseInt(input.substring(MARK_NUMBER_INDEX)) - 1;
-                Task markedTask = taskList.get(index);
+                Task markedTask = taskList[index];
                 markedTask.markAsDone();
                 String markedResponse = "Beep boop! I've marked this task as done:\n\t\t";
                 botResponse(markedResponse + markedTask.getStatusIcon() + markedTask.description);
-            } else if (taskList.size() == TASK_CAPACITY) {
+            } else if (taskCount == taskList.length) {
                 // no more than 100 tasks
                 botResponse("Sorry. No more tasks can be added.");
-            } else {
-                botResponse("added: " + input);
-                Task newTask = new Task(input);
-                taskList.add(newTask);
+            } else if (input.contains("todo")) {
+                String description = input.substring(input.indexOf(" ") + 1);
+                Task newTask = new Todo(description);
+                addTask(newTask);
+            } else if (input.contains("deadline")) {
+                String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/by") - 1);
+                String deadline = input.substring(input.indexOf("/") + 4);
+                Task newTask = new Deadline(description, deadline);
+                addTask(newTask);
+            } else if (input.contains("event")) {
+                String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/at") - 1);
+                String event = input.substring(input.indexOf("/") + 4);
+                Task newTask = new Event(description, event);
+                addTask(newTask);
             }
             input = scan.nextLine();
         }
         botResponse("Bye. Hope to see you again soon!");
+    }
+
+    private static void addTask(Task newTask) {
+        taskList[taskCount] = newTask;
+        taskCount++;
+        System.out.println("............................................................");
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t" + newTask.toString());
+        System.out.printf("\tNow you have %d tasks in your list.%n", taskCount);
+        System.out.println("............................................................");
     }
 
     private static void botResponse(String response) {
