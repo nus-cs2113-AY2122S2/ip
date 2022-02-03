e835c55778d2c9295a46da3710cf408a9d7ecc4e
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-02-04 02:31:49 +0800

    Add TaskManager Class

--- src/main/java/Brave.java
+++ src/main/java/Brave.java
@@ -3,55 +3,43 @@ import java.lang.String;
 
 public class Brave {
     public static void main(String[] args) {
+        String input;
+        String[] inputSplitted;
         String command;
         Scanner in = new Scanner(System.in);
-        Task[] task = new Task[100];
-        int numberOfTask = 0;
+        TaskManager tasks = new TaskManager();
 
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello I'm Brave");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
+        tasks.showWelcomeMessage();
 
-        command = in.nextLine();
 
-        while (!command.equals("bye")) {
-            if (command.split(" ")[0].equals("mark")) {
-                String[] words = command.split(" ");
-                int taskIndex = Integer.parseInt(words[1]) - 1;
-                task[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task[taskIndex].getStatusIcon() + " " + task[taskIndex].getDescription());
+        while (true) {
+            input = in.nextLine();
+            inputSplitted = input.split(" ", 2);
+            command = inputSplitted[0];
+
+            if (command.startsWith("bye")) {
+                break;
             }
 
-            else if (command.split(" ")[0].equals("unmark")) {
-                String[] words = command.split(" ");
-                int taskIndex = Integer.parseInt(words[1]) - 1;
-                task[taskIndex].unmarkAsDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task[taskIndex].getStatusIcon() + " " + task[taskIndex].getDescription());
+            switch (command) {
+            case "list":
+                tasks.printTaskList();
+                break;
+            case "mark":
+                tasks.markTask(Integer.parseInt(inputSplitted[1]) - 1);
+                break;
+            case "unmark":
+                tasks.unmarkTask(Integer.parseInt(inputSplitted[1]) - 1);
+                break;
+            default:
+                tasks.addTask(input);
             }
-
-            else if (command.equals("list")) {
-                System.out.println("\t____________________________________________________________");
-                for (int i=0; i<numberOfTask; i++) {
-                    System.out.println("\t" + (i+1) + "." + task[i].getStatusIcon() + " " + task[i].getDescription());
-                }
-                System.out.println("\t____________________________________________________________");
-            }
-            else {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tadded: " + command);
-                System.out.println("\t____________________________________________________________");
-                task[numberOfTask] = new Task(command);
-                numberOfTask++;
-            }
-
-            command = in.nextLine();
         }
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye, Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
     }
+
+
 }
