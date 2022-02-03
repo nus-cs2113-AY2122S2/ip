6b84407bbadfb155216c8c08f921d98524f5f584
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-02-04 03:37:12 +0800

    Improve code quality, add constant

--- src/main/java/Brave.java
+++ src/main/java/Brave.java
@@ -17,9 +17,12 @@ public class Brave {
         while (true) {
             input = in.nextLine();
             splitInput = input.split(" ", 2);
-            command = splitInput[0];
+            command = splitInput[0]; //e.g. mark 2 -> take the first word as the command -> "mark"
 
-            if (command.startsWith("bye")) {
+            if (command.equals("bye")) {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tBye, Hope to see you again soon!");
+                System.out.println("\t____________________________________________________________");
                 break;
             }
 
@@ -28,35 +31,33 @@ public class Brave {
                 tasks.printTaskList();
                 break;
             case "mark":
-                tasks.markTask(Integer.parseInt(splitInput[1]) - 1);
+                tasks.markTask(Integer.parseInt(splitInput[1]) - 1); // 0 indexing
                 break;
             case "unmark":
-                tasks.unmarkTask(Integer.parseInt(splitInput[1]) - 1);
+                tasks.unmarkTask(Integer.parseInt(splitInput[1]) - 1); // 0 indexing
                 break;
             case "todo":
                 tasks.addTask(new Todo(splitInput[1]));
                 break;
             case "deadline":
+                // To-do validate arguments~
                 arguments = splitInput[1].split(" /by ", 2);
                 description = arguments[0];
                 String by = arguments[1];
                 tasks.addTask(new Deadline(description, by));
                 break;
             case "event":
+                // To-do validate arguments~
                 arguments = splitInput[1].split(" /at ", 2);
                 description = arguments[0];
                 String eventTime = arguments[1];
                 tasks.addTask(new Event(description, eventTime));
                 break;
             default:
-                System.out.println("Wrong input");
+                System.out.println("Wrong input, available command are -> list/mark/unmark/todo/deadline/event");
                 break;
             }
         }
-
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye, Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
     }
 
 
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,5 +1,6 @@
 public class TaskManager {
-    private final Task[] tasks = new Task[100];
+    public static final int MAX_TASK = 100;
+    private final Task[] tasks = new Task[MAX_TASK];
     private int tasksCount = 0;
 
     public TaskManager() {
@@ -8,26 +9,26 @@ public class TaskManager {
     public void addTask(Task description) {
         this.tasks[this.tasksCount] = description;
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tYepp, I have added this task\n\t" + this.tasks[this.tasksCount].description +"\n\tYou currently have " + (this.tasksCount+1) + " task in the list");
+        System.out.println("\tYepp, I have added this task\n\t" + this.tasks[this.tasksCount].description + "\n\tYou currently have " + (this.tasksCount + 1) + " task in the list");
         System.out.println("\t____________________________________________________________");
-        ++this.tasksCount;
+        this.tasksCount++;
     }
 
     public void printTaskList() {
         System.out.println("\t____________________________________________________________");
-        for (int i=0; i<tasksCount; i++) {
-            System.out.println(String.format("\t%d.%s %s", i+1,this.tasks[i].getStatusIcon(), this.tasks[i].getDescription() ));
+        for (int i = 0; i < tasksCount; i++) {
+            System.out.println(String.format("\t%d.%s %s", i + 1, this.tasks[i].getStatusIcon(), this.tasks[i].getDescription()));
         }
         System.out.println("\t____________________________________________________________");
     }
 
-    public void markTask (int taskIndex) {
+    public void markTask(int taskIndex) {
         this.tasks[taskIndex].markAsDone();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(this.tasks[taskIndex].getStatusIcon() + " " + this.tasks[taskIndex].getDescription());
     }
 
-    public void unmarkTask (int taskIndex) {
+    public void unmarkTask(int taskIndex) {
         this.tasks[taskIndex].unmarkAsDone();
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(this.tasks[taskIndex].getStatusIcon() + " " + this.tasks[taskIndex].getDescription());
