d6d167bb914a601691bdcea2414ff40560ad5ff1
quitejasper <jasper.chia9@gmail.com>
2022-02-03 13:49:43 +0800

    updated code format using IntelliJ's Ctrl + Alt + L

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,37 +17,35 @@ public class Duke {
         exit();
     }
 
-    public static void printLine(){
+    public static void printLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void greet(){
+    public static void greet() {
         printLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         printLine();
     }
 
-    public static void exit(){
+    public static void exit() {
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
 
 
-    public static void converse(Task[] list, int taskCount){
+    public static void converse(Task[] list, int taskCount) {
         Scanner sc = new Scanner(System.in);
         String response = sc.nextLine();
-        while (!(response.equals("bye"))){
+        while (!(response.equals("bye"))) {
             if (response.equals("list"))
                 listTasks(list, taskCount);
-            else if (response.startsWith("mark")){
-                    markTask(list, taskCount, response);
-            }
-            else if (response.startsWith("unmark")){
+            else if (response.startsWith("mark")) {
+                markTask(list, taskCount, response);
+            } else if (response.startsWith("unmark")) {
                 unmarkTask(list, taskCount, response);
-            }
-            else {
+            } else {
                 addToList(list, taskCount, response);
                 taskCount++;
             }
@@ -56,59 +54,55 @@ public class Duke {
 
     }
 
-    public static void listTasks(Task[] list, int taskCount){
+    public static void listTasks(Task[] list, int taskCount) {
         printLine();
-        for (int i = 0; i < taskCount; i++){
-            System.out.println((i+1) + ". [" + list[i].getStatusIcon() + "] " + list[i].description);
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". [" + list[i].getStatusIcon() + "] " + list[i].description);
         }
         printLine();
     }
 
-    public static void addToList(Task[] list, int taskCount, String response){
+    public static void addToList(Task[] list, int taskCount, String response) {
         printLine();
         list[taskCount] = new Task(response);
         System.out.println("added: " + response);
         printLine();
     }
 
-    public static void markTask(Task[] list, int taskCount, String response){
+    public static void markTask(Task[] list, int taskCount, String response) {
         printLine();
         String[] words = response.split(" ");
         int taskIndex = Integer.parseInt(words[1]);
-        if (taskIndex > taskCount || taskIndex == 0){
+        if (taskIndex > taskCount || taskIndex == 0) {
             System.out.println("Sorry, seems like there's no such task with that index.");
             printLine();
             return;
-        }
-        else {
-            if (!(list[taskIndex-1].isDone)){
-                list[taskIndex-1].markAsDone();
+        } else {
+            if (!(list[taskIndex - 1].isDone)) {
+                list[taskIndex - 1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t[" + list[taskIndex-1].getStatusIcon() + "]" + list[taskIndex-1].description);
-            }
-            else {
+                System.out.println("\t[" + list[taskIndex - 1].getStatusIcon() + "]" + list[taskIndex - 1].description);
+            } else {
                 System.out.println("Hmm, you've already marked this task.");
             }
         }
         printLine();
     }
 
-    public static void unmarkTask(Task[] list, int taskCount, String response){
+    public static void unmarkTask(Task[] list, int taskCount, String response) {
         printLine();
         String[] words = response.split(" ");
         int taskIndex = Integer.parseInt(words[1]);
-        if (taskIndex > taskCount || taskIndex == 0){
+        if (taskIndex > taskCount || taskIndex == 0) {
             System.out.println("Sorry, seems like there's no such task with that index.");
             printLine();
             return;
-        }
-        else {
-            if (list[taskIndex-1].isDone){
-                list[taskIndex-1].markAsNotDone();
+        } else {
+            if (list[taskIndex - 1].isDone) {
+                list[taskIndex - 1].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("\t[" + list[taskIndex-1].getStatusIcon() + "]" + list[taskIndex-1].description);
-            }
-            else {
+                System.out.println("\t[" + list[taskIndex - 1].getStatusIcon() + "]" + list[taskIndex - 1].description);
+            } else {
                 System.out.println("Hmm, this task is already unmarked.");
             }
         }
