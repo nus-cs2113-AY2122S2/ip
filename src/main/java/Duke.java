e2ce3795703080669ef0e36f29be50d1a2b70040
RenTianle <lele.rer@outlook.com>
2022-02-03 18:49:39 +0800

    complete level 1, the chatbot is now able to echo whatever customers say, and exit when "bye" is entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "______________________________________________________________\n"
+        String greetings =
+              "______________________________________________________________\n"
             + "Hello! I'm Duke\n"
             + "What can I do for you?\n"
-            + "______________________________________________________________\n"
-            + "Bye, Hope to see you again soon!\n"
-            + "______________________________________________________________\n";
-        System.out.println(logo);
+            + "______________________________________________________________";
+        System.out.println(greetings);
+        String echo = greetings;
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            echo = sc.next();
+            System.out.println("______________________________________________________________");
+            if(echo.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("______________________________________________________________\n");
+                break;
+            } else {
+                System.out.println(echo);
+            }
+            System.out.println("______________________________________________________________\n");
+        }
+
+
+
+
         
     }
 }
