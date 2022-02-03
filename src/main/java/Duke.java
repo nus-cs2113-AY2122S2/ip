c49584ad77740a75d3eaa88be214495d0f1c4abb
Bryan-BC <bryanchristopherbc88@gmail.com>
2022-02-03 22:31:15 +0800

    Add A-CodingStandard increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int index = 0;
+    private static String separator = "-------------------------------------------";
+    private static Task[] tasks = new Task[100];
     public static void greet() {
         String logo = "   ________                            \n"
                 + "  / ____/ /_  ___  ___  ____ ___  _____\n"
@@ -19,39 +22,53 @@ public class Duke {
         System.out.println(farewell);
     }
 
-    public static void addList() {
+    public static void printList() {
+        System.out.println("Hemre are the tasks im your list:");
+        for (int i = 0; i < index; i++) {
+            String number = String.valueOf(i + 1) + ". ";
+            System.out.print(number);
+            tasks[i].printTask();
+        }
+        System.out.println(separator);
+    }
+
+    public static void printMark(int ind) {
+        tasks[ind].setDone(true);
+        System.out.println("Ok! I hamve marked the task:");
+        tasks[ind].printTask();
+        System.out.println(separator);
+    }
+
+    public static void printUnmark(int ind) {
+        tasks[ind].setDone(false);
+        System.out.println("Ok! I hamve unmarked the task:");
+        tasks[ind].printTask();
+    }
+
+    public static void printAddTask(String command) {
+        Task task = new Task(command);
+        tasks[index++] = task;
+        System.out.println("I hamve added: " + command);
+        System.out.println(separator);
+    }
+
+    public static void askInput() {
         int ind;
-        String command;
-        Task[] tasks = new Task[100];
-        int index = 0;
-        String separator = "-------------------------------------------";
         Scanner in = new Scanner(System.in);
-        command = in.nextLine();
+        String command = in.nextLine();
         String[] commands = command.split(" ");
         System.out.println(separator);
         do {
             if (command.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    String number = String.valueOf(i + 1) + ". ";
-                    System.out.print(number);
-                    tasks[i].printTask();
-                }
-                System.out.println(separator);
+                printList();
             } else if (commands[0].equals("mark")) {
                 ind = Integer.parseInt(commands[1]) - 1;
-                tasks[ind].setDone(true);
-                System.out.println("Ok! I hamve marked the task:");
-                tasks[ind].printTask();
+                printMark(ind);
             } else if (commands[0].equals("unmark")) {
                 ind = Integer.parseInt(commands[1]) - 1;
-                tasks[ind].setDone(false);
-                System.out.println("Ok! I hamve unmarked the task:");
-                tasks[ind].printTask();
+                printUnmark(ind);
             } else {
-                Task task = new Task(command);
-                tasks[index++] = task;
-                System.out.println("I hamve added: " + command);
-                System.out.println(separator);
+                printAddTask(command);
             }
             in = new Scanner(System.in);
             command = in.nextLine();
@@ -62,8 +79,7 @@ public class Duke {
 
     public static void main(String[] args) {
         greet();
-        addList();
+        askInput();
         farewell();
     }
-
 }
