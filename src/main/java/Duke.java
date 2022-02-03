b80b8281cea23297df587eadaee1d7444617f22b
LAPTOP-ABRSSH4C\clare <clarencechuayinghow@hotmail.com>
2022-02-03 21:26:58 +0800

    Added Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
 
+    public static Scanner sc = new Scanner(System.in);
     private static String HORIZONTAL_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
@@ -9,11 +12,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + "\n"
-        + HORIZONTAL_LINE + "\n"
-        + "Hello! I'm Duke\n"
-        + "What can I do for you?\n"
-        + HORIZONTAL_LINE + "\n"
-        + "Bye. Hope to see you again soon!\n"
-        + HORIZONTAL_LINE);
+                + HORIZONTAL_LINE + "\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + HORIZONTAL_LINE);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(HORIZONTAL_LINE + "\n"
+                        + input + "\n"
+                        + HORIZONTAL_LINE);
+            }
+        }
+
+        System.out.println(HORIZONTAL_LINE + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + HORIZONTAL_LINE);
     }
 }
