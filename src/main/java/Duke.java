c456ebcaaa8c88e8e75728b5b163ba2e6ac907a5
LI-XINYI1 <lilxysa0723p@gmail.com>
2022-02-04 04:21:17 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.lang.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "  Hello! I'm Duke\n"
+                       +  "  What can I do for you? \n";
+        String ending = "Bye. Hope to see you again soon! 、\n";
+        String horiLine = "____________________________________________________________\n";
+
+        System.out.println(horiLine + greeting + horiLine);
+        System.out.println(horiLine + ending + horiLine);
+
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
