67d21462734d43da448976ba346d002600f4fb17
GlendonNotGlen <glendonng38@gmail.com>
2022-02-04 10:24:40 +0800

    Change name of program from Duke to Jarvis, added automated testing

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-
 public class Duke {
     public static void main(String[] args) {
         DisplayMessages.startingMessage();
+        Scanner in = new Scanner(System.in);
         while (true) {
-            Formatter.inputHandler();
+            Formatter.inputHandler(in);
         }
     }
 }
