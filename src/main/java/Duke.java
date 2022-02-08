48442f08115b157de7000870aeb56c8e2aefff03
yuhaochua <yuhaochua99@gmail.com>
2022-02-08 21:06:20 +0800

    Add basic exception handling for wrong input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,11 +39,15 @@ public class Duke {
                     break;
                 default:
                     System.out.println("================================================");
-                    tasks[i] = addTask(userInput);
-                    System.out.println(tasks[i].printTask());
-                    System.out.println("Now you have " + (i+1) + " tasks in the list.");
+                    try {
+                        tasks[i] = addTask(userInput);
+                        System.out.println(tasks[i].printTask());
+                        System.out.println("Now you have " + (i+1) + " tasks in the list.");
+                        i++;
+                    } catch (NullPointerException e) {
+                        System.out.println("Please try again!");
+                    }
                     System.out.println("================================================");
-                    i++;
                     break;
                 }
             }
@@ -54,11 +58,36 @@ public class Duke {
 
     public static Task addTask(String userInput){
         if (userInput.startsWith("todo")) {
-            return new Todo(userInput.substring(5));
+            try{
+                checkEmptyDescription(userInput.substring(5));
+                return new Todo(userInput.substring(5));
+            }catch(EmptyDescriptionException | StringIndexOutOfBoundsException e){
+                System.out.println("OOPS!!! The description of a todo cannot be empty.");
+            }
         } else if (userInput.startsWith("deadline")) {
-            return new Deadline(userInput.substring(9));
+            try{
+                checkEmptyDescription(userInput.substring(5));
+                return new Deadline(userInput.substring(9));
+            }catch(EmptyDescriptionException | StringIndexOutOfBoundsException e){
+                System.out.println("OOPS!!! The description of a todo cannot be empty.");
+            }
+        }else if (userInput.startsWith("event")){
+            try{
+                checkEmptyDescription(userInput.substring(5));
+                return new Event(userInput.substring(6));
+            }catch(EmptyDescriptionException | StringIndexOutOfBoundsException e){
+                System.out.println("OOPS!!! The description of a todo cannot be empty.");
+            }
         }else{
-            return new Event(userInput.substring(6));
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+
+        return null;
+    }
+
+    public static void checkEmptyDescription(String description) throws EmptyDescriptionException{
+        if(description.isBlank()){
+            throw new EmptyDescriptionException();
         }
     }
 
