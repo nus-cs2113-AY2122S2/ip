d56fc52977c72f2a6b1163e75a872c5159f3550d
khseah <seahkithan55@gmail.com>
2022-02-08 23:45:31 +0800

    Personalize Duke

--- src/main/java/Solana.java
+++ src/main/java/Solana.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class Duke {
-    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
+public class Solana {
+    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";
     public static final String RESET_COLOUR = "\033[0m";
-    public static final String PROMPT = "> ";
+    public static final String PROMPT = CYAN_BOLD_BRIGHT + "> " + RESET_COLOUR;
 
     public static final int COMMAND_INDEX = 0;
     public static final int DESCRIPTION_INDEX = 1;
@@ -21,13 +21,13 @@ public class Duke {
     public static final int MAX_TASKS = 100;
 
     public static void printIntro() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(PURPLE_BOLD_BRIGHT + logo + RESET_COLOUR);
-        System.out.println("Hello! I'm Duke");
+        String logo = "   __|   _ \\  |       \\     \\ |    \\\n"
+                + " \\__ \\  (   | |      _ \\   .  |   _ \\\n"
+                + " ____/ \\___/ ____| _/  _\\ _|\\_| _/  _\\\n";
+
+        System.out.print(System.lineSeparator());
+        System.out.println(CYAN_BOLD_BRIGHT + logo + RESET_COLOUR);
+        System.out.println("Hi, I'm Solana");
         System.out.println("What can I do for you?" + System.lineSeparator());
     }
 
@@ -37,25 +37,25 @@ public class Duke {
     }
 
     public static void printTasks(Task[] tasks) {
-        for (int i = 0; i < Task.getNumTask(); i++) {
+        for (int i = 0; i < Task.getTaskNum(); i++) {
             System.out.print(i + CONVERT_I_TO_TASK + ".");
-            tasks[i].printInfo();
+            tasks[i].printDescription();
         }
     }
 
     public static void printAddedPrompt(Task newtask) {
         System.out.print("Added: ");
-        newtask.printInfo();
+        newtask.printDescription();
 
-        if (Task.getNumTask() > 1) {
-            System.out.println("You now have " + Task.getNumTask() + " tasks in the list" + System.lineSeparator());
+        if (Task.getTaskNum() > 1) {
+            System.out.println("You now have " + Task.getTaskNum() + " tasks in the list" + System.lineSeparator());
         } else {
-            System.out.println("You now have " + Task.getNumTask() + " task in the list" + System.lineSeparator());
+            System.out.println("You now have " + Task.getTaskNum() + " task in the list" + System.lineSeparator());
         }
     }
 
     public static void listCommand(Task[] tasks) {
-        if (Task.getNumTask() == 0) {
+        if (Task.getTaskNum() == 0) {
             System.out.println("No tasks yet!");
         } else {
             System.out.println("Here are the tasks in your list:");
@@ -67,20 +67,20 @@ public class Duke {
     public static void unmarkCommand(String parsedInput, Task[] tasks){
         int taskIndex = Integer.parseInt(parsedInput);
         tasks[taskIndex + CONVERT_TASK_TO_I].unmarkAsDone();
-        tasks[taskIndex + CONVERT_TASK_TO_I].printInfo();
+        tasks[taskIndex + CONVERT_TASK_TO_I].printDescription();
         System.out.print(System.lineSeparator());
     }
 
     public static void markCommand(String parsedInput, Task[] tasks) {
         int taskIndex = Integer.parseInt(parsedInput);
         tasks[taskIndex + CONVERT_TASK_TO_I].markAsDone();
-        tasks[taskIndex + CONVERT_TASK_TO_I].printInfo();
+        tasks[taskIndex + CONVERT_TASK_TO_I].printDescription();
         System.out.print(System.lineSeparator());
     }
 
     public static void todoCommand(String parsedInput, Task[] tasks) {
         Todo newTodo = new Todo(parsedInput);
-        tasks[Task.getNumTask() + CONVERT_TASK_TO_I] = newTodo;
+        tasks[Task.getTaskNum() + CONVERT_TASK_TO_I] = newTodo;
 
         printAddedPrompt(newTodo);
     }
@@ -88,7 +88,7 @@ public class Duke {
     public static void deadlineCommand(String parsedInput, Task[] tasks) {
         String[] parsedInputAsArray = parsedInput.split(" /by ", SPLIT_LIMIT);
         Deadline newDeadline = new Deadline(parsedInputAsArray[DEADLINE_INDEX], parsedInputAsArray[BY_INDEX]);
-        tasks[Task.getNumTask() + CONVERT_TASK_TO_I] = newDeadline;
+        tasks[Task.getTaskNum() + CONVERT_TASK_TO_I] = newDeadline;
 
         printAddedPrompt(newDeadline);
     }
@@ -96,7 +96,7 @@ public class Duke {
     public static void eventCommand(String parsedInput, Task[] tasks) {
         String[] parsedInputAsArray = parsedInput.split(" /at ", SPLIT_LIMIT);
         Event newEvent = new Event(parsedInputAsArray[EVENT_INDEX], parsedInputAsArray[AT_INDEX]);
-        tasks[Task.getNumTask() + CONVERT_TASK_TO_I] = newEvent;
+        tasks[Task.getTaskNum() + CONVERT_TASK_TO_I] = newEvent;
 
         printAddedPrompt(newEvent);
     }
@@ -106,7 +106,7 @@ public class Duke {
 
         switch(parsedInput[COMMAND_INDEX]) {
         case "bye":
-            System.out.println("Bye, hope to see you again soon!");
+            System.out.println("Goodbye!");
             System.exit(0);
         case "list":
             listCommand(tasks);
