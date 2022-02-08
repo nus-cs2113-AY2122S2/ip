05271d4ee48183a532af688637e10d00eeabf70d
Xunyi <73570047+icthenic@users.noreply.github.com>
2022-02-08 21:55:32 +0800

    add todo, deadline, event

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task{
+    private String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    public String toString(){
+        return "[E]" + super.toString() + "(at: " + at + ")";
+    }
+
+
+
+}
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,44 +1,86 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TaskManager {
-    public static void main(String[] args) {
-        Task[] taskList = new Task[3];
-        taskList[0] = new Task("read book");
-        taskList[1] = new Task("return book");
-        taskList[2] = new Task("buy bread");
+    private static final String INDENT = "    ";
+    private static final String LINE="-------------------------------------------";
+    private static int taskCount=0;
+    private static Task[] taskList = new Task[100] ;
 
-        taskList[0].markAsDone();
-        
+    public static void main(String[] args) {
+        System.out.println(INDENT + "Hi, I am XiaoAi TongXue ;D");
+        System.out.println(INDENT + LINE);
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while(!input.equals("bye")){
-            if(input.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for(int i =0 ; i<3; i++){
-                    System.out.println((i+1) + ". " + taskList[i].toString());
+        String action = input.split(" ")[0].toLowerCase(Locale.ROOT);
+        System.out.println(INDENT + LINE);
+        while(!action.equals("bye")){
+            if(action.equals("list")){
+                System.out.println(INDENT+"Here are the tasks in your list:");
+                for(int i =0 ; i<taskCount; i++){
+                    System.out.println(INDENT+(i+1) + "." + taskList[i]);
                 }
             }
-            else if(input.contains("unmark")){
-                System.out.println("Ok, I've marked this task as not done yet:");
-                int idx = Integer.parseInt(input.replace("unmark ", ""));
+            else if(action.equals("todo") || action.equals("deadline") || action.equals("event")){
+                int descriptionIdx = input.indexOf(" ")+1;
+                Task newTask = null;
+                switch(action){
+                case "todo":
+
+                    String description = input.substring(descriptionIdx);
+                    newTask = new Todo(description); // 好愚蠢
+                    break;
+                case "deadline":
+                    int byIdx = input.indexOf("/")+4;
+                    description = input.substring(descriptionIdx, byIdx-5);
+                    String by = input.substring(byIdx);
+                    newTask = new Deadline(description,by);
+                    break;
+                case "event":
+                    int atIdx = input.indexOf("/")+4;
+                    description = input.substring(atIdx);
+                    String at = input.substring(atIdx);
+                    newTask = new Event(description,at);
+                }
+                addTask(newTask);
+            }
+
+            else if(action.equals("unmark")){
+                System.out.println(INDENT+"Ok, I've marked this task as not done yet:");
+                int idx = Integer.parseInt(input.split(" ")[1]);
                 taskList[idx-1].markAsUndone();
-                System.out.println(taskList[idx-1].toString());
+                System.out.println(INDENT + taskList[idx-1].toString());
 
             }
-            else if(input.contains("mark")){
-                System.out.println("Nice! I have marked this task as done:");
-                int idx = Integer.parseInt(input.replace("mark ", ""));
+            else if(action.equals("mark")){
+                System.out.println(INDENT+"Nice! I have marked this task as done:");
+                int idx = Integer.parseInt(input.split(" ")[1]);
                 taskList[idx-1].markAsDone();
-                System.out.println(taskList[idx-1].toString());
+                System.out.println(INDENT + taskList[idx-1].toString());
 
             }
             else{
-                System.out.println("  " + input);
+                System.out.println(INDENT+input);
             }
+            System.out.println(INDENT+LINE);
             input = sc.nextLine();
+            action = input.split(" ")[0].toLowerCase(Locale.ROOT);
+            System.out.println(INDENT+LINE);
         }
-        System.out.println("  Bye. Hope to see you again soon!");
+        System.out.println(INDENT + "Bye. Hope to see you again soon!");
+        System.out.println(INDENT + LINE);
+    }
+
+    public static void addTask(Task t){
+        taskList[taskCount] = t;
+        System.out.println(INDENT+"Got it. I've added this task:");
+        System.out.println(INDENT+t.description);
+        taskCount++;
+        System.out.println(INDENT+"Now you have " + taskCount + " tasks in the list.");
+
     }
     
 }
