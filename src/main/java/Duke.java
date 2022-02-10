8da2a8fa805e69f98d724fc697572bf4d03fc5f1
cristoforows <cristoforo.wesley@gmail.com>
2022-02-10 21:11:57 +0800

    level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Duke {
 
@@ -38,81 +38,97 @@ public class Duke {
                 command = ans.substring(0, commandSeparator);
             }
             timeSeparator = ans.indexOf('/');
-            switch (command) {
-                case "bye":
-                    loopInput = false;
-                    break;
-                case "list":
-                    System.out.println("    _________________________________________");
-                    System.out.println("    Here the task you've written m'lord:");
-                    printTasks();
-                    System.out.println("    _________________________________________\n");
-                    break;
-                case "mark":
-                    ans = ans.substring(commandSeparator + 1);
-                    index = Integer.parseInt(ans) - 1;
-                    mark(index);
-                    System.out.println("    _________________________________________");
-                    System.out.println("    I've marked the task as done m'lord:");
-                    printTasks();
-                    System.out.println("    _________________________________________\n");
-                    break;
-                case "unmark":
-                    ans = ans.substring(commandSeparator + 1);
-                    index = Integer.parseInt(ans) - 1;
-                    unmark(index);
-                    System.out.println("    _________________________________________");
-                    System.out.println("    I've revert the task to active m'lord:");
-                    printTasks();
-                    System.out.println("    _________________________________________\n");
-                    break;
-                case "todo":
-                    ans = ans.substring(commandSeparator + 1);
-                    System.out.println("    _________________________________________");
-                    addTask(ans, 'T', time);
-                    System.out.println("     added: " + tasks[idx - 1]);
-                    System.out.println("     there are currently " + idx + " tasks ");
-                    System.out.println("    _________________________________________\n");
-                    break;
-                case "deadline":
-                    // timeSeparator = (timeSeparator == -1 ? ans.length() : timeSeparator);
-                    if (timeSeparator == -1) {
-                        time = "";
-                    } else {
-                        time = ans.substring(timeSeparator + 4);
-                    }
-                    ans = ans.substring(commandSeparator + 1, timeSeparator);
-                    System.out.println("    _________________________________________");
-                    addTask(ans, 'D', time);
-                    System.out.println("     added: " + tasks[idx - 1]);
-                    System.out.println("     there are currently " + idx + " tasks ");
-                    System.out.println("    _________________________________________\n");
-                    break;
-                case "event":
-                    // timeSeparator = (timeSeparator == -1 ? ans.length() : timeSeparator);
-                    if (timeSeparator == -1) {
-                        time = "";
-                    } else {
-                        time = ans.substring(timeSeparator + 4);
-                    }
-                    ans = ans.substring(commandSeparator + 1, timeSeparator);
-                    System.out.println("    _________________________________________");
-                    addTask(ans, 'E', time);
-                    System.out.println("     added: " + tasks[idx - 1]);
-                    System.out.println("     there are currently " + idx + " tasks ");
-                    System.out.println("    _________________________________________\n");
-                    break;
-                default:
-                    loopInput = false;
-                    break;
+            try {
+                switch (command) {
+                    case "bye":
+                        loopInput = false;
+                        break;
+                    case "list":
+                        System.out.println("    _________________________________________");
+                        System.out.println("    Here the task you've written m'lord:");
+                        printTasks();
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    case "mark":
+                        ans = ans.substring(commandSeparator + 1);
+                        index = Integer.parseInt(ans) - 1;
+                        mark(index);
+                        System.out.println("    _________________________________________");
+                        System.out.println("    I've marked the task as done m'lord:");
+                        printTasks();
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    case "unmark":
+                        ans = ans.substring(commandSeparator + 1);
+                        index = Integer.parseInt(ans) - 1;
+                        unmark(index);
+                        System.out.println("    _________________________________________");
+                        System.out.println("    I've revert the task to active m'lord:");
+                        printTasks();
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    case "todo":
+                        ans = ans.substring(commandSeparator + 1);
+                        System.out.println("    _________________________________________");
+                        System.out.println(ans);
+                        if (ans.equalsIgnoreCase(command)) {
+                            throw new EmptyDescriptionException();
+                        }
+                        addTask(ans, 'T', time);
+                        System.out.println("     added: " + tasks[idx - 1]);
+                        System.out.println("     there are currently " + idx + " tasks ");
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    case "deadline":
+                        // timeSeparator = (timeSeparator == -1 ? ans.length() : timeSeparator);
+                        if (timeSeparator == -1) {
+                            time = "";
+                        } else {
+                            time = ans.substring(timeSeparator + 4);
+                        }
+                        ans = ans.substring(commandSeparator + 1, timeSeparator);
+                        System.out.println("    _________________________________________");
+                        if (ans.equalsIgnoreCase(command)) {
+                            throw new EmptyDescriptionException();
+                        }
+                        addTask(ans, 'D', time);
+                        System.out.println("     added: " + tasks[idx - 1]);
+                        System.out.println("     there are currently " + idx + " tasks ");
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    case "event":
+                        // timeSeparator = (timeSeparator == -1 ? ans.length() : timeSeparator);
+                        if (timeSeparator == -1) {
+                            time = "";
+                        } else {
+                            time = ans.substring(timeSeparator + 4);
+                        }
+                        ans = ans.substring(commandSeparator + 1, timeSeparator);
+                        System.out.println("    _________________________________________");
+                        if (ans.equalsIgnoreCase(command)) {
+                            throw new EmptyDescriptionException();
+                        }
+                        addTask(ans, 'E', time);
+                        System.out.println("     added: " + tasks[idx - 1]);
+                        System.out.println("     there are currently " + idx + " tasks ");
+                        System.out.println("    _________________________________________\n");
+                        break;
+                    default:
+                        throw new WrongCommandException();
+                }
+            } catch (EmptyDescriptionException e) {
+                System.out.println("    It seems like you forgot to include the description of your " + command);
+                System.out.println("    _________________________________________\n");
+            } catch (WrongCommandException e) {
+                System.out.println("    Oh no, I don't understand that yet!");
+                System.out.println("    _________________________________________\n");
             }
-
         }
-
         System.out.println("    _________________________________________");
         System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    _________________________________________");
         input.close();
+
     }
 
     public static void addTask(String desc, char type, String time) {
