174efb83ba4ab98f8fc6bde479bda62cb0cb91be
LI-XINYI1 <lilxysa0723p@gmail.com>
2022-02-11 22:26:43 +0800

    Level 5. Handle Errors

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -20,41 +20,70 @@ public class Baymax {
             word_split = command.split(" ", 2);
             String taskName = word_split[0];
 
+            System.out.println(horiLine);
             //different functions based on command lines
             switch (taskName) {
                 case "todo":
-                    tManager.addTask(new Todo(word_split[1]));
+                    try {
+                        tManager.addTask(new Todo(word_split[1]));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(" ☹ OOPS!!! The description of a todo cannot be empty.");
+                    } catch (BaymaxException b){
+                        System.out.println( b.getMessage() +" ☹ OOPS!!! Let's do it again.");
+                    }
                     break;
                 case "deadline":
-                    taskWord = word_split[1].split("/by ", 2);
-                    taskDescrip = taskWord[0];
-                    String ddl = taskWord[1];
-                    tManager.addTask(new Deadline(taskDescrip, ddl));
+                    try {
+                        taskWord = word_split[1].split("/by ", 2);
+                        taskDescrip = taskWord[0];
+                        String ddl = taskWord[1];
+                        tManager.addTask(new Deadline(taskDescrip, ddl));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(" ☹ OOPS!!! The description of a deadline cannot be empty.");
+                    } catch (BaymaxException b){
+                        System.out.println( b.getMessage() +" ☹ OOPS!!! Let's do it again.");
+                    }
                     break;
                 case "event":
-                    taskWord = word_split[1].split("/at ", 2);
-                    taskDescrip = taskWord[0];
-                    String eventTime = taskWord[1];
-                    tManager.addTask(new Event(taskDescrip, eventTime));
+                    try {
+                        taskWord = word_split[1].split("/at ", 2);
+                        taskDescrip = taskWord[0];
+                        String eventTime = taskWord[1];
+                        tManager.addTask(new Event(taskDescrip, eventTime));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(" ☹ OOPS!!! The description of a event cannot be empty.");
+                    } catch (BaymaxException b){
+                        System.out.println( b.getMessage() +" ☹ OOPS!!! Let's do it again.");
+                    }
                     break;
                 case "list":
                     tManager.printTaskList();
                     break;
                 case "mark":
-                    System.out.println(horiLine);
-                    tManager.markTask(Integer.parseInt(word_split[1]) - 1);
-                    System.out.println(horiLine);
+                    try {
+                        tManager.markTask(Integer.parseInt(word_split[1]) - 1);
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Please input an integer for task index.");
+                    } catch (BaymaxException b){
+                        System.out.println( b.getMessage() +" ☹ OOPS!!! Let's do it again.");
+                    }
                     break;
                 case "unmark":
-                    System.out.println(horiLine);
-                    tManager.unmarkTask(Integer.parseInt(word_split[1]) - 1);
-                    System.out.println(horiLine);
+                    try {
+                        tManager.unmarkTask(Integer.parseInt(word_split[1]) - 1);
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Please input an integer for task index.");
+                    } catch (BaymaxException b){
+                        System.out.println( b.getMessage() +" ☹ OOPS!!! Let's do it again.");
+                    }
                     break;
                 default:
                     System.out.println("Error. Please retry");
                     break;
             }
+            System.out.println(horiLine);
             command = in.nextLine();
+
         }
         tManager.bye(); //bye
     }
---
+++ src/main/java/BaymaxException.java
@@ -0,0 +1,5 @@
+public class BaymaxException extends RuntimeException{
+    public BaymaxException(String errorMessage){
+        super(errorMessage);
+    }
+}
