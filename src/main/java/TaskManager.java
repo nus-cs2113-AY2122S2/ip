e168289d0291dbe69b9866a1d77ac969be9f930d
LI-XINYI1 <lilxysa0723p@gmail.com>
2022-02-11 22:21:05 +0800

    restart over

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -21,18 +21,22 @@ public class TaskManager {
 
     public void addTask(Task description) {
         this.tManager[this.taskCount] = description;
+        System.out.println(horiLine);
         System.out.println(" Got it. I've added this task: \n"+
                             this.tManager[this.taskCount].description+"\n" +
                             "Now you have " + (this.taskCount+1) + " tasks in the list.");
+        System.out.println(horiLine);
         this.taskCount++;
     }
 
     public void printTaskList() {
+        System.out.println(horiLine);
         System.out.println("Here are the tasks in your list:");
         for (int i=0; i<taskCount; i++) {
             System.out.println((i+1) + "." + this.tManager[i].getStatusIcon() +
                                 " " + this.tManager[i].getDescription());
         }
+        System.out.println(horiLine);
     }
 
     public void markTask (int taskIndex) {
