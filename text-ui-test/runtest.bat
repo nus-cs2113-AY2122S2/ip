4e215dce89db0eb1a4e9db9c4d755121869b766a
Daryl <ithinkmynameisdaryl@gmail.com>
2022-02-12 12:05:07 +0800

    Update testing script to clear bimData before testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,6 +5,7 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist data\bimData.txt del data\bimData.txt
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\bim\*.java ..\src\main\java\bim\task\*.java
