6f0a7b611f1813f6fe896db81393dca0be52b885
Wang Jingwei <WANG1569@e.ntu.edu.sg>
2022-02-13 23:39:49 +0800

    Add a list and display the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,26 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        ArrayList<String> todolist = new ArrayList<String>();
+        String task;
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        Scanner in = new Scanner(System.in);
+        task=in.nextLine();
+        while(true){
+            if(task.equalsIgnoreCase("bye"))
+                break;
+            if(task.equalsIgnoreCase("list")){
+                for (int i = 0; i < todolist.size(); i++)
+                    System.out.println((i+1)+"."+todolist.get(i));
+                task=in.nextLine();
+                continue;}
+            else{
+                System.out.println("added:"+task);
+                todolist.add(task);
+                task=in.nextLine();
+                continue;}
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
