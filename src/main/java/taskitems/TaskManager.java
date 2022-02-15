1a6115d1fdd32f3d10776b42110a3fcea34fc60f
shxr3f <e0540641@u.nus.edu>
2022-02-15 18:46:47 +0800

    Included functions to save and load data from text files. Text files are saved in CSV format

--- src/main/java/taskitems/TaskManager.java
+++ src/main/java/taskitems/TaskManager.java
@@ -6,9 +6,15 @@ import taskitems.task.Event;
 import taskitems.task.Task;
 import taskitems.task.Todo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskManager {
+    private final String PATH = "data.txt";
     private ArrayList<Task> tasks = new ArrayList<>();
     private int taskCount = 0;
     static Greet greet;
@@ -18,6 +24,26 @@ public class TaskManager {
 
     public int getTaskCount() {
         return taskCount;
+    }
+
+    public void saveData() {
+        File data = new File(PATH);
+        if (!data.exists()) {
+            try {
+                data.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Unable to save data...");
+            }
+        }
+        try {
+            FileWriter dataWrite = new FileWriter(PATH,false);
+            for(Task a : tasks) {
+                dataWrite.write(a.saveString() + "\n");
+            }
+            dataWrite.close();
+        } catch (IOException e) {
+            System.out.println("Unable to save data...");
+        }
     }
 
     public void markTask(int number) throws IllegalInputException {
@@ -33,7 +59,7 @@ public class TaskManager {
             System.out.println(tasks.get(number - 1));
         }
         greet.printDecoration();
-
+        saveData();
     }
 
     public void unmarkTask(int number) throws IllegalInputException {
@@ -49,6 +75,7 @@ public class TaskManager {
             System.out.println(tasks.get(number - 1));
         }
         greet.printDecoration();
+        saveData();
     }
 
     public void addToTasks(String taskName) throws IllegalInputException {
@@ -61,6 +88,7 @@ public class TaskManager {
         taskCount++;
         System.out.println("You now have " + taskCount + " tasks in the list.");
         greet.printDecoration();
+        saveData();
     }
 
     public void addToTasks(String type, String taskName,String date){
@@ -74,9 +102,92 @@ public class TaskManager {
         taskCount++;
         System.out.println("You now have " + taskCount + " tasks in the list.");
         greet.printDecoration();
+        saveData();
     }
 
-    public void printTasks(){
+    private void addToTasks(String taskName, boolean isBackend) throws IllegalInputException {
+        if (taskName.equals("")) {
+            throw new IllegalInputException();
+        }
+        tasks.add(new Todo(taskName));
+        taskCount++;
+    }
+
+    private void addToTasks(String type, String taskName,String date, boolean isBackend){
+        if (type.equals("E")) {
+            tasks.add(new Event(taskName, date));
+        } else {
+            tasks.add(new Deadline(taskName, date));
+        }
+        taskCount++;
+    }
+
+    private void markTask(int number, boolean isBackend) throws IllegalInputException {
+        if (number > taskCount || number < 1) {
+            throw new IllegalInputException();
+        } else {
+            tasks.get(number - 1).setMarked(true);
+        }
+    }
+
+    public void loadData() throws FileNotFoundException {
+        greet.printDecoration();
+        System.out.println("***Please wait while I fetch past data...***");
+        File data = new File(PATH);
+        Scanner reader = new Scanner(data);
+        int count = 1;
+        while (reader.hasNext()) {
+            String line = reader.nextLine();
+            String[] dataLine = line.split(",");
+            boolean hasFailed = false;
+            switch (dataLine[0]) {
+            case "T":
+                try {
+                    addToTasks(dataLine[1],true);
+                } catch (IllegalInputException e) {
+                    System.out.println("Line " + count + " is corrupted.");
+                    hasFailed = true;
+                }
+                break;
+            case "D":
+                try {
+                    addToTasks("D",dataLine[1],dataLine[3],true);
+                } catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
+                    System.out.println("Line " + count + " is corrupted.");
+                    hasFailed = true;
+                }
+                break;
+            case "E":
+                try {
+                    addToTasks("E",dataLine[1],dataLine[3],true);
+                } catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
+                    System.out.println("Line " + count + " is corrupted.");
+                    hasFailed = true;
+                }
+                break;
+            default:
+                System.out.println("Line " + count + " is corrupted,");
+                hasFailed = true;
+                break;
+            }
+            if (!hasFailed && dataLine[2].equals("1")) {
+                try {
+                    markTask(taskCount,true);
+                } catch (IllegalInputException e) {
+                    System.out.println("Corrupted data has been loaded, please check task number: " + taskCount);
+                }
+            }
+            count++;
+        }
+        if (taskCount == 0) {
+            System.out.println("No saved data found");
+        } else {
+            System.out.println("***" + taskCount + " out of " + (count-1) +
+                    " lines of data are valid and fetched accurately." + "***");
+        }
+    }
+
+    public void printTasks() {
         greet.printDecoration();
         if (taskCount == 0) {
             System.out.println("You have not added any Tasks!");
