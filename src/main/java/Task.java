557cb7f73410a6a1bb39349020c8f51f5f69b101
Wang Jingwei <WANG1569@e.ntu.edu.sg>
2022-02-15 11:09:46 +0800

    Add a task class with description and isDone

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description1){
+        description=description1;
+    }
+
+    public void markAsDone(){
+        isDone=true;
+    }
+
+    public void markAsUndone(){
+        isDone=false;
+    }
+}
