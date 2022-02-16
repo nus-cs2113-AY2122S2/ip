48bd226454813092821a57dc2da552144352683a
OzairHasan <e0407597@u.nus.edu>
2022-02-17 05:55:58 +0800

    Adding save and load functionality

---
+++ src/main/java/duke/task/DynamicTask.java
@@ -0,0 +1,19 @@
+package duke.task;
+
+public abstract class DynamicTask extends Task {
+
+    private String time;
+
+    public DynamicTask(String description, String taskTypeSymbol, String time) {
+        super(description, taskTypeSymbol);
+        this.time = time;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,19 +1,21 @@
 package duke.task;
 
-public class Event extends Task {
-    private String eventTime;
-
+public class Event extends DynamicTask {
     public Event(String description,String eventTime){
-        super(description,"E");
-        this.eventTime=eventTime;
+        super(description,"E",eventTime);
     }
 
     public void setEventTime(String eventTime) {
-        this.eventTime = eventTime;
+        this.setTime(eventTime);
     }
+
+    public String getEventTime() {
+        return this.getTime();
+    }
+
 
     // overriding method toString in Object class.
     public String toString(){
-        return super.toString()+" (at: "+eventTime+")";
+        return super.toString()+" (at: "+this.getTime()+")";
     }
 }
