8ca9e0d3e609bbf7ea61f81985b1c5d307adfc5d
IsaacLai <e0540509@u.nus.edu>
2022-02-16 23:49:49 +0800

    Added method to update file whenever tasks are marked done or undone

--- src/main/java/duke/File.java
+++ src/main/java/duke/File.java
@@ -8,6 +8,11 @@ import duke.task.Todo;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class File {
@@ -18,7 +23,9 @@ public class File {
     public static final String FILE_DEADLINE = "D";
     public static final String FILE_EVENT = "E";
     public static final String FILE_DONE = "1";
+    public static final char FILE_DONE_CHAR = '1';
     public static final String FILE_NOT_DONE = "0";
+    public static final int FILE_STATUS_INDEX = 4;
 
     public static void loadFileContents() throws FileNotFoundException {
         java.io.File f = new java.io.File(FILE_PATH);
@@ -37,18 +44,37 @@ public class File {
         }
     }
 
+    public static void updateStatus(Task task, int lineNum) throws IOException {
+        String oldStatus = getFormattedString(task);
+        String newStatus;
+        if (oldStatus.charAt(FILE_STATUS_INDEX) == FILE_DONE_CHAR) {
+            newStatus = oldStatus.substring(0,4) + FILE_NOT_DONE + oldStatus.substring(5);
+        } else {
+            newStatus = oldStatus.substring(0,4) + FILE_DONE + oldStatus.substring(5);
+        }
+        // Solution inspired from https://stackoverflow.com/questions/31375972/how-to-replace-a-specific-line-in-a-file-using-java
+        Path path = Paths.get(FILE_PATH);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        lines.set(lineNum - 1, newStatus);
+        Files.write(path, lines, StandardCharsets.UTF_8);
+    }
+
+    public static String getFormattedString(Task task) {
+        if (task instanceof Todo) {
+            return (FILE_TODO + " | " + convertStatusToString(task.getIsDone()) + " | "
+                    + task.getDescription() + "\n");
+        } else if (task instanceof Deadline) {
+            return (FILE_DEADLINE + " | " + convertStatusToString(task.getIsDone()) + " | "
+                    + task.getDescription() + " | " + ((Deadline) task).getDeadline() + "\n");
+        } else {
+            return (FILE_EVENT + " | " + convertStatusToString(task.getIsDone()) + " | "
+                    + task.getDescription() + " | " + ((Event) task).getAt() + "\n");
+        }
+    }
+
     public static void appendToFile(Task task) throws IOException {
         FileWriter fileWriter = new FileWriter(FILE_PATH, true);
-        if (task instanceof Todo) {
-            fileWriter.write(FILE_TODO + FILE_FLAG + convertStatusToString(task.getIsDone()) + FILE_FLAG
-                    + task.getDescription());
-        } else if (task instanceof Deadline) {
-            fileWriter.write(FILE_DEADLINE + FILE_FLAG + convertStatusToString(task.getIsDone()) + FILE_FLAG
-                    + task.getDescription() + FILE_FLAG + ((Deadline) task).getDeadline());
-        } else {
-            fileWriter.write(FILE_EVENT + FILE_FLAG + convertStatusToString(task.getIsDone()) + FILE_FLAG
-                    + task.getDescription() + FILE_FLAG + ((Event) task).getAt());
-        }
+        fileWriter.write(getFormattedString(task));
         fileWriter.close();
     }
 
