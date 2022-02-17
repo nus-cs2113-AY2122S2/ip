22c67462049aae2aab61f8ce767c99f579f1c2d3
zhenster <bobicusj@gmail.com>
2022-02-17 19:52:04 +0800

    Finish level-7: save (add loading from hard disk behavior)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Duke {
     private static String division = "_____________________________________________\n";
@@ -23,9 +25,101 @@ public class Duke {
     }
 
     /**
+     * Helper for convertTaskStringToTask
+     * "Cleans" a string representation of a task (gets rid of extraneous characters like brackets and parens)
+     * Converts clean string representation into an ArrayList
+     * @param taskString the string representation of the task
+     * @return ArrayList containing the information of the taskString
+     */
+    public static ArrayList<String> cleanTaskListString(String taskString) {
+        taskString = taskString.replace("[", "").replace("]", "").replace("(", "").replace(")", "");
+        String[] splitInformation = taskString.split("\\s");
+        ArrayList<String> taskInformation = new ArrayList<>();
+        for (int i = 0; i < splitInformation.length; i++) {
+            if (splitInformation[i].trim().length() > 0) {
+                taskInformation.add(splitInformation[i]);
+            }
+        }
+        return taskInformation;
+    }
+
+    /**
+     * Helper for convertTaskStringToTask
+     * Given an ArrayList of strings, a starting index, and an ending index, concatenates the strings in the
+     * ArrayList within the boundaries of the indices into a single string
+     * @param list the given ArrayList
+     * @param startIndex the index to start concatenating from
+     * @param endIndex the index to stop concatenating at (not inclusive)
+     * @return the concatenated string
+     */
+    public static String concatenateStringsInArrayList(ArrayList<String> list, int startIndex, int endIndex) {
+        String string = "";
+        for (int i = startIndex; i < endIndex; i++) {
+            string += list.get(i);
+            string += " ";
+        }
+        return string.trim();
+    }
+
+
+    /**
+     * Helper for loadTasksFromDisk
+     * Converts a string representation of a Task to a Task object
+     * @param taskString the string representation of the task
+     * @return the Task object
+     */
+    public static Task convertTaskStringToTask(String taskString) {
+        Task task = new Task("");
+        ArrayList<String> taskInformation = cleanTaskListString(taskString);
+        char taskType = taskInformation.get(0).charAt(0);
+        boolean isTaskDone = false;
+        if (taskInformation.get(0).length() > 1) {
+            isTaskDone = true;
+        }
+        String description;
+        if (taskType == 'T') {
+            description = concatenateStringsInArrayList(taskInformation, 1, taskInformation.size());
+            task = new Todo(description);
+        } else if (taskType == 'D') {
+            description = concatenateStringsInArrayList(taskInformation, 1, taskInformation.indexOf("by:"));
+            String deadline = concatenateStringsInArrayList(taskInformation,taskInformation.indexOf("by:") + 1, taskInformation.size());
+            task = new Deadline(description, deadline);
+        } else if (taskType == 'E') {
+            description = concatenateStringsInArrayList(taskInformation, 1, taskInformation.indexOf("at:"));
+            String time = concatenateStringsInArrayList(taskInformation,taskInformation.indexOf("at:") + 1, taskInformation.size());
+            task = new Event(description, time);
+        }
+        task.isDone = isTaskDone;
+        return task;
+    }
+
+    /**
+     * Helper for processTasks
+     * Loads tasks from duke.txt (file of saved tasks)
+     * Assumes hard-coded location of saved tasks ("data/duke.txt")
+     */
+    public static void loadTasksFromDisk() {
+        File savedTaskFile = new File("data/duke.txt");
+        try {
+            ArrayList<String> taskLines = new ArrayList<>();
+            Scanner s = new Scanner(savedTaskFile);
+            while (s.hasNext()) {
+                taskLines.add(s.nextLine());
+            }
+            for (String taskString : taskLines) {
+                Task task = convertTaskStringToTask(taskString);
+                taskList.add(task);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No tasks found saved on disk, starting with a fresh slate!");
+        }
+    }
+
+    /**
      * Helper for saveTasksToFile
      * Formats taskList into a string to be written into a file
      * @param taskList list of tasks to be formatted into a string
+     * @return the string representation of the list of tasks
      */
     public static String formatTaskListToString(ArrayList<Task> taskList) {
         String taskListString = "";
@@ -38,7 +132,7 @@ public class Duke {
 
     /**
      * Helper for processTasks
-     * Saves the list of tasks to "data/duke.txt"
+     * Saves the list of tasks to "data/duke.txt" (assumes hard-coded location to save the tasks)
      * @param taskListString string representation of list of tasks to write to file
      */
     public static void saveTasksToFile(String taskListString) throws IOException {
@@ -149,7 +243,7 @@ public class Duke {
             throw new DukeException("OOPS!!! The description of a " + type + " cannot be empty.");
         }
 
-        return new String[] { type, description, additionalInfo };
+        return new String[] { type.trim(), description.trim(), additionalInfo.trim() };
     }
 
     /**
@@ -209,6 +303,7 @@ public class Duke {
      * Processes user input (addition of tasks to the global taskList) until user types "bye"
      */
     public static void processTasks() {
+        loadTasksFromDisk();
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
@@ -265,6 +360,5 @@ public class Duke {
         System.out.println(division);
 
         processTasks();
-
     }
 }
