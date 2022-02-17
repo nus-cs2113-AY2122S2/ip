1aa6bacce09ff0c0d516f06821df566dd3c9cdd3
Roy Tang <roy_tql@hotmail.com>
2022-02-17 15:37:25 +0800

    Add messages class

---
+++ src/main/java/duke/common/Messages.java
@@ -0,0 +1,24 @@
+package duke.common;
+
+public class Messages {
+    public final static String BORDER = "__________________________________________________";
+    public final static String INTRO_LOGO = "   ___             _               ___             _\n" +
+            "  |   \\   _  _    | |__    ___    | _ )    ___    | |_\n" +
+            "  | |) | | +| |   | / /   / -_)   | _ \\   / _ \\   |  _|\n" +
+            "  |___/   \\_,_|   |_\\_\\   \\___|   |___/   \\___/   _\\__|\n" +
+            "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|\n" +
+            "\"`-0-0-'\"`-0-0-'\    "`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'";
+    public final static String OUTRO_LOGO = "                               __\n" +
+            "                            _ / /\n" +
+            "  __ ___      ____      __ (_) |\n" +
+            " / _` \\ \\ /\\ / /\\ \\ /\\ / /   | |\n" +
+            "| (_| |\\ V  V /  \\ V  V /   _| |\n" +
+            " \\__,_| \\_/\\_/    \\_/\\_/   (_) |\n" +
+            "                              \\_\\";
+
+    public final static String WELCOME_MESSAGE = " Good Morning sir, I am DukeBot, your personal assistant!" +
+                                                    "\n What can I do for you today?";
+
+    public final static String GOODBYE_MESSAGE = " I guess it's time to say good bye..." +
+                                                    "\n Please come again soon!";
+}
