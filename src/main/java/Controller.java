89b1c013bc49857ed25f9c5d7691f45cfb904ac0
hlwang56 <e0550418@u.nus.edu>
2022-02-17 20:59:52 +0800

    Fix some bugs

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -49,65 +49,66 @@ public class Controller {
         }
     }
 
-    public void deleteTask() throws DukeExceptions{
+    public void deleteTask() throws DukeExceptions {
         try {
             int index = Integer.parseInt(analyst.taskName);
             manager.deleteTask(index);
         } catch (NumberFormatException e) {
             throw new IllegalTaskIndexException();
-*/
-
+        }
+    }
     /**
      * Listen the instruction and operate during the session
      */
-    public void listen() throws DukeExceptions{
-        Scanner msg = new Scanner(System.in);
-        this.recvMsg = msg.nextLine();
-        try {
-            analyst = new OperationAnalyst(this.recvMsg);
-            String command = analyst.getCommand();
-            switch (command) {
-            case "bye":
-                this.bye();
-                break;
-            case "list":
-                manager.listTask();
-                break;
-            case "mark":
-                this.markTask();
-                break;
-            case "unmark":
-                this.unmarkTask();
-                break;
-            case "deadline":
-                manager.addDeadline(analyst.taskName, analyst.time);
-                break;
-            case "event":
-                manager.addEvent(analyst.taskName, analyst.time);
-                break;
-            case "todo":
-                manager.addToDo(analyst.taskName);
-                break;
-            case "delete":
-                this.deleteTask();
+    public void listen() throws DukeExceptions {
+                Scanner msg = new Scanner(System.in);
+                this.recvMsg = msg.nextLine();
+                try {
+                    analyst = new OperationAnalyst(this.recvMsg);
+                    String command = analyst.getCommand();
+                    switch (command) {
+                    case "bye":
+                        this.bye();
+                        break;
+                    case "list":
+                        manager.listTask();
+                        break;
+                    case "mark":
+                        this.markTask();
+                        break;
+                    case "unmark":
+                        this.unmarkTask();
+                        break;
+                    case "deadline":
+                        manager.addDeadline(analyst.taskName, analyst.time);
+                        break;
+                    case "event":
+                        manager.addEvent(analyst.taskName, analyst.time);
+                        break;
+                    case "todo":
+                        manager.addToDo(analyst.taskName);
+                        break;
+                    case "delete":
+                        this.deleteTask();
+                    }
+                    //manager.saveTask();
+                } catch (IllegalInstructionException e) {
+                    chatbox.setContent("Sorry, I don't understand your instruction :(");
+                    chatbox.chatboxPrinter();
+                } catch (IllegalFormatException e) {
+                    chatbox.setContent("Please check your format again :(");
+                    chatbox.chatboxPrinter();
+                } catch (IllegalTaskNameException e) {
+                    chatbox.setContent("Did you specify your task name?");
+                    chatbox.chatboxPrinter();
+                } catch (IllegalSavingAction e) {
+                    chatbox.setContent("Cannot save to file :(");
+                    chatbox.chatboxPrinter();
+                } catch (IllegalTaskIndexException e) {
+                    chatbox.setContent("Please specify the index of the task :(");
+                    chatbox.chatboxPrinter();
+                }
             }
-            //manager.saveTask();
-        } catch (IllegalInstructionException e){
-            chatbox.setContent("Sorry, I don't understand your instruction :(");
-            chatbox.chatboxPrinter();
-        } catch (IllegalFormatException e) {
-            chatbox.setContent("Please check your format again :(");
-            chatbox.chatboxPrinter();
-        } catch (IllegalTaskNameException e) {
-            chatbox.setContent("Did you specify your task name?");
-            chatbox.chatboxPrinter();
-        } catch (IllegalSavingAction e) {
-            chatbox.setContent("Cannot save to file :(");
-            chatbox.chatboxPrinter();
-        } catch (IllegalTaskIndexException e) {
-            chatbox.setContent("Please specify the index of the task :(");
-            chatbox.chatboxPrinter();
-         }
 
 
 }
