9b5347ac77c61a34ca7c06f827b08393ab00d1db
1szheng <wansizheng@gmail.com>
2022-02-19 01:51:19 +0800

    Update date formats

--- src/main/java/Bob/util/controller/Parser.java
+++ src/main/java/Bob/util/controller/Parser.java
@@ -10,8 +10,8 @@ import java.time.format.DateTimeParseException;
 public class Parser {
 
     public static final String UNKNOWN_DATE_FORMAT = "Date format unknown, setting default date 1 month ahead.";
-    public static final String[] POSSIBLE_FORMAT_ONE = {"yyyy-MM-dd", "yyyy/MM/dd", "dd/MM/yyyy", "dd-MM-yyyy",
-        "MM/dd/yyyy", "MM-dd-yyyy"};
+    public static final String[] POSSIBLE_FORMAT_ONE = {"yyyy-MM-dd", "yyyy/MM/dd", "E, MMM dd yyyy",
+            "dd/MM/yyyy", "dd-MM-yyyy", "MM/dd/yyyy", "MM-dd-yyyy"};
     public static final int COMMAND_TOKEN_LENGTH = 2;
 
     /**
