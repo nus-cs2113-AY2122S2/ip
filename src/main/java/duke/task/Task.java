dc33cd7d626f25aa95e1907c4d8d022b0934cb6a
Roy Tang <roy_tql@hotmail.com>
2022-02-19 17:48:22 +0800

    Convert task class to an abstract class

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,6 +1,6 @@
 package duke.task;
 
-public class Task {
+public abstract class Task {
 
     private String description;
     private boolean isDone;
@@ -31,17 +31,11 @@ public class Task {
         return isDone;
     }
 
-    public String getTaskType() {
-        return "task";
-    }
+    public abstract String getTaskType();
 
-    public String getDateFormattedString() {
-        return "-";
-    }
+    public abstract String getDateFormattedString();
 
-    public String getDateForStorage() {
-        return "-";
-    }
+    public abstract String getDateForStorage();
 
     @Override
     public String toString(){
