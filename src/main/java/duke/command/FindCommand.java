5656b35e1edc3aec264c546dff623270a5735cb7
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-02-21 00:33:55 +0800

    Update Find

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -25,19 +25,29 @@ public class FindCommand extends Command {
      * that match the keyword which the user input.
      *
      * @param tasks The TaskList object that contains the list of tasks.
-     * @param ui The user interface object that allows for printing of the confirmation message.
+     * @param ui The user interface object that allows for printing of the list of tasks.
      * @param storage The storage object which allows for the saving of the tasks to the file.
      * @throws AdditionalException If there are no tasks.
      * @see AdditionalException
      */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws AdditionalException {
-        int startingIndex = "find".length();
-        int endingIndex = fullCommand.length();
-        String keyword = fullCommand.substring(startingIndex, endingIndex).trim();
-        if (keyword.length() == 0) {
-            throw new AdditionalException("You want me to find nothing or everything...?");
-        }
+        String keyword = getKeyword();
+        ArrayList<Task> listOfTasks = getTasks(tasks, ui, keyword);
+        ui.showList(listOfTasks);
+    }
+
+    /**
+     * This is the getTasks method that searches through the tasks in the TaskList.
+     * It creates a new list of tasks and adds the tasks that contain the keyword.
+     * After searching and adding the tasks, it then returns the list of tasks.
+     *
+     * @param tasks The TaskList object that contains the list of tasks.
+     * @param ui The user interface object that allows for printing when no results are found.
+     * @param keyword The keyword to be searched for.
+     * @return The list of tasks that contain the keyword.
+     */
+    private ArrayList<Task> getTasks(TaskList tasks, Ui ui, String keyword) {
         ArrayList<Task> listOfTasks = new ArrayList<>();
         for (int i = 0; i < tasks.getSize(); i++) {
             Task task = tasks.getTask(i);
@@ -46,7 +56,24 @@ public class FindCommand extends Command {
         if (listOfTasks.isEmpty()) {
             ui.showNoResults();
         }
-        ui.showList(listOfTasks);
+        return listOfTasks;
+    }
+
+    /**
+     * This is the getKeyword function that gets the keyword to be searched for from the user input.
+     *
+     * @return The keyword to be search for.
+     * @throws AdditionalException If there are no keywords to search for.
+     * @see AdditionalException
+     */
+    private String getKeyword() throws AdditionalException {
+        int startingIndex = "find".length();
+        int endingIndex = fullCommand.length();
+        String keyword = fullCommand.substring(startingIndex, endingIndex).trim();
+        if (keyword.length() == 0) {
+            throw new AdditionalException("You want me to find nothing or everything...?");
+        }
+        return keyword;
     }
 
     /**
