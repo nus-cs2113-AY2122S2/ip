23e7f0c6144a04137775d342caf0c6c378936a39
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-02-21 01:53:17 +0800

    Update Date

--- src/main/java/duke/command/DateCommand.java
+++ src/main/java/duke/command/DateCommand.java
@@ -4,6 +4,8 @@ import duke.TaskList;
 import duke.Ui;
 import duke.exception.AdditionalException;
 import duke.storage.Storage;
+import duke.task.Deadline;
+import duke.task.Event;
 import duke.task.Task;
 
 import java.time.LocalDate;
@@ -34,8 +36,22 @@ public class DateCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws AdditionalException {
         LocalDate date = getDate();
-        ArrayList<Task> listOfTasks = tasks.getListOfSameDates(date);
+        ArrayList<Task> listOfTasks = getListOfSameDates(tasks, date);
         ui.showList(listOfTasks);
+    }
+
+    /**
+     * This is the isBye method that returns whether the command is "bye".
+     *
+     * @return False because the command is "date".
+     */
+    @Override
+    public boolean isBye() {
+        return false;
+    }
+
+    @Override
+    public void executeFromFile(ArrayList<Task> listOfTasks) throws AdditionalException {
     }
 
     /**
@@ -55,16 +71,56 @@ public class DateCommand extends Command {
     }
 
     /**
-     * This is the isBye method that returns whether the command is "bye".
+     * This is the getListOfSameDates method that returns a list of tasks with the same date amongst the tasks
+     * in the list of tasks.
      *
-     * @return False because the command is "date".
+     * @param date The date that is to be compared with the date of tasks.
+     * @return The list of tasks with the same date.
      */
-    @Override
-    public boolean isBye() {
-        return false;
+    private ArrayList<Task> getListOfSameDates(TaskList tasks, LocalDate date) {
+        ArrayList<Task> listOfTasksWithSameDate = new ArrayList<>();
+        for (int i = 0; i < tasks.getSize(); i++) {
+            addToList(listOfTasksWithSameDate, date, tasks.getTask(i));
+        }
+        return listOfTasksWithSameDate;
     }
 
-    @Override
-    public void executeFromFile(ArrayList<Task> listOfTasks) throws AdditionalException {
+    /**
+     * This is the addToList method that takes in the list of tasks with same date and passes it to the addIfSameDate
+     * method when there is a task, with the same date of the user input, in the list of tasks.
+     *
+     * @param tasksWithSameDate The list of tasks with the same date.
+     * @param date The date that is to be compared with the date of tasks.
+     * @param task The task to be compared with.
+     */
+    private void addToList(ArrayList<Task> tasksWithSameDate, LocalDate date, Task task) {
+        LocalDate dateOfTask;
+        if (task instanceof Deadline) {
+            Deadline deadline = (Deadline) task;
+            dateOfTask = deadline.getDate();
+            addIfSameDate(tasksWithSameDate, date, dateOfTask, deadline);
+        } else if (task instanceof Event) {
+            Event event = (Event) task;
+            dateOfTask = event.getDate();
+            addIfSameDate(tasksWithSameDate, date, dateOfTask, event);
+        } else {
+            return;
+        }
+
+    }
+
+    /**
+     * This is the addIfSameDate method that takes in the list of tasks with same date and adds the task to the
+     * list of tasks with same date when the date matches.
+     *
+     * @param tasksWithSameDate The list of tasks with the same date.
+     * @param date The date that is to be compared with the date of the tasks.
+     * @param dateOfTask The date of the task that is being compared with.
+     * @param task The task that is being compared with, and added if it has the same date.
+     */
+    private void addIfSameDate(ArrayList<Task> tasksWithSameDate, LocalDate date, LocalDate dateOfTask, Task task) {
+        if (date.equals(dateOfTask)) {
+            tasksWithSameDate.add(task);
+        }
     }
 }
