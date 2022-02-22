255e5983c75546e4fd85c17f2362d46a10f58c35
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-02-22 10:31:49 +0800

    InvalidCommandException.java: Add JavaDoc documentation

--- src/main/java/sora/InvalidCommandException.java
+++ src/main/java/sora/InvalidCommandException.java
@@ -1,5 +1,13 @@
 package sora;
 
+/**
+ * Thrown to indicate that a command entered by the user has certain issues. In particular, this exception
+ * is thrown when the user...
+ * <ul>
+ *     <li>enters a command that Sora does not understand, or</li>
+ *     <li>enters an invalid command when adding a task (todo/event/deadline)</li>
+ * </ul>
+ */
 public class InvalidCommandException extends Exception {
     // Used for when exception was not thrown with specified expected and received values
     private static final String EMPTY_VALUE_PLACEHOLDER = "N.A.";
@@ -24,6 +32,11 @@ public class InvalidCommandException extends Exception {
     private String expectedValue;
     private String receivedValue;
 
+    /**
+     * Constructs an InvalidCommandException with a specified error message.
+     *
+     * @param errorMsg The error message written by the throwing method.
+     */
     public InvalidCommandException(String errorMsg) {
         super(errorMsg);
         this.expectedValue = EMPTY_VALUE_PLACEHOLDER;
@@ -35,8 +48,6 @@ public class InvalidCommandException extends Exception {
         this.expectedValue = expectedValue;
         this.receivedValue = receivedValue;
     }
-
-
 
     /**
      * Accessor methods
