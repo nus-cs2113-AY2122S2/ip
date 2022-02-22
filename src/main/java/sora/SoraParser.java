500996e0134e9cf77327d185e28a2c2074089577
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-02-22 10:57:25 +0800

    SoraParser.java: Add JavaDoc documentation and add exception handling of invalid task numbers

--- src/main/java/sora/SoraParser.java
+++ src/main/java/sora/SoraParser.java
@@ -2,13 +2,27 @@ package sora;
 
 import java.util.Scanner;
 
+/**
+ * This class manages the inputs entered by the user as well as parsing it to provide specific information
+ * as required by Sora's components.
+ */
 public class SoraParser {
+    // Specifies a set of characters that the user is not allowed to use in his/her input.
     public static final String[] ILLEGAL_CHARACTERS = {
             "|"
     };
 
     private Scanner reader = new Scanner(System.in);
 
+    /**
+     * Reads in a user's input. Thereafter, leading and trailing whitespaces are removed for program stability.
+     * In addition, the user's input is checked for illegal characters and if any are found, an
+     * IllegalCharacterException is thrown.
+     *
+     * @return A string containing the user's sanitised input.
+     * @throws IllegalCharacterException If the user's input contains an illegal character as specified in the
+     * ILLEGAL_CHARACTERS string array.
+     */
     protected String getUserInput() throws IllegalCharacterException {
         String userInput = reader.nextLine();
         String userInputTrimmed = userInput.trim();
@@ -22,19 +36,43 @@ public class SoraParser {
         return userInputTrimmed;
     }
 
-    protected String extractCommand(String userRawInput) {
-        String userCommand = userRawInput.toLowerCase().split(" ", 2)[0];
+    /**
+     * Returns the command keyword the user has entered in his/her input and wants Sora to execute.
+     *
+     * @param userInput The user's full input.
+     * @return A string containing the command keyword the user has entered.
+     */
+    protected String extractCommand(String userInput) {
+        String userCommand = userInput.toLowerCase().split(" ", 2)[0];
         return userCommand;
     }
 
-    protected int getTaskNumberFromCommand(String userRawInput) {
-        int taskNum = Integer.parseInt(userRawInput.split(" ")[1]);
-        return taskNum;
+    /**
+     * Returns the task number the user has entered in his/her input.
+     *
+     * @param userInput The user's full input.
+     * @return The task number that the user has entered in his/her input.
+     */
+    protected int getTaskNumberFromCommand(String userInput) {
+        try {
+            int taskNum = Integer.parseInt(userInput.split(" ")[1]);
+            return taskNum;
+        } catch (NumberFormatException e) {
+            // Re-throw it to calling method to handle
+            throw e;
+        }
     }
 
-    private boolean checkForIllegalCharacters(String userRawInput) {
+    /**
+     * Checks if the user's input contains at least one character that is specified in the
+     * ILLEGAL_CHARACTERS string array.
+     *
+     * @param userInput
+     * @return true if the user's input contains at least one illegal character. Otherwise, false is returned.
+     */
+    private boolean checkForIllegalCharacters(String userInput) {
         for (String illegalChar : ILLEGAL_CHARACTERS) {
-            if (userRawInput.contains(illegalChar)) {
+            if (userInput.contains(illegalChar)) {
                 return true;
             }
         }
