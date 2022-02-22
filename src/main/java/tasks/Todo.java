245e41c2ca561d6be71bfe83d8179f419ddf8c59
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-02-22 14:07:18 +0800

    Todo.java: Add JavaDoc documentation

--- src/main/java/tasks/Todo.java
+++ src/main/java/tasks/Todo.java
@@ -1,12 +1,26 @@
 package tasks;
 
+/**
+ * Represents a Task of type Todo. Unlike Deadline and Event tasks, Todo tasks only contains
+ * a description of the task (no date and time is stored).
+ */
 public class Todo extends Task {
     private static final String TODO_CHECKBOX = "[T]";
 
+    /**
+     * Constructs a Todo task with the specified description.
+     *
+     * @param description The description of the Todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the Todo instance.
+     *
+     * @return A string representation of the Todo instance.
+     */
     public String toString() {
         String checkbox = getCheckbox();
         return String.format("%s%s %s", TODO_CHECKBOX, checkbox, getDescription());
