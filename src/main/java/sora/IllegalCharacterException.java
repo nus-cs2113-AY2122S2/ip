676a1785736c631fa807de0b8c245ca13c75458f
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-02-22 09:51:50 +0800

    IllegalCharacterException.java: Add JavaDocs documentation

--- src/main/java/sora/IllegalCharacterException.java
+++ src/main/java/sora/IllegalCharacterException.java
@@ -1,11 +1,25 @@
 package sora;
 
+/**
+ * Thrown to indicate that the user's command input contains at least one character that is regarded
+ * as illegal due to design constraints.
+ * <p>
+ * For example, the pipe character ("|") is not allowed because this character is used as a delimiter
+ * for data stored in Sora's storage file.
+ */
 public class IllegalCharacterException extends Exception {
     public static final String ILLEGAL_CHARACTER_MSG = "Input string contains an illegal character.";
 
     private String throwingMethod;
     private String throwingClass;
 
+    /**
+     * Constructs an IllegalCharacterException with a specified error message.
+     *
+     * @param errorMsg The message written by the throwing method.
+     * @param throwingMethod (to be removed)
+     * @param throwingClass (to be removed)
+     */
     public IllegalCharacterException(String errorMsg, String throwingMethod, String throwingClass) {
         super(errorMsg);
         this.throwingMethod = throwingMethod;
