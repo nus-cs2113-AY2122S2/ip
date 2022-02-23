054372af62dee2b421cb9081d07049b25319ffca
Kr0sfyre <teanweijun@gmail.com>
2022-02-23 15:41:51 +0800

    Edit structure of main class to be more intuitive

--- src/main/java/serene/Serene.java
+++ src/main/java/serene/Serene.java
@@ -7,7 +7,6 @@ import serene.operation.Storage;
 import serene.operation.TaskList;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -16,38 +15,38 @@ public class Serene {
     private static int statusOfSerene = Constant.CONTINUE;
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         TaskList taskList = new TaskList();
-        try {
-            initiateSerene(taskList);
-        } catch (IOException e) {
-            System.out.println(Ui.IO_FAIL_MESSAGE);
-            Ui.printExitMessage();
+        statusOfSerene = initiateSerene(taskList);
+        if (statusOfSerene == Constant.DONE) {
             return;
         }
         Ui.printWelcomeMessage();
-        operateSerene(in, taskList);
+        operateSerene(taskList);
         Ui.printExitMessage();
     }
 
-    private static void initiateSerene(TaskList taskList) throws IOException {
-        File dataDirectory = new File("data");
-        if (dataDirectory.mkdir()) {
-            System.out.println("Data directory created~");
-        }
-        File save = new File(SAVE_FILE_PATH);
-        if (save.createNewFile()) {
-            System.out.println("Save file created~");
-            return;
-        }
+    private static int initiateSerene(TaskList taskList) {
         try {
+            File dataDirectory = new File("data");
+            if (dataDirectory.mkdir()) {
+                System.out.println("Data directory created~");
+            }
+            File save = new File(SAVE_FILE_PATH);
+            if (save.createNewFile()) {
+                System.out.println("Save file created~");
+                // Return as there will be no data to read from
+                return Constant.CONTINUE;
+            }
             Storage.readSavedContents(save, taskList.getTaskList(), taskList.getTaskCount());
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println(Ui.IO_FAIL_MESSAGE);
+            return Constant.DONE;
         }
+        return Constant.CONTINUE;
     }
 
-    private static void operateSerene(Scanner in, TaskList taskList) {
+    private static void operateSerene(TaskList taskList) {
+        Scanner in = new Scanner(System.in);
         while (statusOfSerene != Constant.DONE) {
             String userInput = in.nextLine();
             statusOfSerene = Parser.parseInput(userInput, taskList.getTaskList());
