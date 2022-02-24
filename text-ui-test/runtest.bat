252dcb14185e7ac37f144efb8a61d2453425b4f4
JL <tha.junlim@gmail.com>
2022-02-24 20:50:20 +0800

    update text-ui-test cases for level 8

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\vera\*.java ..\src\main\java\vera\exception\*.java ..\src\main\java\vera\task\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\vera\Vera.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
