4eddc4f4ce0f0dd319fa3c0cabe18bebda88c1f5
Kr0sfyre <teanweijun@gmail.com>
2022-02-24 17:22:44 +0800

    Make error messages clearer

--- src/main/java/serene/global/Ui.java
+++ src/main/java/serene/global/Ui.java
@@ -11,8 +11,8 @@ public class Ui {
     public static final String INPUT_ERROR_MESSAGE = "Uhm... I'm afraid I don't know what you mean by that :/";
     public static final String INVALID_NUM_ERROR_MESSAGE = "Please enter a valid task number ;-;";
     public static final String EMPTY_DESC_ERROR_MESSAGE = "Hey! Don't try to make me record nothing for fun :<";
-    public static final String EMPTY_BY_ERROR_MESSAGE = "No time input? Please remember your /by~";
-    public static final String EMPTY_AT_ERROR_MESSAGE = "No time input? Please remember your /at~";
+    public static final String EMPTY_BY_ERROR_MESSAGE = "No time input? Please remember your \" /by \"~";
+    public static final String EMPTY_AT_ERROR_MESSAGE = "No time input? Please remember your \" /at \"~";
     public static final String EMPTY_TIME_ERROR_MESSAGE = "Empty time? I'll make you run out of time.";
     public static final String EMPTY_REGEX_ERROR_MESSAGE = "Uhh... What are you trying to look for?";
     public static final String NOTHING_FOUND = "No such task .-.";
