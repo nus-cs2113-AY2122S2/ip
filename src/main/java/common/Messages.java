6ba28678c39ff871cdc6b51a42af37cfa0554e9f
ElaineQT <wangqianteng@gmail.com>
2022-02-24 20:37:04 +0800

    Refine Ui class

--- src/main/java/common/Messages.java
+++ src/main/java/common/Messages.java
@@ -9,4 +9,6 @@ public class Messages {
     public static final String BYE = "\t" + "-".repeat(60) + "\n" +
             "\t Bye. Hope to see you again soon!\n" +
             "\t" + "-".repeat(60);
+
+    public static final String LINE = "\t" + "-".repeat(60);
 }
--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,9 +1,13 @@
 package ui;
 import common.Messages;
 
-public class Ui {
-    public Ui() {
+import java.util.Scanner;
 
+public class Ui {
+    private final Scanner sc;
+
+    public Ui() {
+        sc = new Scanner(System.in);
     }
 
     public void showGreetingMessage() {
@@ -12,6 +16,11 @@ public class Ui {
 
     public void showByeMessage() {
         System.out.println(Messages.BYE);
+    }
+
+    public String getUserCommand() {
+        String fullInputLine = sc.nextLine();
+        return fullInputLine;
     }
 
 }
