9e88efe4a7d2a9f3c24bfb9ef669b6d95cb1d296
ElaineQT <wangqianteng@gmail.com>
2022-03-02 00:16:18 +0800

    Add JavaDoc to tasks

--- src/main/java/data/Event.java
+++ src/main/java/data/Event.java
@@ -3,14 +3,30 @@ package data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represent an event task with description and the happening date and time of it.
+ */
 public class Event extends Task {
     protected LocalDateTime at;
 
+    /**
+     * Class constructor.
+     *
+     * @param description description of the event task
+     * @param at happening date and time of the event task
+     */
     public Event(String description, LocalDateTime at) {
         super(description);
         this.at = at;
     }
 
+    /**
+     * Class constructor specifying whether the task is done.
+     *
+     * @param description description of the event task
+     * @param at happening date and time of the event task
+     * @param isDone whether the task is done
+     */
     public Event(String description, LocalDateTime at, boolean isDone) {
         super(description, isDone);
         this.at = at;
--- src/main/java/data/Task.java
+++ src/main/java/data/Task.java
@@ -1,14 +1,28 @@
 package data;
 
+/**
+ * Represents a general task with only description.
+ */
 public class Task {
     private String description;
     private boolean isDone;
 
+    /**
+     * Class constructor specifying the task description.
+     *
+     * @param description description of the task
+     */
     public Task(String description) {
         this.description = description;
         isDone = false;
     }
 
+    /**
+     * Class constructor specifying the task description and whether the task is done.
+     *
+     * @param description description of the task
+     * @param isDone whether the task is done
+     */
     public Task(String description, boolean isDone){
         this.description = description;
         this.isDone = isDone;
@@ -34,6 +48,11 @@ public class Task {
         isDone = false;
     }
 
+    /**
+     * Get the icon indicating whether the task is done.
+     *
+     * @return "X" if the task is done.
+     */
     public String getStatusIcon() {
 
         return (isDone ? "X" : " ");
@@ -44,6 +63,11 @@ public class Task {
         return "[" + getStatusIcon() + "] " + description;
     }
 
+    /**
+     * Get information of the task that can be stored in data file.
+     *
+     * @return information about the task.
+     */
     public String getInfo() {
         return "U / " + isDone + " / " + description;
     }
