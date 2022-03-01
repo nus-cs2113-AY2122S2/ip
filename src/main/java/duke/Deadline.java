4aa0cf8d926f420415966ee91974281ee0fc7e2c
yuhaochua <yuhaochua99@gmail.com>
2022-03-02 01:58:40 +0800

    Add JavaDoc and credit for reused work

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,18 +1,35 @@
 package duke;
 
+/**
+ * Represents Deadline subclass
+ */
 public class Deadline extends Task{
+    /**
+     * Create new Deadline task
+     * @param description description of Deadline task
+     */
     public Deadline(String description){
         super(description);
         this.description = addInDate(description);
         symbol = "[D]";
     }
 
+    /**
+     * Create new Deadline task with specification of whether it is done
+     * @param description description of Deadline task
+     * @param isDone status of completion for Deadline task
+     */
     public Deadline(String description, boolean isDone){
         super(description, isDone);
         this.description = addInDate(description);
         symbol = "[D]";
     }
 
+    /**
+     * Interprets the user input and formats the specified deadline into description accordingly
+     * @param task description of task with the details of deadline
+     * @return formatted string with deadline
+     */
     public String addInDate(String task){
         int marker = task.indexOf("/");
         String date;
