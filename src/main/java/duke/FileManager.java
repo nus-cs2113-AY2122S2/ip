858a5bd837412b228aa5c5497be1d3c883680358
yuzhongng <yuzhongng@gmail.com>
2022-03-01 23:41:17 +0800

    Refractor FileManager

--- src/main/java/duke/FileManager.java
+++ src/main/java/duke/FileManager.java
@@ -4,24 +4,25 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileManager {
-    public FileManager() {
+    public FileManager(File file) {
+        File duke = file;
+        openFile(duke);
     }
 
-    static File openFile() {
-        File duke = new File("./duke.txt");
-        if (duke.exists()) {
-            return duke;
+    static File openFile(File file) {
+        if (file.exists()) {
+            return file;
         } else {
             try {
-                duke.createNewFile();
-                return duke;
+                file.createNewFile();
+                return file;
             } catch (IOException e) {
                 System.out.println("Error IO");
             } catch (SecurityException e) {
                 System.out.println("Error Security");
             }
         }
-        return duke;
+        return file;
     }
 }
 
