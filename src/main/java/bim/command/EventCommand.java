ffb17f03c2e515b00dba12fb6dc8b4d03a40e8db
Daryl <ithinkmynameisdaryl@gmail.com>
2022-03-02 14:31:17 +0800

    Split AddCommand into DeadlineCommand, EventCommand and TodoCommand

---
+++ src/main/java/bim/command/EventCommand.java
@@ -0,0 +1,42 @@
+package bim.command;
+
+import bim.BimException;
+import bim.Storage;
+import bim.Ui;
+import bim.task.Event;
+import bim.task.TaskList;
+
+import java.time.LocalDate;
+
+/**
+ * Adds an event task to the task list when executed.
+ */
+public class EventCommand extends Command {
+    private String description;
+    private LocalDate date;
+
+    public EventCommand(String description, LocalDate date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    /**
+     * Writes the new task to the data file. Only if the writing is successful, add the new task
+     * to the task list and print the newly added task.
+     *
+     * @param tasks The task list to add the new event into
+     * @param ui Ui object to print messages
+     * @param storage Storage object for saving to disk.
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        Event newEvent = new Event(description, date);
+        try {
+            storage.writeData(newEvent);
+            tasks.addTask(newEvent);
+            ui.printAddTaskMessage(newEvent, tasks.getSize());
+        } catch (BimException exception) {
+            ui.printErrorMessage(exception.getMessage());
+        }
+    }
+}
