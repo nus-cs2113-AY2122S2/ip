52299ba65795600eb71b74b68e8864c0270fcb07
Kang Hwee Thern <kanghweethern@gmail.com>
2022-03-04 00:54:23 +0800

    Updated tests

--- src/main/java/duke/exceptions/InvalidCommandException.java
+++ src/main/java/duke/exceptions/InvalidCommandException.java
@@ -3,15 +3,7 @@ package duke.exceptions;
 public class InvalidCommandException extends DukeException {
     private static final String INVALID_COMMAND_MESSAGE =
             "☹ OOPS!!! I'm sorry, but I don't know what '%s' means :-("
-            + "\nList of commands for Duke: "
-            + "\nlist"
-            + "\nbye"
-            + "\nmark <task number>"
-            + "\nunmark <task number>"
-            + "\ntodo <description>"
-            + "\ndeadline <description> /by <date>"
-            + "\nevent <description> /at <date>"
-            + "\ndelete <task number>";
+            + "\nType 'help' to get a list of commands for Duke!";
 
     public InvalidCommandException(String errorCommand) {
         super(String.format(INVALID_COMMAND_MESSAGE, errorCommand));
