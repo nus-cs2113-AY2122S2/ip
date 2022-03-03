2a0a31ee38c6c913b04468eedd27f21468d6945b
quitejasper <jasper.chia9@gmail.com>
2022-03-03 18:06:27 +0800

    introduced TaskList class in accordance to project requirements. This class essentially holds an arraylist, and methods are to make contact with the arraylist

---
+++ src/main/java/tasklist/TaskList.java
@@ -0,0 +1,33 @@
+package tasklist;
+
+import duke.Task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    public ArrayList<Task> taskArrayList;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.taskArrayList = tasks;
+    }
+
+    public TaskList() {
+        this.taskArrayList = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return taskArrayList.size();
+    }
+
+    public Task getTaskViaIndex(int index) {
+        return taskArrayList.get(index);
+    }
+
+    public void addTaskToList(Task task) {
+        taskArrayList.add(task);
+    }
+
+    public void removeTaskFromList(Task task) {
+        taskArrayList.remove((task));
+    }
+}
