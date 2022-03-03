341c951aa7a39fe4179f91963955b6b378190d60
RenTianle <lele.rer@outlook.com>
2022-03-04 04:45:20 +0800

    Create new messages class

---
+++ src/main/java/duke/messages/Messages.java
@@ -0,0 +1,19 @@
+package duke.messages;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class Messages {
+
+    public static final String MESSAGE_WELCOME = "______________________________________________________________\n"
+                                                 + "Hello! I'm Duke\n"
+                                                 + "What can I do for you?\n"
+                                                 + "______________________________________________________________";
+
+    public static final String MESSAGE_LOADING_ERROR = "Failed to load the file";
+    public static final String MESSAGE_FILEPATH = "duke.txt";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
+}
