4eca00b6859b16e92a729fdda54f766b09c2de03
Kang Hwee Thern <kanghweethern@gmail.com>
2022-03-03 23:58:50 +0800

    Added Level 8

--- src/main/java/duke/commands/EventCommand.java
+++ src/main/java/duke/commands/EventCommand.java
@@ -1,11 +1,16 @@
 package duke.commands;
 
+import duke.Parser;
 import duke.exceptions.DukeException;
 import duke.exceptions.InvalidArgumentException;
+import java.time.format.DateTimeParseException;
+
 import duke.tasks.Event;
 import duke.tasks.TaskList;
 import duke.Ui;
 import duke.Storage;
+
+import java.time.LocalDateTime;
 
 import java.util.HashMap;
 
@@ -13,7 +18,8 @@ public class EventCommand extends Command {
     public static final String TASK_ADDED_MESSAGE_FORMAT = "added: %s";
     private static final String COMMAND_NAME = "deadline";
     private static final String EMPTY_ARGUMENTS = "Event must have a description!";
-    private static final String EMPTY_BYDATE = "Event must have a valid date for /at!";
+    private static final String EMPTY_ATDATE = "Event must have a date for /at!";
+    private static final String INCORRECT_ATDATE = "The date and time entered for /at must be a valid date time!";
 
     private HashMap<String, String> arguments;
     /**
@@ -36,7 +42,7 @@ public class EventCommand extends Command {
             errorMsg += EMPTY_ARGUMENTS+"\n";
         }
         if (arguments.get("/at")==null || arguments.get("").equals("")) {
-            errorMsg += EMPTY_BYDATE+"\n";
+            errorMsg += EMPTY_ATDATE +"\n";
         }
         if (!errorMsg.equals("")) {
             throw new InvalidArgumentException(COMMAND_NAME,errorMsg.trim());
@@ -52,11 +58,28 @@ public class EventCommand extends Command {
     public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
         assertArguments();
         String description = arguments.get("");
-        String byDate = arguments.get("/at");
-        Event eventTask = new Event(description, byDate);
-        taskList.add(eventTask);
-        String output = String.format(TASK_ADDED_MESSAGE_FORMAT, eventTask.toString());
-        ui.showOutput(output);
-        storage.write(taskList);
+        String atDateTime = arguments.get("/at");
+        try {
+            LocalDateTime[] atDateTimeParsed = Parser.parseAtDateTime(atDateTime);
+            LocalDateTime atDateTimeStartParsed = atDateTimeParsed[0];
+            LocalDateTime atDateTimeEndParsed = atDateTimeParsed[1];
+            if (atDateTimeStartParsed.isAfter(atDateTimeEndParsed)) {
+                InvalidArgumentException exception = new InvalidArgumentException(COMMAND_NAME, INCORRECT_ATDATE);
+                throw exception;
+            }
+            Event eventTask = new Event(description, atDateTimeStartParsed, atDateTimeEndParsed);
+            taskList.add(eventTask);
+            String output = String.format(TASK_ADDED_MESSAGE_FORMAT, eventTask.toString());
+            ui.showOutput(output);
+            storage.write(taskList);
+        } catch (IllegalStateException e) {
+            // Date given did not match regex format
+            InvalidArgumentException exception = new InvalidArgumentException(COMMAND_NAME, INCORRECT_ATDATE);
+            throw exception;
+        } catch (DateTimeParseException e) {
+            // Values given cannot be parsed into date (e.g. value >12 for month)
+            InvalidArgumentException exception = new InvalidArgumentException(COMMAND_NAME, INCORRECT_ATDATE);
+            throw exception;
+        }
     }
 }
--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,12 +1,18 @@
 package duke.tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Queue;
 
 public class Event extends Task {
 
-    private static final String FORMAT_STRING = "[%c][%c] %s (at: %s)";
+    private static final String EVENT_TOSTRING_FORMAT_STRING = "[%c][%c] %s (at: %s)";
+    private static final String DATE_FORMAT = "d/M/y";
+    private static final String TIME_FORMAT = "HHmm";
+    private static final String AT_DATE_STRING_FORMAT = "%s %s-%s";
 
-    protected String atDate;
+    protected LocalDateTime atDateTimeStart;
+    protected LocalDateTime atDateTimeEnd;
 
     /**
      * Create an Event with isDone set to false, description of choice and atDate of choice
@@ -14,10 +20,11 @@ public class Event extends Task {
      * @param description Description of the Task to be created
      * @param atDate String representing event time
      */
-    public Event(String description, String atDate) {
+    public Event(String description, LocalDateTime atDateTimeStart, LocalDateTime atDateTimeEnd) {
         this.description = description;
         this.isDone = false;
-        this.atDate = atDate;
+        this.atDateTimeStart = atDateTimeStart;
+        this.atDateTimeEnd = atDateTimeEnd;
         this.tag = 'E';
     }
 
@@ -26,18 +33,37 @@ public class Event extends Task {
      *
      * @return byDate
      */
-    public String getAtDate() {
-        return this.atDate;
+    public LocalDateTime getAtDateTimeStart() {
+        return this.atDateTimeStart;
+    }
+
+    /**
+     * Returns the atDate associated with event
+     *
+     * @return byDate
+     */
+    public LocalDateTime getAtDateTimeEnd() {
+        return this.atDateTimeEnd;
     }
 
     @Override
     public String toString() {
-        return String.format(FORMAT_STRING, this.getTag(), this.getIsDone(), this.getDescription(), this.getAtDate());
+        return String.format(EVENT_TOSTRING_FORMAT_STRING, this.getTag(), this.getIsDone(), this.getDescription(), this.getAtDateTimeAsString());
     }
 
     @Override
     public void getFileWriterFormatString(Queue<String> infoToWrite) {
         super.getFileWriterFormatString(infoToWrite);
-        infoToWrite.add(atDate);
+        infoToWrite.add(getAtDateTimeAsString());
+    }
+
+    public String getAtDateTimeAsString() {
+        DateTimeFormatter atDateFormat = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        DateTimeFormatter atTimeFormat = DateTimeFormatter.ofPattern(TIME_FORMAT);
+        String getAtDateString = this.atDateTimeStart.format(atDateFormat);
+        String getAtTimeStartString = this.atDateTimeStart.format(atTimeFormat);
+        String getAtTimeEndString = this.atDateTimeEnd.format(atTimeFormat);
+        String getAtDateTimeString = String.format(AT_DATE_STRING_FORMAT, getAtDateString, getAtTimeStartString, getAtTimeEndString);
+        return getAtDateTimeString;
     }
 }
