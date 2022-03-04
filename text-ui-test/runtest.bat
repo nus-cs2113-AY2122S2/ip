3c2c3b1c02859f459fc9fbdca2f5375cbcaab932
LAPTOP-ABRSSH4C\clare <clarencechuayinghow@hotmail.com>
2022-03-04 18:47:59 +0800

    Edits to text-ui-test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,15 +5,16 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
-
+Pause
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
+    Pause
     exit /b 1
 )
 REM no error here, errorlevel == 0
-
+Pause
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
