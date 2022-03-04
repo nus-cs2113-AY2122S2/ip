0538bb754782eb908fda7bfbd2c81d9e52ee55ae
Lester Sim <lester.sim@u.nus.edu>
2022-03-04 15:49:38 +0800

    Refactor Code. Removed executeCommands.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -69,56 +69,6 @@ public class Duke {
 //        }
     }
 
-    // Takes in a string. Executed commands will print related statements.
-//    public static void executeCommands (TaskList tasks, Ui ui, Storage storage) throws DukeException {
-//        String[] userInputArr;
-//        String userCommand;
-//        String[] userArguments;
-//        String description;
-//
-//        userInputArr = userInput.split(" ", 2);
-//        userCommand = userInputArr[0];
-//
-//        switch (userCommand) {
-//        case "list":
-//            currChat.printTaskList();
-//            break;
-//        case "mark":
-//            // Additional argument provided by user is the task no. to mark
-//            currChat.markTaskIndex(Integer.parseInt(userInputArr[1]));
-//            break;
-//        case "unmark":
-//            currChat.unmarkTaskIndex(Integer.parseInt(userInputArr[1]));
-//            break;
-//        case "todo":
-//            // Detect missing arguments
-//            if (userInputArr.length == 1) {
-//                throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-//            }
-//            currChat.addTask(new Todo(userInputArr[1]));
-//            break;
-//        case "deadline":
-//            // eg. return book /by Sunday
-//            userArguments = userInputArr[1].split(" /by ", 2);
-//            description = userArguments[0]; // eg. return book
-//            String by = userArguments[1]; // eg. Sunday
-//            tasks.addTask(new Deadline(description, by));
-//            break;
-//        case "event":
-//            // eg. project meeting /at Mon 2-4pm
-//            userArguments = userInputArr[1].split(" /at ", 2);
-//            description = userArguments[0]; // eg. project meeting
-//            String eventTime = userArguments[1]; // eg. Mon 2-4pm
-//            currChat.addTask(new Event(description, eventTime));
-//            break;
-//        case "delete":
-//            currChat.deleteTaskIndex(Integer.parseInt(userInputArr[1]));
-//            break;
-//        default:
-//            throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-//        }
-//    }
-
     public static void main(String[] args) {
         new Duke("data/duke.txt").run();
     }
