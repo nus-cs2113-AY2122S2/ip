c578842676ed05896ad4dd1b08a45017d855ef9c
Lester Sim <lester.sim@u.nus.edu>
2022-03-05 18:43:32 +0800

    Updated AddCommand to remove input validation
    Input validation is now handled by Parser instead.

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -4,10 +4,7 @@ import duke.DukeException;
 import duke.Storage;
 import duke.TaskList;
 import duke.Ui;
-import duke.task.Deadline;
-import duke.task.Event;
 import duke.task.Task;
-import duke.task.Todo;
 
 /**
  * Represents a Command to add a Task to the TaskList.
@@ -23,29 +20,6 @@ public class AddCommand extends Command {
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
-//        Task newTask;
-//        String[] argumentList;
-//        String description;
-//        String dateInput;
-//
-//        switch (this.commandWord) {
-//        case "deadline":
-//            argumentList = splitArguments("/by");
-//            description = argumentList[0].trim(); // eg. return book
-//            dateInput = argumentList[1].trim(); // yyyy-mm-dd
-//            newTask = new Deadline(description, dateInput);
-//            break;
-//        case "event":
-//            argumentList = splitArguments("/at");
-//            description = argumentList[0].trim(); // eg. social event
-//            dateInput = argumentList[1].trim(); // yyyy-mm-dd
-//            newTask = new Event(description, dateInput);
-//            break;
-//        default:
-//            // default is todo
-//            newTask = new Todo(this.arguments);
-//        }
-
         tasks.addTask(this.task);
         storage.writeTasksToStorage(tasks);
 
@@ -53,11 +27,4 @@ public class AddCommand extends Command {
         System.out.println(String.format("  %s", this.task));
         System.out.println(String.format("Now you have %d tasks in the list.", tasks.getSize()));
     }
-
-//    public String[] splitArguments(String sep) throws DukeException {
-//        if (this.arguments.split(sep, 2).length < 2) {
-//            throw new DukeException("OOPS!!! There are missing arguments. Please state the date/time.");
-//        }
-//        return this.arguments.split(sep, 2);
-//    }
 }
