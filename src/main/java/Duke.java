63fd391aca904be5971388d7560db32b1dc2cc64
Anvitha-r <anvithar1@gmail.com>
2022-03-19 22:46:12 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
-    public static void main(String[] args) {
+    public static void greetings() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String fillerLine = "____________________________________________________________";
+
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println(fillerLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(fillerLine);
+
+    }
+    public static void main(String[] args){
+        greetings();
     }
 }
