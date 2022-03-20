d8bd450050e646615d18f4da17802efced987299
Anvitha-r <anvithar1@gmail.com>
2022-03-20 13:03:45 +0800

    (Level-1) Greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void greetings() {
         String logo = " ____        _        \n"
@@ -10,12 +12,35 @@ public class Duke {
 
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
-        System.out.println(fillerLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(fillerLine);
 
     }
+
+
     public static void main(String[] args){
+        //start the programme
         greetings();
+
+        //get the user input
+        Scanner in = new Scanner(System.in);
+        String userInput = "";
+        userInput = in.nextLine();
+
+        String fillerLine = "____________________________________________________________";
+
+        while (!userInput.toLowerCase().equals("bye")) {
+            // Echo the user input
+            System.out.println(fillerLine);
+            System.out.println(userInput);
+            System.out.println(fillerLine);
+
+
+            //get the next user input
+            userInput = in.nextLine();
+            System.out.println(fillerLine);
+        }
+
+        //if user says bye
+        System.out.println("See you again soon!");
+
     }
 }
